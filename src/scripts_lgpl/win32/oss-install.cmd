26b8ecb4a14495878f74ff9f3939176149869ef0
abel_dl <abel_dl>
2011-10-31 10:23:44 +0000

    oss install script: improved error handling

--- src/scripts_lgpl/win32/oss-install.cmd
+++ src/scripts_lgpl/win32/oss-install.cmd
@@ -126,7 +126,15 @@ rem ====================
     if not exist !dest_default! mkdir !dest_default!
 
     copy engines_gpl\flow2d3d\bin\Release\flow2d3d.dll                                     !dest_bin!
+    if NOT %ErrorLevel%==0 (
+        set ErrorLevel_flowdll=1
+    )
     copy engines_gpl\flow2d3d\bin\Release\flow2d3d_sp.dll                                  !dest_bin!
+    if %ErrorLevel%!=0 (
+        if NOT !ErrorLevel_flowdll! == 0 (
+            set GlobalErrorLevel=1
+        )
+    )
        rem One of these two dlls will not exist and cause an ErrorLevel=1. Reset it.
     set ErrorLevel=0
     copy engines_gpl\flow2d3d\scripts\meteo_old2new.m                                      !dest_bin!
