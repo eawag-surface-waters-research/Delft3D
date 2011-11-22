125b71cca1ed4626515105c2bdeaf162267afefe
abel_dl <abel_dl>
2011-11-22 14:49:02 +0000

    Added Triangle grid generator by J.R. Shewchuk

---
+++ src/third_party_open/triangle/f2c.h
@@ -0,0 +1,22 @@
+ 
+#if ! defined(WIN32)
+#define STDCALL
+#if defined (FTN_UNDERSCORE)
+
+#   define TRICALL2 tricall2_
+
+#else
+
+#   define TRICALL2 tricall2
+
+#endif
+#else
+#   if defined(_MSC_VER)   /* Microsoft Visual C++ */
+#      if (_MSC_VER < 1300)   /* versions earlier than V7.0: use CVF naming convention */
+#         define STDCALL _stdcall
+#      else                   /* V8.0: use C-Default naming convention */
+#         define STDCALL
+#      endif
+#   endif
+#endif
+
