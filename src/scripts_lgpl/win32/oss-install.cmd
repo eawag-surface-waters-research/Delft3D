847b3c1ccb33699580a70394bec1854bff4ffb86
Adri Mourits <Adri.Mourits@deltares.nl>
2012-07-12 11:27:24 +0000

    install script: copy Intel libraries also for WAVE

--- src/scripts_lgpl/win32/oss-install.cmd
+++ src/scripts_lgpl/win32/oss-install.cmd
@@ -269,11 +269,12 @@ rem ================
     call :makeDir !dest_swan_lib!
     call :makeDir !dest_swan_scripts!
 
-    call :copyFile engines_gpl\wave\bin\release\wave.exe           !dest_bin!
-    call :copyFile engines_gpl\flow2d3d\default\dioconfig.ini      !dest_default!
-    call :copyFile "third_party_open\swan\lib\win32\*.*"           !dest_swan_lib!
-    call :copyFile "third_party_open\swan\bin\win32\*.*"           !dest_swan_bin!
-    call :copyFile third_party_open\swan\scripts\swan_install.bat "!dest_swan_scripts!\swan.bat"
+    call :copyFile engines_gpl\wave\bin\release\wave.exe            !dest_bin!
+    call :copyFile engines_gpl\flow2d3d\default\dioconfig.ini       !dest_default!
+    call :copyFile "third_party_open\intel_fortran\lib\win32\*.dll" !dest_lib!
+    call :copyFile "third_party_open\swan\lib\win32\*.*"            !dest_swan_lib!
+    call :copyFile "third_party_open\swan\bin\win32\*.*"            !dest_swan_bin!
+    call :copyFile third_party_open\swan\scripts\swan_install.bat " !dest_swan_scripts!\swan.bat"
 goto :endproc
 
 
