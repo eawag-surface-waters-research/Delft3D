1dfb4cba58694346a67dfb36b3c3fdee7760a7d1
postma <postma>
2012-11-06 15:46:06 +0000

    De dps file is er ook bijgezet in de .hyd file

--- src/engines_gpl/flow2d3d/packages/io/src/output/wrwaqhyd.f90
+++ src/engines_gpl/flow2d3d/packages/io/src/output/wrwaqhyd.f90
@@ -8,30 +8,30 @@
      &                      ilaggr , nd     , nlb    , nub    , mlb    , &
      &                      mub    , kfsmin , ksrwaq )
 !----- GPL ---------------------------------------------------------------------
-!                                                                               
-!  Copyright (C)  Stichting Deltares, 2011-2012.                                
-!                                                                               
-!  This program is free software: you can redistribute it and/or modify         
-!  it under the terms of the GNU General Public License as published by         
-!  the Free Software Foundation version 3.                                      
-!                                                                               
-!  This program is distributed in the hope that it will be useful,              
-!  but WITHOUT ANY WARRANTY; without even the implied warranty of               
-!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                
-!  GNU General Public License for more details.                                 
-!                                                                               
-!  You should have received a copy of the GNU General Public License            
-!  along with this program.  If not, see <http://www.gnu.org/licenses/>.        
-!                                                                               
-!  contact: delft3d.support@deltares.nl                                         
-!  Stichting Deltares                                                           
-!  P.O. Box 177                                                                 
-!  2600 MH Delft, The Netherlands                                               
-!                                                                               
-!  All indications and logos of, and references to, "Delft3D" and "Deltares"    
-!  are registered trademarks of Stichting Deltares, and remain the property of  
-!  Stichting Deltares. All rights reserved.                                     
-!                                                                               
+!
+!  Copyright (C)  Stichting Deltares, 2011-2012.
+!
+!  This program is free software: you can redistribute it and/or modify
+!  it under the terms of the GNU General Public License as published by
+!  the Free Software Foundation version 3.
+!
+!  This program is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!  GNU General Public License for more details.
+!
+!  You should have received a copy of the GNU General Public License
+!  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!
+!  contact: delft3d.support@deltares.nl
+!  Stichting Deltares
+!  P.O. Box 177
+!  2600 MH Delft, The Netherlands
+!
+!  All indications and logos of, and references to, "Delft3D" and "Deltares"
+!  are registered trademarks of Stichting Deltares, and remain the property of
+!  Stichting Deltares. All rights reserved.
+!
 !-------------------------------------------------------------------------------
 !  $Id$
 !  $HeadURL$
@@ -188,6 +188,8 @@
       write ( lunout , '(A,A    )' ) 'vert-diffusion-file      ',trim(filstring)
       filstring = ''''//trim(filnam)//'srf'''
       write ( lunout , '(A,A    )' ) 'surfaces-file            ',trim(filstring)
+      filstring = ''''//trim(filnam)//'dps'''
+      write ( lunout , '(A,A    )' ) 'depths-file              ',trim(filstring)
       filstring = ''''//trim(filnam)//'lgt'''
       write ( lunout , '(A,A    )' ) 'total-grid-file          ',trim(filstring)
       filstring = ''''//trim(filnam)//'src'''
