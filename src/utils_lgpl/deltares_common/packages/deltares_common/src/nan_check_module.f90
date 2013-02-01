02d1433678b8fb9e4c7cc51c302bc4ca50625cba
Fedor Baart <Fedor.Baart@deltares.nl>
2013-02-01 16:17:57 +0000

    consistent use of libtool, for compatibility with PIC and non shared library systems

--- src/utils_lgpl/deltares_common/packages/deltares_common/src/nan_check_module.f90
+++ src/utils_lgpl/deltares_common/packages/deltares_common/src/nan_check_module.f90
@@ -1,29 +1,29 @@
 module nan_check_module
 !----- GPL ---------------------------------------------------------------------
-!                                                                               
-!  Copyright (C)  Stichting Deltares, 2011-2012.                                
-!                                                                               
-!  This program is free software: you can redistribute it and/or modify         
-!  it under the terms of the GNU General Public License as published by         
-!  the Free Software Foundation version 3.                                      
-!                                                                               
-!  This program is distributed in the hope that it will be useful,              
-!  but WITHOUT ANY WARRANTY; without even the implied warranty of               
-!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                
-!  GNU General Public License for more details.                                 
-!                                                                               
-!  You should have received a copy of the GNU General Public License            
-!  along with this program.  If not, see <http://www.gnu.org/licenses/>.        
-!                                                                               
-!  contact: delft3d.support@deltares.nl                                         
-!  Stichting Deltares                                                           
-!  P.O. Box 177                                                                 
-!  2600 MH Delft, The Netherlands                                               
-!                                                                               
-!  All indications and logos of, and references to, "Delft3D" and "Deltares"    
-!  are registered trademarks of Stichting Deltares, and remain the property of  
-!  Stichting Deltares. All rights reserved.                                     
-!                                                                               
+!
+!  Copyright (C)  Stichting Deltares, 2011-2012.
+!
+!  This program is free software: you can redistribute it and/or modify
+!  it under the terms of the GNU General Public License as published by
+!  the Free Software Foundation version 3.
+!
+!  This program is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!  GNU General Public License for more details.
+!
+!  You should have received a copy of the GNU General Public License
+!  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!
+!  contact: delft3d.support@deltares.nl
+!  Stichting Deltares
+!  P.O. Box 177
+!  2600 MH Delft, The Netherlands
+!
+!  All indications and logos of, and references to, "Delft3D" and "Deltares"
+!  are registered trademarks of Stichting Deltares, and remain the property of
+!  Stichting Deltares. All rights reserved.
+!
 !-------------------------------------------------------------------------------
 !  $Id$
 !  $HeadURL$
@@ -72,7 +72,7 @@ logical function nan_check_1D_sp(field, fieldnam, lundia) result (retval)
        if (isnan(field(i))) then
           write(message,*) 'NaN found in ', fieldnam, &
                ' at (i) = (', i, ')'
-          call prterr(lundia, 'U021', trim(message))
+          write(lundia,*) 'U021', trim(message)
           retval = .false.
           return
        endif
@@ -105,7 +105,7 @@ logical function nan_check_2D_sp(field, fieldnam, lundia) result (retval)
           if (isnan(field(n,m))) then
              write(message,*) 'NaN found in ', fieldnam, &
               ' at (n,m) = (', n, ',' , m, ')'
-             call prterr(lundia, 'U021', trim(message))
+             write(lundia,*) 'U021', trim(message)
              retval = .false.
              return
           endif
@@ -140,7 +140,7 @@ logical function nan_check_3D_sp(field, fieldnam, lundia) result (retval)
             if (isnan(field(n,m,k))) then
               write(message,*) 'NaN found in ', fieldnam, &
                   ' at (n,m,k) = (', n, ',', m, ',', k, ')'
-              call prterr(lundia, 'U021', trim(message))
+              write(lundia,*) 'U021', trim(message)
               retval = .false.
               return
             endif
@@ -177,7 +177,7 @@ logical function nan_check_4D_sp(field, fieldnam, lundia) result (retval)
                 if (isnan(field(n,m,k,l))) then
                    write(message,*) 'NaN found in ', fieldnam, &
                       ' at (n,m,k,l) = (', n, ',', m, ',', k, ',', l, ')'
-                   call prterr(lundia, 'U021', trim(message))
+                   write(lundia,*) 'U021', trim(message)
                    retval = .false.
                    return
                 endif
@@ -212,7 +212,7 @@ logical function nan_check_1D_dp(field, fieldnam, lundia) result (retval)
        if (isnan(field(i))) then
           write(message,*) 'NaN found in ', fieldnam, &
                ' at (i) = (', i, ')'
-          call prterr(lundia, 'U021', trim(message))
+          write(lundia,*) 'U021', trim(message)
           retval = .false.
           return
        endif
@@ -245,7 +245,7 @@ logical function nan_check_2D_dp(field, fieldnam, lundia) result (retval)
           if (isnan(field(n,m))) then
              write(message,*) 'NaN found in ', fieldnam, &
               ' at (n,m) = (', n, ',' , m, ')'
-             call prterr(lundia, 'U021', trim(message))
+             write(lundia,*) 'U021', trim(message)
              retval = .false.
              return
           endif
@@ -280,7 +280,7 @@ logical function nan_check_3D_dp(field, fieldnam, lundia) result (retval)
             if (isnan(field(n,m,k))) then
               write(message,*) 'NaN found in ', fieldnam, &
                   ' at (n,m,k) = (', n, ',', m, ',', k, ')'
-              call prterr(lundia, 'U021', trim(message))
+              write(lundia,*) 'U021', trim(message)
               retval = .false.
               return
             endif
@@ -317,7 +317,7 @@ logical function nan_check_4D_dp(field, fieldnam, lundia) result (retval)
                 if (isnan(field(n,m,k,l))) then
                    write(message,*) 'NaN found in ', fieldnam, &
                       ' at (n,m,k,l) = (', n, ',', m, ',', k, ',', l, ')'
-                   call prterr(lundia, 'U021', trim(message))
+                   write(lundia,*) 'U021', trim(message)
                    retval = .false.
                    return
                 endif
