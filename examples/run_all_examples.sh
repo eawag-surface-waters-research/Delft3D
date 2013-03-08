cb97a45abc43c0da9ef30abf2601b04ffb11d6bd
Adri Mourits <Adri.Mourits@deltares.nl>
2013-03-08 17:44:16 +0000

    run_all_examples.sh added

---
+++ examples/run_all_examples.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+echo "Running testcase 01_standard ..."
+cd 01_standard
+./run_flow2d3d.sh >screen.log 2>&1
+
+
+echo "Running testcase 01_standard parallel ..."
+./run_flow2d3d_parallel.sh >screen_parallel.log 2>&1
+cd ..
+
+
+echo "Running testcase 02_domaindecomposition ..."
+cd 02_domaindecomposition
+./run_flow2d3d.sh >screen.log 2>&1
+cd ..
+
+
+echo "Running testcase 03_flow-wave ..."
+cd 03_flow-wave
+./run_flow2d3d.sh >screen.log 2>&1
+cd ..
+
+
+echo "Running testcase 04_fluidmud ..."
+cd 04_fluidmud
+./run_flow2d3d_flm.sh >screen.log 2>&1
+cd ..
+
+
+echo "Running testcase 05_mormerge ..."
+cd 05_mormerge/merge
+./run_flow2d3d_wave_mormerge.sh >screen.log 2>&1
+cd ../..
+
+
+echo ...finished
+
