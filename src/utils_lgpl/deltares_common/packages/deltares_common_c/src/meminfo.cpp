671a2b6006bc3be5a3fed5429be5ff0615a2b910
Fedor Baart <Fedor.Baart@deltares.nl>
2013-04-25 10:33:05 +0000

    meminfo for osx

--- src/utils_lgpl/deltares_common/packages/deltares_common_c/src/meminfo.cpp
+++ src/utils_lgpl/deltares_common/packages/deltares_common_c/src/meminfo.cpp
@@ -1,22 +1,33 @@
 #include "meminfo.h"
-
+#include <sys/sysctl.h>
 #ifdef WIN32
 
 unsigned __int64 MemInfo::GetTotalMemSize()
 {
-	MEMORYSTATUSEX status;
-    status.dwLength = sizeof(status);
-    GlobalMemoryStatusEx(&status);
- 	return unsigned __int64 (status.ullTotalPhys);
+  MEMORYSTATUSEX status;
+  status.dwLength = sizeof(status);
+  GlobalMemoryStatusEx(&status);
+  return unsigned __int64 (status.ullTotalPhys);
+}
+#elseif defined(_SC_PHYS_PAGES)
+unsigned long long MemInfo::GetTotalMemSize()
+{
+long long pages = sysconf(_SC_PHYS_PAGES);
+long long page_size = sysconf(_SC_PAGE_SIZE);
+return (pages * page_size);
 }
 
 #else
-
 unsigned long long MemInfo::GetTotalMemSize()
 {
-	long long pages = sysconf(_SC_PHYS_PAGES);
-    long long page_size = sysconf(_SC_PAGE_SIZE);
-    return (pages * page_size);
-}
+int mib[2];
+size_t len;
+long long totalphys64;
 
+mib[0] = CTL_HW;
+mib[1] = HW_MEMSIZE; /* gives a 64 bit int */
+len = sizeof(totalphys64);
+sysctl(mib, 2, &totalphys64, &len, NULL, 0);
+return totalphys64;
+}
 #endif
