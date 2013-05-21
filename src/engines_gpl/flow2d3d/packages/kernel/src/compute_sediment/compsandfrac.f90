e51e18d37bc2dcfee815cab349730ad91f41f4cd
Adri Mourits <Adri.Mourits@deltares.nl>
2013-05-21 13:37:30 +0000

    16356: sedtyp is not a string but a (enumerated) integer

--- src/engines_gpl/flow2d3d/packages/kernel/src/compute_sediment/compsandfrac.f90
+++ src/engines_gpl/flow2d3d/packages/kernel/src/compute_sediment/compsandfrac.f90
@@ -44,17 +44,23 @@ subroutine compsandfrac(frac      ,seddm     ,nmmax     ,lsedtot   , &
     !
     implicit none
 !
+! Parameters
+!
+    integer, parameter :: SEDTYP_NONCOHESIVE_TOTALLOAD = 0
+    integer, parameter :: SEDTYP_NONCOHESIVE_SUSPENDED = 1
+    integer, parameter :: SEDTYP_COHESIVE              = 2
+!
 ! Global variables
 !
     integer                                 , intent(in)  :: lsedtot   ! number of sediment fractions
     integer                                 , intent(in)  :: nmmax     ! last space index to be processed
     integer                                 , intent(in)  :: nmlb      ! start space index
     integer                                 , intent(in)  :: nmub      ! end space index
+    integer , dimension(lsedtot)            , intent(in)  :: sedtyp    ! Sediment type: 0=total/1=noncoh/2=coh
     real(fp), dimension(nmlb:nmub, lsedtot) , intent(in)  :: frac      ! fractional composition of sediment
     real(fp), dimension(lsedtot)            , intent(in)  :: seddm     ! mean diameter of sediment fraction
     real(fp), dimension(nmlb:nmub)          , intent(out) :: sandfrac  ! sand fraction
     real(fp), dimension(nmlb:nmub)          , intent(in)  :: sedd50fld ! D50 field (in case of 1 sediment fraction)
-    character(4), dimension(lsedtot)        , intent(in)  :: sedtyp    ! sediment type: sand/mud/bedload
 !
 ! Local variables
 !
@@ -65,7 +71,7 @@ subroutine compsandfrac(frac      ,seddm     ,nmmax     ,lsedtot   , &
 !
 !! executable statements -------------------------------------------------------
 !
-    if (lsedtot==1 .and. sedtyp(1) /= 'mud') then
+    if (lsedtot==1 .and. sedtyp(1) /= SEDTYP_COHESIVE) then
       ! Single size fraction
       do nm = 1, nmmax
         if (sedd50fld(nm) < 0.002_fp) then
@@ -80,7 +86,7 @@ subroutine compsandfrac(frac      ,seddm     ,nmmax     ,lsedtot   , &
         fracnonmud = 0.0_fp
         sandfrac(nm) = 0.0_fp
         do l = 1, lsedtot
-          if (sedtyp(l) /= 'mud') then
+          if (sedtyp(l) /= SEDTYP_COHESIVE) then
             fracnonmud = fracnonmud + frac(nm,l)
             if (seddm(l) < 0.002_fp) then
               sandfrac(nm) = sandfrac(nm) + frac(nm,l)                
