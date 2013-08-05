9e3aa5ad1803169261c183a1919df5f40252e55a
Fedor Baart <Fedor.Baart@deltares.nl>
2013-08-05 08:07:31 +0000

    fix non-mpi build of deltares_common_mpi (todo: get rid of warning)

--- src/utils_lgpl/deltares_common/packages/deltares_common_mpi/src/dfbroadc.F90
+++ src/utils_lgpl/deltares_common/packages/deltares_common_mpi/src/dfbroadc.F90
@@ -1,29 +1,32 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 subroutine dfbroadc ( iptr, ilen, itype, error, msgstr )
 !----- LGPL --------------------------------------------------------------------
-!                                                                               
-!  Copyright (C)  Stichting Deltares, 2011-2013.                                
-!                                                                               
-!  This library is free software; you can redistribute it and/or                
-!  modify it under the terms of the GNU Lesser General Public                   
-!  License as published by the Free Software Foundation version 2.1.                 
-!                                                                               
-!  This library is distributed in the hope that it will be useful,              
-!  but WITHOUT ANY WARRANTY; without even the implied warranty of               
-!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU            
-!  Lesser General Public License for more details.                              
-!                                                                               
-!  You should have received a copy of the GNU Lesser General Public             
-!  License along with this library; if not, see <http://www.gnu.org/licenses/>. 
-!                                                                               
-!  contact: delft3d.support@deltares.nl                                         
-!  Stichting Deltares                                                           
-!  P.O. Box 177                                                                 
-!  2600 MH Delft, The Netherlands                                               
-!                                                                               
-!  All indications and logos of, and references to, "Delft3D" and "Deltares"    
-!  are registered trademarks of Stichting Deltares, and remain the property of  
-!  Stichting Deltares. All rights reserved.                                     
-!                                                                               
+!
+!  Copyright (C)  Stichting Deltares, 2011-2013.
+!
+!  This library is free software; you can redistribute it and/or
+!  modify it under the terms of the GNU Lesser General Public
+!  License as published by the Free Software Foundation version 2.1.
+!
+!  This library is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+!  Lesser General Public License for more details.
+!
+!  You should have received a copy of the GNU Lesser General Public
+!  License along with this library; if not, see <http://www.gnu.org/licenses/>.
+!
+!  contact: delft3d.support@deltares.nl
+!  Stichting Deltares
+!  P.O. Box 177
+!  2600 MH Delft, The Netherlands
+!
+!  All indications and logos of, and references to, "Delft3D" and "Deltares"
+!  are registered trademarks of Stichting Deltares, and remain the property of
+!  Stichting Deltares. All rights reserved.
+!
 !-------------------------------------------------------------------------------
 !  $Id$
 !  $HeadURL$
@@ -37,7 +40,8 @@ subroutine dfbroadc ( iptr, ilen, itype, error, msgstr )
 !
 !
 !!--declarations----------------------------------------------------------------
-#if defined (DFMPI)
+
+#ifdef HAVE_MPI
     use mpi
 #endif
     use dfparall
@@ -65,7 +69,7 @@ subroutine dfbroadc ( iptr, ilen, itype, error, msgstr )
     !
     if (.not.parll) return
     !
-#if defined (DFMPI)
+#ifdef HAVE_MPI
     call mpi_bcast ( iptr, ilen, itype, master-1, MPI_COMM_WORLD, ierr )
     if ( ierr /= MPI_SUCCESS ) then
        write (msgstr,'(a,i5)') 'MPI produces some internal error - return code is ',ierr
