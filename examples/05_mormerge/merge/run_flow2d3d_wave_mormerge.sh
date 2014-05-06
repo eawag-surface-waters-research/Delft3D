59dc66d485e13a08dcb60ef9eeaf7c6285c6673b
Adri Mourits <Adri.Mourits@deltares.nl>
2014-05-06 12:59:34 +0000

    Example 05_mormerge: use a local mormerge.tcl when present

--- examples/05_mormerge/merge/run_flow2d3d_wave_mormerge.sh
+++ examples/05_mormerge/merge/run_flow2d3d_wave_mormerge.sh
@@ -3,7 +3,15 @@
 export ARCH=lnx
 export HOSTNAME=$HOSTNAME
 
-scriptname=../../../bin/lnx/flow2d3d/scripts/mormerge.tcl
+
+   # If the directory where this script is located contains a "mormerge.tcl": use that
+   # Else use another one
+scriptdirname=`readlink \-f \$0`
+scriptdir=`dirname $scriptdirname`
+scriptname=$scriptdir/mormerge.tcl
+if [ ! -f $scriptname ]; then 
+  scriptname=../../../bin/lnx/flow2d3d/scripts/mormerge.tcl
+fi
 
 $scriptname -i basin_linux.mm -s $scriptname
 
