ed2360760fe269422bc80bace71d47564982cfa9
Adri Mourits <Adri.Mourits@deltares.nl>
2014-05-08 07:36:24 +0000

    Correction on EC-module merge (see revision 3645-3649): mathconsts: Additional brackets needed in the calculation of raddeg

--- src/utils_lgpl/deltares_common/packages/deltares_common/src/mathconsts.f90
+++ src/utils_lgpl/deltares_common/packages/deltares_common/src/mathconsts.f90
@@ -45,21 +45,21 @@ module mathconsts
    !
    ! flexible precision constants
    !
-   real(fp), save :: pi     = 4.0_fp*atan(1.0_fp)          !< pi = 3.141592...
-   real(fp), save :: degrad = 4.0_fp*atan(1.0_fp)/180.0_fp !< conversion factor from degrees to radians  
-   real(fp), save :: raddeg = 180.0_fp/4.0_fp*atan(1.0_fp) !< conversion factor from radians to degrees
-   real(fp), save :: daysec = 24.0_fp*60.0_fp*60.0_fp      !< conversion factor from earth day to seconds
-   real(fp), save :: eps_fp = epsilon(1.0_fp)              !< epsilon for fp
+   real(fp), save :: pi     = 4.0_fp*atan(1.0_fp)            !< pi = 3.141592...
+   real(fp), save :: degrad = 4.0_fp*atan(1.0_fp)/180.0_fp   !< conversion factor from degrees to radians  
+   real(fp), save :: raddeg = 180.0_fp/(4.0_fp*atan(1.0_fp)) !< conversion factor from radians to degrees
+   real(fp), save :: daysec = 24.0_fp*60.0_fp*60.0_fp        !< conversion factor from earth day to seconds
+   real(fp), save :: eps_fp = epsilon(1.0_fp)                !< epsilon for fp
    
    !
    ! high precision constants
    !
-   real(hp), save :: pi_hp      = 4.0_hp*atan(1.0_hp)          !< pi = 3.14159265358979...
-   real(hp), save :: degrad_hp  = 4.0_hp*atan(1.0_hp)/180.0_hp !< conversion factor from degrees to radians
-   real(hp), save :: raddeg_hp  = 180.0_hp/4.0_hp*atan(1.0_hp) !< conversion factor from radians to degrees
-   real(hp), save :: daysec_hp  = 24.0_hp*60.0_hp*60.0_hp      !< conversion factor from earth day to seconds
-   real(hp), save :: yearsec_hp = 31536000.0_hp                !< conversion factor from earth year to seconds
-   real(hp), save :: eps_hp     = epsilon(1.0_hp)              !< epsilon for hp
+   real(hp), save :: pi_hp      = 4.0_hp*atan(1.0_hp)            !< pi = 3.14159265358979...
+   real(hp), save :: degrad_hp  = 4.0_hp*atan(1.0_hp)/180.0_hp   !< conversion factor from degrees to radians
+   real(hp), save :: raddeg_hp  = 180.0_hp/(4.0_hp*atan(1.0_hp)) !< conversion factor from radians to degrees
+   real(hp), save :: daysec_hp  = 24.0_hp*60.0_hp*60.0_hp        !< conversion factor from earth day to seconds
+   real(hp), save :: yearsec_hp = 31536000.0_hp                  !< conversion factor from earth year to seconds
+   real(hp), save :: eps_hp     = epsilon(1.0_hp)                !< epsilon for hp
    
    contains
       
