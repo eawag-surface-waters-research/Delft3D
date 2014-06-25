71acc0f0e4314b07ed6ee3bc4b2e8d6023bd7e4c
Michelle Jeuken <Michelle.Jeuken@deltares.nl>
2014-06-25 18:20:26 +0000

    These batch files will help you work with the DELWAQ process library tools on the process library tables, how to modify the process library, and how to create a new proc_def

---
+++ src/engines_gpl/waq/packages/waq_kernel/src/waq_tables/waqpb_export.bat
@@ -0,0 +1,25 @@
+@echo off
+if exist ..\..\..\..\..\..\tools_gpl\waqpb\packages\waqpb_export\Release\waqpb_export.exe (
+    echo This will execute: waqpb_export.exe < UpdateNEF.ini
+    echo.
+    echo This command will 'export' the data in the csv-tables to a proc_def,
+	echo a procesm.asc and latex files for the manuals. The proc_def files
+    echo will be copied to src\engines_gpl\waq\default\
+    echo.
+    echo Run waqpb_help.bat for more information
+    echo.	
+    pause
+    ..\..\..\..\..\..\tools_gpl\waqpb\packages\waqpb_export\Release\waqpb_export.exe < UpdateNEF.ini
+    copy proc_def.* ..\..\..\..\default
+    echo.
+    pause
+) else (
+    echo.
+    echo Please build the waq proces library tools first.
+	echo You will find then in src\tools_gpl\waqpb\waqpb.sln
+    echo or under tools_gpl - waqpb in the main solution
+    echo.
+    echo Run waqpb_help.bat for more information
+	echo.
+	pause
+)
