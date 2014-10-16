ff49dae6d359fe523984af987b83f2db5faab9d3
Michelle Jeuken <Michelle.Jeuken@deltares.nl>
2014-10-16 12:56:26 +0000

    Tools to build on OpenPLCT for Delwaq from a Duprol file using MinGW

--- src/tools_gpl/duprol2delwaq/example/example.bat
+++ src/tools_gpl/duprol2delwaq/example/example.bat
@@ -1,8 +1,9 @@
 @echo off
-set modfile=Eutrof1a.mod
-set ascfile=%modfile:~0,6%.asc
+set model=Eutrof1a
+set modfile=%model%.mod
+set ascfile=%model%.asc
 
-..\bin\Release\Parse.exe %modfile%
-..\..\waqpb\packages\waqpb_import\Release\waqpb_import.exe %ascfile% OPL newtab newfrm duprol
-..\..\waqpb\packages\waqpb_export\Release\waqpb_export.exe < updateNEF.ini
+..\bin\Parse.exe %modfile%
+..\bin\waqpb_import.exe %ascfile% OPL newtab newfrm duprol
+..\bin\waqpb_export.exe < updateNEF.ini
 pause
