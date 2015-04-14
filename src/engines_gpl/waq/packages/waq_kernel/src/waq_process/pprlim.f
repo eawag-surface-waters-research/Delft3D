c7b67e1801a7d04f4b4633c2a26ae2cd3d5dfcae
Michelle Jeuken <Michelle.Jeuken@deltares.nl>
2015-04-14 13:58:21 +0000

    Modernisation of DYNAMO code, and fix for running DYNAMO in a single cell (by Leo Postma)

--- src/engines_gpl/waq/packages/waq_kernel/src/waq_process/pprlim.f
+++ src/engines_gpl/waq/packages/waq_kernel/src/waq_process/pprlim.f
@@ -26,120 +26,156 @@
      &                    noq3   , noq4   )
 !>\file
 !>       Limitation (numerical) on primary production DYNAMO
+!>
+!>       If more Si is required than available, the growth of Diatoms is reduced.
+!>       If more N and/or P is required than available, the growth of both species
+!>       is reduced. The reduction has the form of a negative correction flux on C and
+!>       produces together with the original flux on C the increase in algae species.
+!>       The sum of original and correction C-flux is forwarded through the output
+!>       variables fcPPxxx to the process NuUpt_Alg that makes the nutrient reduction
+!>       fluxes.
 
-!
-!     Description of the module :
-!
-! Name    T   L I/O   Description                                   Unit
-! ----    --- -  -    -------------------                            ---
+      implicit none
 
-!     Logical Units : -
+!     Type    Name         I/O Description
 
-!     Modules called : -
+      real(4) pmsa(*)     !I/O Process Manager System Array, window of routine to process library
+      real(4) fl(*)       ! O  Array of fluxes made by this process in mass/volume/time
+      integer ipoint( 14) ! I  Array of pointers in pmsa to get and store the data
+      integer increm( 14) ! I  Increments in ipoint for segment loop, 0=constant, 1=spatially varying
+      integer noseg       ! I  Number of computational elements in the whole model schematisation
+      integer noflux      ! I  Number of fluxes, increment in the fl array
+      integer iexpnt(4,*) ! I  From, To, From-1 and To+1 segment numbers of the exchange surfaces
+      integer iknmrk(*)   ! I  Active-Inactive, Surface-water-bottom, see manual for use
+      integer noq1        ! I  Nr of exchanges in 1st direction (the horizontal dir if irregular mesh)
+      integer noq2        ! I  Nr of exchanges in 2nd direction, noq1+noq2 gives hor. dir. reg. grid
+      integer noq3        ! I  Nr of exchanges in 3rd direction, vertical direction, pos. downward
+      integer noq4        ! I  Nr of exchanges in the bottom (bottom layers, specialist use only)
+      integer ipnt( 14)   !    Local work array for the pointering
+      integer iseg        !    Local loop counter for computational element loop
 
-!     Name     Type   Library
-!     ------   -----  ------------
+!***********************************************************************
+!
+!     Project : STANDAARDISATIE PROCES FORMULES T721.72
+!     Author  : Pascal Boderie
+!     Date    : 1992-12-10             Version : 0.01
+!
+!     History :
+!
+!     Date        Author          Description
+!     ----------  --------------  ------------------------------------
+!     2014-05-25  L. Postma       Functionality adapted for robustness
+!
+!***********************************************************************
 
-      IMPLICIT REAL (A-H,J-Z)
+!     Type    Name         I/O Description                                        Unit
 
-      REAL     PMSA  ( * ) , FL    (*)
-      INTEGER  IPOINT( * ) , INCREM(*) , NOSEG , NOFLUX,
-     +         IEXPNT(4,*) , IKNMRK(*) , NOQ1, NOQ2, NOQ3, NOQ4
+      real(4) fPPGreen    ! I  net primary production of Greens                   (gC/m3/d)
+      real(4) NCRatGreen  ! I  N:C ratio Greens                                   (gN/gC)
+      real(4) PCRatGreen  ! I  P:C ratio Greens                                   (gP/gC)
+      real(4) fPPDiat     ! I  net primary production of Diatoms                  (gC/m3/d)
+      real(4) NCRatDiat   ! I  N:C ratio Diatoms                                  (gN/gC)
+      real(4) PCRatDiat   ! I  P:C ratio Diatoms                                  (gP/gC)
+      real(4) SCRatDiat   ! I  Si:C ratio Diatoms                                 (gSi/gC)
+      real(4) DELT        ! I  timestep for processes                             (d)
+      real(4) NH4         ! I  Ammonium (NH4)                                     (gN/m3)
+      real(4) NO3         ! I  Nitrate (NO3)                                      (gN/m3)
+      real(4) PO4         ! I  Ortho-Phosphate (PO4)                              (gP/m3)
+      real(4) Si          ! I  dissolved Silica (Si)                              (gSi/m3)
+      real(4) fcPPGreeN   ! O  numerical maximum flux Greens                      (gC/m3/d)
+      real(4) fcPPDiat    ! O  numerical maximum flux Diatoms                     (gC/m3/d)
+      real(4) dcPPGreeN   ! F  correction flux Greens growth                      (gC/m3/d)
+      real(4) dcPPDiat    ! F  correction flux Diatoms growth                     (gC/m3/d)
+      integer IdcPPGreeN  !    Pointer to the correction flux Greens growth
+      integer IdcPPDiat   !    Pointer to the correction flux Diatoms growth
+      real(4) ConmxN      !    Total available nitrogen, minimally 0.0            (gN/m3)
+      real(4) ConmxP      !    Total available phosphorus, minimally 0.0          (gP/m3)
+      real(4) ConmxS      !    Total available silica, minimally 0.0              (gSi/m3)
+      real(4) N_demand    !    Nitrogen needed for this time step                 (gN/m3)
+      real(4) P_demand    !    Phosphorus needed for this time step               (gP/m3)
+      real(4) Si_demand   !    Silica needed for this time step                   (gSi/m3)
+      real(4) N_fact      !    available / demand N                               (-)
+      real(4) P_fact      !    available / demand P                               (-)
+      real(4) Si_fact     !    available / demand Si                              (-)
+      real(4) G_fact      !    available / demand N & P                           (-)
 
-      IP1  = IPOINT( 1)
-      IP2  = IPOINT( 2)
-      IP3  = IPOINT( 3)
-      IP4  = IPOINT( 4)
-      IP5  = IPOINT( 5)
-      IP6  = IPOINT( 6)
-      IP7  = IPOINT( 7)
-      IP8  = IPOINT( 8)
-      IP9  = IPOINT( 9)
-      IP10 = IPOINT(10)
-      IP11 = IPOINT(11)
-      IP12 = IPOINT(12)
-      IP13 = IPOINT(13)
-      IP14 = IPOINT(14)
-!
-      IFLUX = 0
-      DO 9000 ISEG = 1 , NOSEG
-!!    CALL DHKMRK(1,IKNMRK(ISEG),IKMRK1)
-!!    IF (IKMRK1.GT.0) THEN
-      IF (BTEST(IKNMRK(ISEG),0)) THEN
-!
+      ipnt        = ipoint
+      IdcPPGreeN  = 1
+      IdcPPDiat   = 2
 
-      PROD1     = PMSA(IP1 )
-      NCRAT1    = PMSA(IP2 )
-      PCRAT1    = PMSA(IP3 )
-      PROD2     = PMSA(IP4 )
-      NCRAT2    = PMSA(IP5 )
-      PCRAT2    = PMSA(IP6 )
-      SCRAT2    = PMSA(IP7 )
-      DELT      = PMSA(IP8 )
-      NH4       = PMSA(IP9 )
-      NO3       = PMSA(IP10)
-      PO4       = PMSA(IP11)
-      SI        = PMSA(IP12)
-!
-      CONMXN = AMAX1(NO3 + NH4,0.0)
-      CONMXP = AMAX1(PO4      ,0.0)
-      CONMXS = AMAX1(SI       ,0.0)
-      PRDMXN = CONMXN / NCRAT1 / DELT
-      PRDMXP = CONMXP / PCRAT1 / DELT
-      PRDMXA = AMIN1(PRDMXN,PRDMXP)
-      PRDMXD = AMIN1(PRDMXN,PRDMXP)
-      IF (SCRAT2.GT.0.) THEN
-        PRDMXS = CONMXS / SCRAT2 / DELT
-        PRDMXD = AMIN1(PRDMXD,PRDMXS)
-      ENDIF
-      PRDMX  = AMAX1(PRDMXA,PRDMXD)
-!
-!     THE PRODUCTION RATE OF BOTH DIATOMS AND ALGEA IS CORRECTED
-!     RELATIVE TO THEIR INITIAL VALUES.
-!     CHECK IF NEWLY CALCULATED RATES MATCH THE MAXIMUM GROWTH RATE
-!     ACCORDING TO THE NUTRIENT AVAILABILITY
-!
-      PRODTA = PROD1 + PROD2
-      IF (PRODTA .GT. PRDMX) THEN
-         PRODTA = PRDMX
-         PROD2C = AMIN1(PRDMX * PROD2 / (PROD2 + PROD1), PRDMXD)
-         PROD1C = PRODTA - PROD2C
-!
+      do 9000 iseg = 1 , noseg
+
+         if ( btest(iknmrk(iseg),0) ) then
+
+            fPPGreen   = pmsa( ipnt(  1) )
+            NCRatGreen = pmsa( ipnt(  2) )
+            PCRatGreen = pmsa( ipnt(  3) )
+            fPPDiat    = pmsa( ipnt(  4) )
+            NCRatDiat  = pmsa( ipnt(  5) )
+            PCRatDiat  = pmsa( ipnt(  6) )
+            SCRatDiat  = pmsa( ipnt(  7) )
+            DELT       = pmsa( ipnt(  8) )
+            NH4        = pmsa( ipnt(  9) )
+            NO3        = pmsa( ipnt( 10) )
+            PO4        = pmsa( ipnt( 11) )
+            Si         = pmsa( ipnt( 12) )
+
+            ConmxN = amax1( NO3+NH4, 0.0 )
+            ConmxP = amax1( PO4    , 0.0 )
+            ConmxS = amax1( Si     , 0.0 )
+
+            N_demand  = ( fPPDiat * NCratDiat + fPPGreen * NCRatGreen ) * DELT
+            P_demand  = ( fPPDiat * PCratDiat + fPPGreen * PCRatGreen ) * DELT
+            Si_demand = ( fPPDiat * SCratDiat ) * DELT
+
+            N_fact  = 1.0
+            P_fact  = 1.0
+            Si_fact = 1.0
+            if ( N_demand  .gt. ConmxN ) N_fact  = ConmxN / N_demand
+            if ( P_demand  .gt. ConmxP ) P_fact  = ConmxP / P_demand
+            if ( Si_demand .gt. ConmxS ) Si_fact = ConmxS / Si_demand
+            G_fact = MIN ( N_fact, P_fact )
+
+            if ( Si_fact .ge. G_fact ) then
+
+               fcPPGreen = G_fact  * fPPGreen
+               fcPPDiat  = G_fact  * fPPDiat
+
+            else
+
+               fcPPDiat  = Si_fact * fPPDiat
+               if ( G_fact .eq. 1.0 ) then
+                  fcPPGreen = fPPGreen
+               else
+                  ConmxN = ConmxN - fcPPDiat * NCratDiat * DELT
+                  ConmxP = ConmxP - fcPPDiat * PCratDiat * DELT
+                  N_demand = fPPGreen * NCRatGreen * DELT
+                  P_demand = fPPGreen * PCRatGreen * DELT
+                  N_fact  = 1.0
+                  P_fact  = 1.0
+                  if ( N_demand .gt. ConmxN ) N_fact  = ConmxN / N_demand
+                  if ( P_demand .gt. ConmxP ) P_fact  = ConmxP / P_demand
+                  fcPPGreen = MIN ( N_fact, P_fact ) * fPPGreen
+               endif
+
+            endif
+
 !     CORRECTION ON Nett primary production 1 and 2
-!
-         PMSA(IP13) = PROD1C
-         PMSA(IP14) = PROD2C
-         FL ( 1 + IFLUX )  = PROD1C - PROD1
-         FL ( 2 + IFLUX )  = PROD2C - PROD2
-!
-      ELSE
-!
-         PMSA(IP13) = PROD1
-         PMSA(IP14) = PROD2
-         FL ( 1 + IFLUX )  = 0.0
-         FL ( 2 + IFLUX )  = 0.0
-!
-      ENDIF
-!
-      ENDIF
-!
-      IFLUX = IFLUX + NOFLUX
-      IP1   = IP1   + INCREM (  1 )
-      IP2   = IP2   + INCREM (  2 )
-      IP3   = IP3   + INCREM (  3 )
-      IP4   = IP4   + INCREM (  4 )
-      IP5   = IP5   + INCREM (  5 )
-      IP6   = IP6   + INCREM (  6 )
-      IP7   = IP7   + INCREM (  7 )
-      IP8   = IP8   + INCREM (  8 )
-      IP9   = IP9   + INCREM (  9 )
-      IP10  = IP10  + INCREM ( 10 )
-      IP11  = IP11  + INCREM ( 11 )
-      IP12  = IP12  + INCREM ( 12 )
-      IP13  = IP13  + INCREM ( 13 )
-      IP14  = IP14  + INCREM ( 14 )
-!
- 9000 CONTINUE
-!
-      RETURN
-      END
+
+            dcPPGreeN = fcPPGreeN - fPPGreen
+            dcPPDiat  = fcPPDiat  - fPPDiat
+            fl  ( IdcPPGreeN  ) = dcPPGreeN
+            fl  ( IdcPPDiat   ) = dcPPDiat
+            pmsa( ipnt( 13)   ) = fcPPGreeN
+            pmsa( ipnt( 14)   ) = fcPPDiat
+
+         endif
+
+         IdcPPGreeN  = IdcPPGreeN  + noflux
+         IdcPPDiat   = IdcPPDiat   + noflux
+         ipnt        = ipnt        + increm
+
+ 9000 continue
+
+      return
+      end
--- src/engines_gpl/waq/packages/waq_kernel/src/waq_process/radalg.f
+++ src/engines_gpl/waq/packages/waq_kernel/src/waq_process/radalg.f
@@ -21,157 +21,136 @@
 !!  of Stichting Deltares remain the property of Stichting Deltares. All
 !!  rights reserved.
 
-      subroutine radalg ( pmsa   , fl     , ipoint , increm , noseg  ,
-     &                    noflux , iexpnt , iknmrk , noq1   , noq2   ,
-     &                    noq3   , noq4   )
+      subroutine RADALG     ( pmsa   , fl     , ipoint , increm, noseg ,
+     &                        noflux , iexpnt , iknmrk , noq1  , noq2  ,
+     &                        noq3   , noq4   )
 !>\file
 !>       Light efficiency function DYNAMO algae
+!>
+!>              Routine is called per algae species with species specific coefficients
+!>              Function returns 1.0 if:
+!>              - both light at top of cell and at bottom of cell are larger than saturation
+!>              - saturation value is zero
+!>              If light within the cell becomes below the saturation value, then for
+!>              that light a linear dependency between 0.0 and the saturation value is
+!>              assumed: mu(I) = I/Isat for I < Isat and 1.0 for I > Isat.\n
+!>              This (partly) linear lightcurve is analytically integrated over depth for
+!>              the given extinction coefficient.\n
+!>              If the extinction coefficient or the water depth is zero, like more or less
+!>              in a chemostat, then the function value for the given amount of light
+!>              is returned: either 1.0 or a value on the slope.
 
-!
-!     Description of the module :
-!
-!        General water quality module for DELWAQ:
-!        DYNAMIC ALGEA MODEL: GREEN, BLUE AND DIATOMS
-!
-! Name    T   L I/O   Description                                   Unit
-! ----    --- -  -    -------------------                            ---
-! DEPTH   R*4 1 I depth of the water column                            [
-! EFF     R*4 1 L average light efficiency green-algea                 [
-! ACTRAD  R*4 1 I radiation                                         [W/m
-! SATRAD  R*4 1 I radiation growth saturation green-algea           [W/m
+      implicit none
 
-!     Logical Units : -
+!     Type    Name         I/O Description
 
-!     Modules called : -
+      real(4) pmsa(*)     !I/O Process Manager System Array, window of routine to process library
+      real(4) fl(*)       ! O  Array of fluxes made by this process in mass/volume/time
+      integer ipoint(  6) ! I  Array of pointers in pmsa to get and store the data
+      integer increm(  6) ! I  Increments in ipoint for segment loop, 0=constant, 1=spatially varying
+      integer noseg       ! I  Number of computational elements in the whole model schematisation
+      integer noflux      ! I  Number of fluxes, increment in the fl array
+      integer iexpnt(4,*) ! I  From, To, From-1 and To+1 segment numbers of the exchange surfaces
+      integer iknmrk(*)   ! I  Active-Inactive, Surface-water-bottom, see manual for use
+      integer noq1        ! I  Nr of exchanges in 1st direction (the horizontal dir if irregular mesh)
+      integer noq2        ! I  Nr of exchanges in 2nd direction, noq1+noq2 gives hor. dir. reg. grid
+      integer noq3        ! I  Nr of exchanges in 3rd direction, vertical direction, pos. downward
+      integer noq4        ! I  Nr of exchanges in the bottom (bottom layers, specialist use only)
+      integer ipnt(  6)   !    Local work array for the pointering
+      integer iseg        !    Local loop counter for computational element loop
 
-!     Name     Type   Library
-!     ------   -----  ------------
+!***********************************************************************
+!
+!     Project : STANDAARDISATIE PROCES FORMULES T721.72
+!     Author  : Pascal Boderie
+!     Date    : 921210             Version : 0.01
+!
+!     History :
+!
+!     Date      Author          Description
+!     --------  --------------  --------------------------------------
+!     19921229  Pascal Boderie  Add third algae type, nutrient ratio's
+!                               per species
+!     20140530  Leo Postma      Allow for zero depth, zero extinction
+!                               and/or zero RadSat
+!
+!***********************************************************************
 
-      IMPLICIT REAL (A-H,J-Z)
+!     Type    Name         I/O Description                                         Unit
 
-      REAL     PMSA  ( * ) , FL    (*)
-      INTEGER  IPOINT( * ) , INCREM(*) , NOSEG , NOFLUX,
-     +         IEXPNT(4,*) , IKNMRK(*) , NOQ1, NOQ2, NOQ3, NOQ4
+      real(4) Depth       ! I  depth of segment                                    (m)
+      real(4) Rad         ! I  irradiation at the segment upper-boundary           (W/m2)
+      real(4) RadSat      ! I  total radiation growth saturation for this species  (W/m2)
+      real(4) ExtVl       ! I  total extinction coefficient visible light          (1/m)
+      real(4) TFGro       ! I  temperature function growth of this species <0-1>   (-)
+      real(4) LimRad      ! O  radiation limitation function of this species <0-1> (-)
+      logical LgtOpt      !    False if RadSat, Frad and LnFrad are equal for all cells
+      real(4) Frad        !    Saturation fraction: < 1.0 is under-saturation      (-)
+      real(4) LnFrad      !    natural logarithm of Frad                           (-)
+      real(4) ExtDpt      !    product of extinction and depth                     (-)
+      real(4) RadBot      !    radiation at the bottom of the cell                 (-)
 
-      LOGICAL  LGTOPT
-!
-      IN1  = INCREM( 1)
-      IN2  = INCREM( 2)
-      IN3  = INCREM( 3)
-      IN4  = INCREM( 4)
-      IN5  = INCREM( 5)
-      IN6  = INCREM( 6)
-!
-      IP1  = IPOINT( 1)
-      IP2  = IPOINT( 2)
-      IP3  = IPOINT( 3)
-      IP4  = IPOINT( 4)
-      IP5  = IPOINT( 5)
-      IP6  = IPOINT( 6)
-!
-      IF ( IN2 .EQ. 0 .AND. IN3 .EQ. 0 .AND. IN5 .EQ. 0 ) THEN
-         ACTRAD = PMSA(IP2 )
-         SATRAD = PMSA(IP3 )
-         TFGRO  = PMSA(IP5 )
-!
-!        Correct SATRAD for temperature using Temp function for growth
-!
-         SATRAD = TFGRO * SATRAD
-!     actuele straling / straling voor groei verzadiging
-         FRAD   = ACTRAD / SATRAD
-         IF (ACTRAD .GT. 1E-5) THEN
-            LNFRAD = LOG ( FRAD )
-         ELSE
-            LNFRAD = 0.0
-         ENDIF
-         LGTOPT = .FALSE.
-      ELSE
-         LGTOPT = .TRUE.
-      ENDIF
-!
-      IFLUX = 0
-      DO 9000 ISEG = 1 , NOSEG
-!!    CALL DHKMRK(1,IKNMRK(ISEG),IKMRK1)
-!!    IF (IKMRK1.EQ.1) THEN
-      IF (BTEST(IKNMRK(ISEG),0)) THEN
-!
-      IF ( LGTOPT ) THEN
-         ACTRAD = PMSA(IP2 )
-         SATRAD = PMSA(IP3 )
-         TFGRO  = PMSA(IP5 )
-!
-!        Correct SATRAD for temperature using Temp function for growth
-!
-         SATRAD = TFGRO * SATRAD
-!     actuele straling / straling voor groei verzadiging
-         FRAD   = ACTRAD / SATRAD
-         IF (ACTRAD .GT. 1E-5) THEN
-            LNFRAD = LOG ( FRAD )
-         ELSE
-            LNFRAD = 0.0
-         ENDIF
-      ENDIF
-!
-      DEPTH     = PMSA(IP1 )
-      EXTINC    = PMSA(IP4 )
-!
-      IF (SATRAD .LT. 1E-20 )  CALL ERRSYS ('SATRAD in RADALG zero', 1 )
-      IF (EXTINC .LT. 1E-20 )  CALL ERRSYS ('EXTINC in RADALG zero', 1 )
+      ipnt        = ipoint
 
-!     Light limitation functions (no inhibition assumed)
-!     CALL EFFIC( EFF  , RAD  , RAD1  , EXT , DEPTH )
-!     SUBROUTINE EFFIC( EFF , ACTRAD , SATRAD , EXTINC , DIEPTE )
-! ----------------------------------------------------------------------
-!     BEREKENING VAN DE GEMIDDELDE PRODUKTIE-EFFICIENTIE:
-!     INTEGRATIE VAN DE LICHTFUNKTIE VOOR DE PRODUKTIESNELHEID OVER DE
-!     DEPTH. DE FUNKTIE:
-!     F(X) = X ,ALS X < 1
-!     F(X) = 1 ,ALS X > 1
-!     HIERIN IS X DE LICHTINTENSITEIT GEDEELD DOOR DE LICHTINTENSITEIT
-!     WAARBIJ VERZADIGING OPTREEDT.
-!     ER MOET REKENING GEHOUDEN WORDEN MET DE LIGGING VAN DE LICHTNIVEAU
-!     AAN DE BEIDE GRENZEN: X(0), X(H) GROTER OF KLEINER DAN 1.
-! ----------------------------------------------------------------------
-!
-!     Local
-!
+      LgtOpt = .true.
+      if ( increm(2) .eq. 0 .and. increm(3) .eq. 0 .and. increm(5) .eq. 0 ) then
+         LgtOpt = .false.             !  This is constant for all cells
+         Rad    = pmsa( ipnt(  2) )
+         RadSat = pmsa( ipnt(  3) )
+         TFGro  = pmsa( ipnt(  5) )
+         RadSat = TFGro * RadSat      !  Correct RadSat for temperature
+         if ( RadSat .gt. 1e-20 ) then
+            Frad   = Rad / RadSat
+            LnFrad = 0.0
+            if ( Rad .gt. 1E-5 ) LnFrad = Log ( Frad )
+         endif
+      endif
 
-! ----Lichtniveau bij de bodem
-      EXTDPT = EXTINC * DEPTH
-      EXDIEP = EXP( - EXTDPT )
-      RADBOD = FRAD * EXDIEP
-!
-!---- Treedt er aan het oppervlak verzadiging op of niet
-      IF ( FRAD .GT. 1.0 ) GOTO 100
-!
-      PMSA(IP6 )  = ( FRAD - RADBOD ) / EXTDPT
-      GOTO 8900
-!
-!---- er treedt verzadiging op, maar hoe zit het aan de bodem
-  100 CONTINUE
-      IF ( RADBOD .GT. 1.0 ) GOTO 110
-!
-      PMSA(IP6 )  = ( 1.0 + LNFRAD - RADBOD ) / EXTDPT
-      GOTO 8900
-!
-!---- over de hele waterkolom treedt verzadiging op
-  110 CONTINUE
-      PMSA(IP6 )  =   1.0
+      do 9000 iseg = 1 , noseg
 
- 8900 CONTINUE
-!
-      ENDIF
-!
-      IFLUX = IFLUX + NOFLUX
-      IP1   = IP1   + IN1
-         IP2 = IP2 + IN2
-         IP3 = IP3 + IN3
-      IP4   = IP4   + IN4
-         IP5 = IP5 + IN5
-      IP6   = IP6   + IN6
-!
- 9000 CONTINUE
-!
-      RETURN
-!
-      END
+         if ( btest(iknmrk(iseg),0) ) then
+
+            if ( LgtOpt ) then
+               Rad    = pmsa( ipnt(  2) )
+               RadSat = pmsa( ipnt(  3) )
+               TFGro  = pmsa( ipnt(  5) )
+               RadSat = TFGro * RadSat
+               if ( RadSat .gt. 1e-20 ) then
+                  Frad   = Rad / RadSat
+                  LnFrad = 0.0
+                  if ( Rad .gt. 1E-5 ) LnFrad = Log ( Frad )
+               endif
+            endif
+
+            if ( RadSat .le. 1e-20 ) then
+               LimRad = 1.0
+            else
+               Depth = pmsa( ipnt(  1) )
+               ExtVl = pmsa( ipnt(  4) )
+               ExtDpt = ExtVl * Depth
+               if ( ExtDpt .le. 1.0e-10 ) then    !  No extinction, e.g. chemostat
+                  LimRad = min( Frad, 1.0 )
+               else
+                  RadBot = Frad * exp( - ExtDpt )
+                  if ( Frad .gt. 1.0 ) then       !  Saturation at the surface of the cell
+                     if ( RadBot .gt. 1.0 ) then
+                        LimRad = 1.0
+                     else
+                        LimRad = ( 1.0 + LnFrad - RadBot ) / ExtDpt
+                     endif
+                  else
+                     LimRad = ( Frad - RadBot ) / ExtDpt
+                  endif
+               endif
+            endif
+
+            pmsa( ipnt(  6)   ) = LimRad
+
+         endif
+
+         ipnt        = ipnt        + increm
+
+ 9000 continue
+
+      return
+      end subroutine
