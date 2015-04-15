fb72d976c462f89360e4bcc90167a07ea93cfb83
Michelle Jeuken <Michelle.Jeuken@deltares.nl>
2015-04-15 14:39:51 +0000

    Added extra stoichiometry definition to Evap_Conti: Prec        dEvapConti     1.00000

--- src/engines_gpl/waq/packages/waq_kernel/src/waq_tables/waqpb_export.bat
+++ src/engines_gpl/waq/packages/waq_kernel/src/waq_tables/waqpb_export.bat
@@ -1,6 +1,6 @@
 @echo off
 set version=5.03
-set serial=2015022401
+set serial=2015041401
 if exist ..\..\..\..\..\..\tools_gpl\waqpb\packages\waqpb_export\Release\waqpb_export.exe (
     echo This will execute: waqpb_export.exe -version%version% -serial%serial%
     echo.
