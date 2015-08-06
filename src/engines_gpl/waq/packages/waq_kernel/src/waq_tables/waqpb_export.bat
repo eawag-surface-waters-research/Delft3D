929e337fb7427a306cf48aec01850444f47cbb8a
Michelle Jeuken <Michelle.Jeuken@deltares.nl>
2015-08-06 09:21:25 +0000

    Issue DELFT3D-34082: There is a hard coded minimum value in the proces consbl that should be turned into a parameter Default for SWCLim should be 0 instead of 1 (no carbon limitation)

--- src/engines_gpl/waq/packages/waq_kernel/src/waq_tables/waqpb_export.bat
+++ src/engines_gpl/waq/packages/waq_kernel/src/waq_tables/waqpb_export.bat
@@ -1,6 +1,6 @@
 @echo off
 set version=5.03
-set serial=2015080501
+set serial=2015080601
 if exist ..\..\..\..\..\..\tools_gpl\waqpb\packages\waqpb_export\Release\waqpb_export.exe (
     echo This will execute: waqpb_export.exe -version%version% -serial%serial%
     echo.
