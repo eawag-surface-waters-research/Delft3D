1b0fd47e7f2f8e151c400b48881cef312af19366
Qinghua Ye <Qinghua.Ye@deltares.nl>
2015-09-29 10:25:45 +0000

    Cmd line for teamcity compilation for win 64-bit, again

--- src/build.cmd
+++ src/build.cmd
@@ -1,5 +1,5 @@
 del build.log
-call "%VS110COMNTOOLS%\..\..\VC\vcvarsall.bat"" amd64
+call "%VS110COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
   rem the path to devenv.exe is now added to PATH: no full path specificitation needed on next line.
 devenv.exe delft3d_open.sln /Build Release /Out build.log
 third_party_open\commandline\bin\win32\sed.exe -e "/[Ee]rror[\:\ ]/s/^/\#\#teamcity\[buildStatus status\=\'FAILURE\' text\=\' /g;/buildStatus/s/$/\'\]/g" build.log 
