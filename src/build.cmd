e7eb2455f0b938202a50278ce91338bb180293aa
Qinghua Ye <Qinghua.Ye@deltares.nl>
2015-09-29 08:52:30 +0000

    Cmd line for teamcity compilation.

--- src/build.cmd
+++ src/build.cmd
@@ -1,5 +1,5 @@
 del build.log
-call "%VS90COMNTOOLS%\vsvars32.bat"
+call "%VS110COMNTOOLS%\vsvars64.bat"
   rem the path to devenv.exe is now added to PATH: no full path specificitation needed on next line.
-devenv.exe ds.sln /Build Release /Out build.log
-open_third_party\commandline\bin\win32\sed.exe -e "/[Ee]rror[\:\ ]/s/^/\#\#teamcity\[buildStatus status\=\'FAILURE\' text\=\' /g;/buildStatus/s/$/\'\]/g" build.log 
+devenv.exe delft3d_open.sln /Build Release /Out build.log
+third_party_open\commandline\bin\win32\sed.exe -e "/[Ee]rror[\:\ ]/s/^/\#\#teamcity\[buildStatus status\=\'FAILURE\' text\=\' /g;/buildStatus/s/$/\'\]/g" build.log 
