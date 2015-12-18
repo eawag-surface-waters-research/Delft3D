53b48a042730a996d3efb70a1d8605fc6272e2ea
Jing Zhao <Jing.Zhao@deltares.nl>
2015-12-18 15:22:32 +0000

    Added the functionality that users are allowed to define the value of eps in the interface comparereal.

--- src/utils_lgpl/deltares_common/packages/deltares_common/src/precision_basics.F90
+++ src/utils_lgpl/deltares_common/packages/deltares_common/src/precision_basics.F90
@@ -51,10 +51,11 @@ integer, parameter :: long = SELECTED_INT_KIND(16)
   end interface
 contains
 
-function comparerealdouble(val1, val2)
+function comparerealdouble(val1, val2, eps)
 !!--description-----------------------------------------------------------------
 !
 ! Compares two double precision numbers
+! Allow users to define the value of eps. If not, eps equals to the default machine eps
 !
 ! Return value: -1 if val1 < val2
 !                0 if val1 = val2
@@ -78,17 +79,22 @@ integer :: comparerealdouble
 !
 ! Global variables
 !
-real(hp), intent(in) :: val1
-real(hp), intent(in) :: val2
+real(hp), intent(in)           :: val1
+real(hp), intent(in)           :: val2
+real(hp), optional, intent(in) :: eps
 !
 ! Local variables
 !
-real(hp) :: eps
+real(hp) :: eps0
 real(hp) :: value
 !
 !! executable statements -------------------------------------------------------
 !
-eps = 2.0_hp * epsilon(val1)
+if (present(eps)) then
+    eps0 = eps
+else 
+    eps0 = 2.0_hp * epsilon(val1)
+endif
 !
 if (abs(val1)<1.0_hp .or. abs(val2)<1.0_hp) then
    value = val1 - val2
@@ -96,7 +102,7 @@ else
    value = val1/val2 - 1.0_hp
 endif
 !
-if (abs(value)<eps) then
+if (abs(value)<eps0) then
    comparerealdouble = 0
 elseif (val1<val2) then
    comparerealdouble = -1
@@ -107,13 +113,14 @@ end function comparerealdouble
 
 
 
-function comparerealsingle(val1, val2)
+function comparerealsingle(val1, val2,eps)
 !!--description-----------------------------------------------------------------
 !
 ! REMARK: THE NAME OF THIS FUNCTION IS WRONG!
 !         The name should be comparefp
 !
 ! Compares two real numbers of type fp
+! Allow users to define the value of eps. If not, eps equals to the default machine eps
 !
 ! Return value: -1 if val1 < val2
 !                0 if val1 = val2
@@ -137,17 +144,23 @@ integer :: comparerealsingle
 !
 ! Global variables
 !
-real(sp), intent(in)  :: val1
-real(sp), intent(in)  :: val2
+real(sp), intent(in)           :: val1
+real(sp), intent(in)           :: val2
+real(sp), optional, intent(in) :: eps
 !
 ! Local variables
 !
-real(sp) :: eps
+real(sp) :: eps0
 real(sp) :: value
 !
 !! executable statements -------------------------------------------------------
 !
-eps = 2.0_sp * epsilon(val1)
+!  
+if (present(eps)) then
+    eps0 = eps
+else
+    eps0 = 2.0_sp * epsilon(val1)
+endif
 !
 if (abs(val1)<1.0_sp .or. abs(val2)<1.0_sp) then
    value = val1 - val2
@@ -155,7 +168,7 @@ else
    value = val1/val2 - 1.0_sp
 endif
 !
-if (abs(value)<eps) then
+if (abs(value)<eps0) then
    comparerealsingle = 0
 elseif (val1<val2) then
    comparerealsingle = -1
