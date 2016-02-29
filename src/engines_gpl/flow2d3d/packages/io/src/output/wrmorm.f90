44f30c91bafc14d1a08ecf8b80c5a9cfbf78e27a
Bert Jagers <bert.jagers@deltares.nl>
2016-02-29 17:28:55 +0000

    UNST-1043: Prepare for generic NetCDF visualization: integrate WRMORM1 and WRMORM2 into WRMORM since output quantities are now equal. Add axis/positive attributes to DP_BEDLYR.

--- src/engines_gpl/flow2d3d/packages/io/src/output/wrmorm.f90
+++ src/engines_gpl/flow2d3d/packages/io/src/output/wrmorm.f90
@@ -41,7 +41,9 @@ subroutine wrmorm(lundia    ,error     ,mmax      ,nmaxus    ,lsedtot   , &
     use precision
     use bedcomposition_module
     use globaldata
-    use dfparall
+    use dfparall, only: nproc
+    use wrtarray, only: wrtarray_nml, wrtarray_nmll
+    use datagroups
     !
     implicit none
     !
@@ -70,7 +72,11 @@ subroutine wrmorm(lundia    ,error     ,mmax      ,nmaxus    ,lsedtot   , &
 !
 ! Local variables
 !
-    integer                                       :: istat
+    integer                             , pointer :: celidt
+    type (datagroup)                    , pointer :: group
+    integer                             , pointer :: nmaxgl
+    integer                             , pointer :: mmaxgl
+    !
     integer                             , pointer :: iporos
     integer                             , pointer :: iunderlyr
     integer                             , pointer :: nlyr
@@ -81,6 +87,27 @@ subroutine wrmorm(lundia    ,error     ,mmax      ,nmaxus    ,lsedtot   , &
     real(fp)         , dimension(:,:)   , pointer :: svfrac
     real(fp)         , dimension(:,:,:) , pointer :: msed
     real(fp)         , dimension(:,:)   , pointer :: thlyr
+    !
+    integer                                       :: ierror      ! Local error flag
+    integer                                       :: istat
+    integer                                       :: i
+    integer                                       :: k
+    integer                                       :: l
+    integer                                       :: m
+    integer                                       :: n
+    integer                                       :: nm
+    real(fp)                                      :: dens
+    real(fp)   , dimension(:,:,:)  , allocatable  :: rbuff3
+    real(fp)   , dimension(:,:,:,:), allocatable  :: rbuff4
+    !
+    integer                                       :: idatt_z
+    integer                                       :: idatt_down
+    !
+    integer                                       :: iddim_n
+    integer                                       :: iddim_m
+    integer                                       :: iddim_lsedtot
+    integer                                       :: iddim_nlyr
+    integer                                       :: iddim_nlyrp1
 !
 !! executable statements -------------------------------------------------------
 !
@@ -93,33 +120,180 @@ subroutine wrmorm(lundia    ,error     ,mmax      ,nmaxus    ,lsedtot   , &
        call d3stop(1, gdp)
     endif
     !
+    istat = bedcomp_getpointer_integer(gdp%gdmorlyr,'nlyr',nlyr)
+    if (istat==0) istat = bedcomp_getpointer_integer(gdp%gdmorlyr,'iporosity',iporos)
     select case (iunderlyr)
     case (1)
-       istat = bedcomp_getpointer_realprec(gdp%gdmorlyr,'bodsed',bodsed)
+       if (istat==0) istat = bedcomp_getpointer_realprec(gdp%gdmorlyr,'bodsed',bodsed)
        if (istat==0) istat = bedcomp_getpointer_realfp(gdp%gdmorlyr,'dpsed',dpsed)
-       if (istat/=0) then
-          call prterr(lundia, 'U021', 'Memory problem in WRMORM')
-          call d3stop(1, gdp)
-       endif
-       call wrmorm1   (lundia    ,error     ,mmax      ,nmaxus    ,lsedtot   , &
-                     & irequest  ,fds       ,grpnam    ,bodsed    ,dpsed     , &
-                     & filename  ,gdp       ,filetype  ,cdryb     , &
-                     & mf        ,ml        ,nf        ,nl        ,iarrc     )
     case (2)
-       istat = bedcomp_getpointer_integer(gdp%gdmorlyr,'nlyr',nlyr)
-       if (istat==0) istat = bedcomp_getpointer_integer(gdp%gdmorlyr,'iporosity',iporos)
        if (istat==0) istat = bedcomp_getpointer_realfp (gdp%gdmorlyr,'svfrac',svfrac)
        if (istat==0) istat = bedcomp_getpointer_realfp (gdp%gdmorlyr,'msed',msed)
        if (istat==0) istat = bedcomp_getpointer_realfp (gdp%gdmorlyr,'thlyr',thlyr)
-       if (istat/=0) then
-          call prterr(lundia, 'U021', 'Memory problem in WRMORM')
-          call d3stop(1, gdp)
-       endif
-       call wrmorm2   (lundia    ,error     ,mmax      ,nmaxus    ,lsedtot   , &
-                     & nlyr      ,irequest  ,fds       ,grpnam    ,msed      , &
-                     & thlyr     ,svfrac    ,iporos    ,cdryb     ,rhosol    , &
-                     & filename  ,gdp       ,filetype  , &
-                     & mf        ,ml        ,nf        ,nl        ,iarrc     )
     case default
     end select
+    if (istat/=0) then
+       call prterr(lundia, 'U021', 'Memory problem in WRMORM')
+       call d3stop(1, gdp)
+    endif
+    !
+    call getdatagroup(gdp, FILOUT_MAP, grpnam, group)
+    celidt         => group%celidt
+    mmaxgl         => gdp%gdparall%mmaxgl
+    nmaxgl         => gdp%gdparall%nmaxgl
+    !
+    select case (irequest)
+    case (REQUESTTYPE_DEFINE)
+       !
+       ! Define dimensions
+       !
+       iddim_n       = adddim(gdp, lundia, FILOUT_MAP, 'N'      , nmaxgl        ) ! Number of N-grid points (cell centres)
+       iddim_m       = adddim(gdp, lundia, FILOUT_MAP, 'M'      , mmaxgl        ) ! Number of M-grid points (cell centres)
+       iddim_lsedtot = adddim(gdp, lundia, FILOUT_MAP, 'LSEDTOT', lsedtot       ) ! Number of total sediment fractions
+       iddim_nlyr    = adddim(gdp, lundia, FILOUT_MAP, 'nlyr'   , nlyr          ) ! Number of bed layers
+       iddim_nlyrp1  = adddim(gdp, lundia, FILOUT_MAP, 'nlyrp1' , nlyr+1        ) ! Number of bed layer interfaces
+       !
+       ! Define attributes
+       !
+       idatt_z    = addatt(gdp, lundia, FILOUT_MAP, 'axis','Z')
+       idatt_down = addatt(gdp, lundia, FILOUT_MAP, 'positive','down')
+       !
+       ! Define elements
+       !
+       call addelm(gdp, lundia, FILOUT_MAP, grpnam, 'MSED', ' ', IO_REAL4     , 4, dimids=(/iddim_n, iddim_m, iddim_nlyr, iddim_lsedtot/), longname='Mass of sediment in layer', unit='kg/m2', acl='z')
+       call addelm(gdp, lundia, FILOUT_MAP, grpnam, 'LYRFRAC', ' ', IO_REAL4  , 4, dimids=(/iddim_n, iddim_m, iddim_nlyr, iddim_lsedtot/), longname='Volume fraction of sediment in layer', acl='z')
+       call addelm(gdp, lundia, FILOUT_MAP, grpnam, 'DP_BEDLYR', ' ', IO_REAL4  , 3, dimids=(/iddim_n, iddim_m, iddim_nlyrp1/), longname='Vertical position of sediment layer interface', unit='m', attribs=(/idatt_z,idatt_down/), acl='z')
+       if (iporos>0) then
+          call addelm(gdp, lundia, FILOUT_MAP, grpnam, 'EPSPOR', ' ', IO_REAL4, 3, dimids=(/iddim_n, iddim_m, iddim_nlyr/), longname='Porosity coefficient', acl='z')
+       endif
+    case (REQUESTTYPE_WRITE)
+       !
+       ! Write data to file
+       !
+       ! element 'MSED'
+       !
+       allocate( rbuff4(gdp%d%nlb:gdp%d%nub, gdp%d%mlb:gdp%d%mub, nlyr, lsedtot) )
+       select case (iunderlyr)
+       case (1)
+          do l = 1, lsedtot
+             do m = 1, mmax
+                do n = 1, nmaxus
+                   call n_and_m_to_nm(n, m, nm, gdp)
+                   rbuff4(n, m, 1, l) = real(bodsed(l, nm),fp)
+                enddo
+             enddo
+          enddo
+       case (2)
+          do l = 1, lsedtot
+             do k = 1, nlyr
+                do m = 1, mmax
+                   do n = 1, nmaxus
+                      call n_and_m_to_nm(n, m, nm, gdp)
+                      rbuff4(n, m, k, l) = msed(l, k, nm)
+                   enddo
+                enddo
+             enddo
+          enddo
+       end select
+       call wrtarray_nmll(fds, filename, filetype, grpnam, celidt, &
+                     & nf, nl, mf, ml, iarrc, gdp, nlyr, lsedtot, &
+                     & ierror, lundia, rbuff4, 'MSED')
+       deallocate(rbuff4)
+       if (ierror /= 0) goto 9999
+       !
+       ! element 'LYRFRAC'
+       !
+       allocate( rbuff4(gdp%d%nlb:gdp%d%nub, gdp%d%mlb:gdp%d%mub, nlyr, lsedtot) )
+       select case (iunderlyr)
+       case (1)
+          do l = 1, lsedtot
+             do m = 1, mmax
+                do n = 1, nmaxus
+                   call n_and_m_to_nm(n, m, nm, gdp)
+                   if (dpsed(nm)>0.0_fp) then
+                        rbuff4(n, m, 1, l) = real(bodsed(l, nm),fp)/(cdryb(l)*dpsed(nm))
+                   else
+                        rbuff4(n, m, 1, l) = 0.0_fp
+                   endif
+                enddo
+             enddo
+          enddo
+       case (2)
+          do l = 1, lsedtot
+             if (iporos==0) then
+                dens = cdryb(l)
+             else
+                dens = rhosol(l)
+             endif
+             do k = 1, nlyr
+                do m = 1, mmax
+                   do n = 1, nmaxus
+                      call n_and_m_to_nm(n, m, nm, gdp)
+                      if (thlyr(k,nm)>0.0_fp) then
+                           rbuff4(n, m, k, l) = msed(l, k, nm)/(dens*svfrac(k, nm)*thlyr(k, nm))
+                      else
+                           rbuff4(n, m, k, l) = 0.0_fp
+                      endif
+                   enddo
+                enddo
+             enddo
+          enddo
+       end select
+       call wrtarray_nmll(fds, filename, filetype, grpnam, celidt, &
+                     & nf, nl, mf, ml, iarrc, gdp, nlyr, lsedtot, &
+                     & ierror, lundia, rbuff4, 'LYRFRAC')
+       deallocate(rbuff4)
+       if (ierror /= 0) goto 9999
+       !
+       ! element 'DP_BEDLYR'
+       !
+       allocate( rbuff3(gdp%d%nlb:gdp%d%nub, gdp%d%mlb:gdp%d%mub, nlyr+1) )
+       rbuff3(:, :, 1) = 0.0_fp
+       select case (iunderlyr)
+       case (1)
+          do m = 1, mmax
+             do n = 1, nmaxus
+                call n_and_m_to_nm(n, m, nm, gdp)
+                rbuff3(n, m, 2) = dpsed(nm)
+             enddo
+          enddo
+       case (2)
+          do k = 1, nlyr
+             do m = 1, mmax
+                do n = 1, nmaxus
+                   call n_and_m_to_nm(n, m, nm, gdp)
+                   rbuff3(n, m, k+1) = rbuff3(n, m, k) + thlyr(k, nm)
+                enddo
+             enddo
+          enddo
+       end select
+       call wrtarray_nml(fds, filename, filetype, grpnam, celidt, &
+                     & nf, nl, mf, ml, iarrc, gdp, nlyr+1, &
+                     & ierror, lundia, rbuff3, 'DP_BEDLYR')
+       deallocate(rbuff3)
+       if (ierror /= 0) goto 9999
+       !
+       ! element 'EPSPOR'
+       !
+       if (iporos>0) then
+          allocate( rbuff3(gdp%d%nlb:gdp%d%nub, gdp%d%mlb:gdp%d%mub, nlyr) )
+          rbuff3(:, :, :) = -999.0_fp
+          do k = 1, nlyr
+             do m = 1, mmax
+                do n = 1, nmaxus
+                   call n_and_m_to_nm(n, m, nm, gdp)
+                   rbuff3(n, m, k) = 1.0_fp - svfrac(k, nm)
+                enddo
+             enddo
+          enddo
+          call wrtarray_nml(fds, filename, filetype, grpnam, celidt, &
+                        & nf, nl, mf, ml, iarrc, gdp, nlyr, &
+                        & ierror, lundia, rbuff3, 'EPSPOR')
+          deallocate(rbuff3)
+          if (ierror /= 0) goto 9999
+       endif
+       !
+ 9999  continue
+       if (ierror/= 0) error = .true.
+    endselect
 end subroutine wrmorm
