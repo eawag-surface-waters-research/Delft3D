1fdcb99148f462aa146f94af3b104fbbddc6c46a
Adri Mourits <Adri.Mourits@deltares.nl>
2016-11-29 14:27:53 +0000

    Dimr: Added: C# testprogram

---
+++ src/engines_gpl/dimr/packages/DimrTesting/DimrDllNative.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DimrTesting
+{
+    public class DimrDllNative
+    {
+        const string dllName = @"c:\code\oss\src\engines_gpl\dimr\bin\x64\Debug\dimr_dll.dll";
+
+        [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int initialize(string filename);
+
+        [DllImport(dllName)]
+        public static extern void finalize();
+
+        [DllImport(dllName)]
+        public static extern void update(double dt);
+
+        [DllImport(dllName)]
+        public static extern void get_start_time(ref double dt);
+
+        [DllImport(dllName)]
+        public static extern void get_end_time(ref double dt);
+
+        [DllImport(dllName)]
+        public static extern void get_current_time(ref double dt);
+
+        [DllImport(dllName)]
+        public static extern void get_var(StringBuilder locationId, int LocationIdLength, double[] value);
+
+        [DllImport(dllName)]
+        public static extern void set_var(StringBuilder locationId, int LocationIdLength, double[] value);
+    }
+}
---
+++ src/engines_gpl/dimr/packages/DimrTesting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DimrTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBasicModelInterface model = new Sobek3ModelWrapper();
+            model.Initialize(@"dimr.xml");
+            model.Update(600.0);
+            model.Finish();
+        }
+    }
+}
