97d7b84729a5661bf03c46d0751f28fde3c1c338
Bert Jagers <bert.jagers@deltares.nl>
2017-01-09 15:33:49 +0000

    Update OpenStreetMap WMS request

--- src/tools_lgpl/matlab/quickplot/progsrc/private/wms.m
+++ src/tools_lgpl/matlab/quickplot/progsrc/private/wms.m
@@ -76,8 +76,11 @@ switch servername
         INFO.YOrigin           = 'top';
         INFO.Servers           = {''};
     case 'openstreetmap'
-        INFO = getcap('http://129.206.228.72/cached/osm?');
-        INFO.Data.Layer(2:end) = [];
+        INFO.GetMap.URL = 'http://${s}.tile.openstreetmap.org/${z}/${x}/${y}.png';
+        INFO.GetMap.TileLevels = 19;
+        INFO.YOrigin           = 'top';
+        INFO.Servers           = {'a','b','c'};
+        INFO.GetMap.Formats = {'image/png'};
         %wms_layer='osm_auto:all'; % <-- only support this one
    	    %wms_layer='europe_wms:hs_srtm_europa';
     %case 'ahn'
