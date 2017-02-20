3edb0b4bf0305e09a76cd8ad4388aacfd8f46c74
kockx <kockx>
2017-02-20 16:16:48 +0000

    UNST-1151: io_netcdf: Added method transform_coordinates. This uses the proj4 library for coordinate transformations.

--- src/utils_lgpl/io_netcdf/packages/io_netcdf/src/io_netcdf.f90
+++ src/utils_lgpl/io_netcdf/packages/io_netcdf/src/io_netcdf.f90
@@ -699,6 +699,67 @@ function ionc_initialize(c_msg_callback,c_prgs_callback) result(ierr)
    ierr = IONC_NOERR
 end function ionc_initialize
 
+!> Transforms the given coordinates from the given source coordinate system to the given destination coordinate system.
+!! This subroutine uses the proj4 library for coordinate transformations.
+subroutine transform_coordinates(src_proj_string, dst_proj_string, src_x, src_y, dst_x, dst_y)
+   use proj
+
+   implicit none
+
+   character(len=*),            intent(in)  :: src_proj_string !< proj4 string describing source coordinate system.
+   character(len=*),            intent(in)  :: dst_proj_string !< proj4 string describing destination coordinate system.
+   real(kind=dp), dimension(:), intent(in)  :: src_x !< x coordinates to transform.
+   real(kind=dp), dimension(:), intent(in)  :: src_y !< y coordinates to transform.
+   real(kind=dp), dimension(:), intent(out) :: dst_x !< transformed x coordinates.
+   real(kind=dp), dimension(:), intent(out) :: dst_y !< transformed y coordinates.
+
+   type(pj_object) :: src_projection !< source coordinate system object.
+   type(pj_object) :: dst_projection !< destination coordinate system object.
+   integer :: error
+   character(len=1024) :: message !< Temporary variable for writing log messages.
+
+   ! Copy coordinates to dst_x, dst_y.
+   ! This code assumes that dst_x and dst_y have already been allocated and have the same length as src_x and src_y.
+   dst_x = src_x
+   dst_y = src_y
+
+   ! Get projections.
+   src_projection = pj_init_plus(trim(src_proj_string)//char(0))
+   if (.not. pj_associated(src_projection)) then
+      write(message, *) 'Cannot initialize source projection for proj4 string: "', src_proj_string, '". Coordinates will not be transformed.'
+      call mess(LEVEL_ERROR, trim(message))
+      return
+   endif
+   dst_projection = pj_init_plus(trim(dst_proj_string)//char(0))
+   if (.not. pj_associated(dst_projection)) then
+      write(message, *) 'Cannot initialize destination projection for proj4 string: "', dst_proj_string, '". Coordinates will not be transformed.'
+      call mess(LEVEL_ERROR, trim(message))
+      return
+   endif
+
+   ! Convert to radians.
+   dst_x = dst_x*pj_deg_to_rad
+   dst_y = dst_y*pj_deg_to_rad
+
+   ! Transform coordinates in place in dst_x, dst_y.
+   error = pj_transform_f(src_projection, dst_projection, dst_x, dst_y)
+   if (error /= 0) then ! If error.
+      ! Put back original coordinates.
+      dst_x = src_x
+      dst_y = src_y
+      write(message, *) 'Error (', error, ') while transforming coordinates from "', trim(src_proj_string), '" to "', trim(dst_proj_string), '". Coordinates will not be transformed.'
+      call mess(LEVEL_ERROR, trim(message))
+      return
+   endif
+
+   ! Convert to degrees.
+   dst_x = dst_x*pj_rad_to_deg
+   dst_y = dst_y*pj_rad_to_deg
+
+   call pj_free(src_projection)
+   call pj_free(dst_projection)
+end subroutine
+
 !
 ! -- Private routines -----------------------------------------------------
 !
