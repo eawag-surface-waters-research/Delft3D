cb3b402f99cc84a813aedcf8ace7fab246fb3df0
Michelle Jeuken <Michelle.Jeuken@deltares.nl>
2017-03-09 12:58:04 +0000

    Issue DELFT3D-35104 PLCT crashes when PO4 is added to the list of substances, even when there are no processes. The .0 file from the PLCT uses the keyword 'active', but also reads the description of the processes word by word. The new grazing process contains the word active in it's description which lead to a crash. Because the reading is case sensitive, capitalisation of active is a quick workaround. The PLCT will be fixed later.

--- src/engines_gpl/waq/packages/waq_kernel/src/waq_tables/waqpb_export.bat
+++ src/engines_gpl/waq/packages/waq_kernel/src/waq_tables/waqpb_export.bat
@@ -1,6 +1,6 @@
 @echo off
 set version=5.06
-set serial=2017013001
+set serial=2017030901
 if exist ..\..\..\..\..\..\tools_gpl\waqpb\packages\waqpb_export\Release\waqpb_export.exe (
     echo This will execute: waqpb_export.exe -version%version% -serial%serial%
     echo.
