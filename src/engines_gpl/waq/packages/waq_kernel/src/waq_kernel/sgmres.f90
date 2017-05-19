1c9ff30268f19335f92b1159f668aa93e4fffb86
Arjen Markus <Arjen.Markus@deltares.nl>
2017-05-19 11:35:19 +0000

    Solve a small but nasty error: in special circumstances the variable ITER could be used before it was set. One debug session brought this to light.

--- src/engines_gpl/waq/packages/waq_kernel/src/waq_kernel/sgmres.f90
+++ src/engines_gpl/waq/packages/waq_kernel/src/waq_kernel/sgmres.f90
@@ -111,6 +111,7 @@
 
 !        sloppy way of output
 
+      ITER = 0
       IERR = 0
       IF (FIRST) THEN
          FIRST = .FALSE.
@@ -190,7 +191,6 @@
       ENDIF
       IF ( RESID .LT. TOL ) GOTO 70
 
-      ITER = 0
 
 
 !     Main GMRES iteration loop
