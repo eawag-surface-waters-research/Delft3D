206eb13f099eabe5c39839e841d169bdea143cd9
Luca Carniato <Luca.Carniato@deltares.nl>
2018-01-24 12:20:14 +0000

    UNST-1705, Remove duplicate code in gridgeom: add imiss to missing values

--- src/utils_lgpl/gridgeom/packages/gridgeom/src/generalmodules.f90
+++ src/utils_lgpl/gridgeom/packages/gridgeom/src/generalmodules.f90
@@ -52,6 +52,7 @@
    double precision                  :: dxymis          = -999d0
    !double precision                 :: ieee_negative_inf = -1.7976931348623158e+308 ! IEEE standard for the maximum negative value
    integer                           :: intmiss         = -2147483647 ! integer fillvlue
+   integer                           :: imiss           = -999        ! cf_dll missing value 
    integer                           :: LMOD, KMOD                    ! TBV READDY, LC gui related variables can go to unstruc_display
    integer                           :: jins            = 1
    integer                           :: jadelnetlinktyp = 0
