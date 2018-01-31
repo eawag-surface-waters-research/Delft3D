551f9b2f58e77ed21a8d1f0e655f4d0d4e740cda
Adri Mourits <Adri.Mourits@deltares.nl>
2018-01-31 10:26:29 +0000

    DELFT3D-35990: New delivery directory structure, Windows: Added: examples/12_dflowfm/test_data/e02_f14_c040_westerscheldt Added: run.bat script in examples/12_dflowfm/test_data/e100_*

---
+++ examples/12_dflowfm/test_data/e02_f14_c040_westerscheldt/run.bat
@@ -0,0 +1,7 @@
+@ echo off
+
+call ..\..\..\..\src\bin\x64\dimr\scripts\run_dimr_parallel.bat 3 dimr_config.xml
+
+
+    rem To prevent the DOS box from disappearing immediately: remove the rem on the following line
+rem pause
