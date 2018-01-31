fea88dc0ac936332a6d1d51c31f9399d6b285209
Adri Mourits <Adri.Mourits@deltares.nl>
2018-01-31 16:36:03 +0000

    DELFT3D-35990: New delivery directory structure, Windows: examples/01_standard/run_parallel.bat: Comments added about starting smpd

--- examples/01_standard/run_parallel.bat
+++ examples/01_standard/run_parallel.bat
@@ -1,5 +1,19 @@
 @ echo off
 
+
+    rem When using mpich2 for the first time on a machine:
+    rem Execute "smpd -install" as administrator:
+    rem     "Start" -> "All programs" -> "Accessories", right-click "Command Prompt", "Run as Administrator"
+    rem     In this command box:
+    rem         cd ...\src\bin\x64\share\bin
+    rem         (or when not compiled yet)
+    rem         cd ...\src\third_party_open\mpich2\bin
+    rem         smpd -install
+    rem     When there is an smpd already running on the machine, it must be ended first, using the Microsoft Task Manager
+
+
+
+
     rem This example testcase is that small, that it can run with a maximum of 5 partitions
 set NPROC=%NUMBER_OF_PROCESSORS%
 if %NPROC% gtr 5 set NPROC=5
