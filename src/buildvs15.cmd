46ff9bc427c60008dd341f2a2b003500e848a375
Adri Mourits <Adri.Mourits@deltares.nl>
2018-04-13 14:14:55 +0000

    Single precision scripts on Windows: Use "...\src\third_party_open\tcl\bin\win64\tclkitsh852.exe" instead of "...\src\third_party_open\tcl\bin\win32\tclkit.exe", which had to be removed due to Kaspersky false hits.

--- src/buildvs15.cmd
+++ src/buildvs15.cmd
@@ -1,4 +1,4 @@
-@ echo on
+@ echo off
 
 setlocal enabledelayedexpansion
 
@@ -11,11 +11,23 @@ cd %~dp0
 if [%1] EQU [/sp] (
     cd utils_lgpl\deltares_common\scripts
     call singleprecision.bat
+    if NOT %ErrorLevel% EQU 0 (
+        echo "Error in executing singleprecision.bat: %ErrorLevel%"
+        set globalErrorLevel=%ErrorLevel%
+        cd %~dp0
+        goto end
+    )
     cd %~dp0
 )
 if [%1] EQU [/hp] (
     cd utils_lgpl\deltares_common\scripts
     call doubleprecision.bat
+    if NOT %ErrorLevel% EQU 0 (
+        echo "Error in executing doubleprecision.bat: %ErrorLevel%"
+        set globalErrorLevel=%ErrorLevel%
+        cd %~dp0
+        goto end
+    )
     cd %~dp0
 )
   
@@ -68,6 +80,7 @@ third_party_open\commandline\bin\win32\sed.exe -e "/[Ee]rror[\:\ ]/s/^/\#\#teamc
 third_party_open\commandline\bin\win32\sed.exe -e "/[Ee]rror[\:\ ]/s/^/\#\#teamcity\[buildStatus status\=\'FAILURE\' text\=\' /g;/buildStatus/s/$/\'\]/g" build_nefis.log 
 
 
+:end
 
 if NOT %globalErrorLevel% EQU 0 (
     echo An error occurred in one or more compilation steps
