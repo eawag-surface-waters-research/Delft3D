9f1d84d159a0ed2550c8de019db446c25ea6b126
Adri Mourits <Adri.Mourits@deltares.nl>
2018-04-18 09:54:10 +0000

    DELFT3D-35990: New delivery directory structure: ESMF_RegridWeightGen_in_Delft3D-WAVE.sh: "D3D_HOME/share/esmf" added to $LD_LIBRARY_PATH Needed by docker

--- src/third_party_open/esmf/lnx64/scripts/ESMF_RegridWeightGen_in_Delft3D-WAVE.sh
+++ src/third_party_open/esmf/lnx64/scripts/ESMF_RegridWeightGen_in_Delft3D-WAVE.sh
@@ -53,7 +53,9 @@ scriptdirname=`readlink \-f \$0`
 scriptdir=`dirname $scriptdirname`
 D3D_HOME=$scriptdir/..
 regridexec=$D3D_HOME/bin/ESMF_RegridWeightGen
-export LD_LIBRARY_PATH=$D3D_HOME/lib:$LD_LIBRARY_PATH
+# ESMF_regrid is build with old compilers etc.
+# share/esmf optionally contains some library-versions especially for running this version of ESMF_regrid
+export LD_LIBRARY_PATH=$D3D_HOME/share/esmf:$D3D_HOME/lib:$LD_LIBRARY_PATH 
 
 echo Executing batchscript "ESMF_RegridWeightGen_in_Delft3D-WAVE.sh" for Delft3D-WAVE >>esmf_sh.log
 echo This script is located in directory $scriptdir >>esmf_sh.log
