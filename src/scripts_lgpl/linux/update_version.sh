f85c4a6b247cf670e94ffb6b2438180e502f7de1
Adri Mourits <Adri.Mourits@deltares.nl>
2018-05-09 15:47:12 +0000

    Reverted revision(s) 8758 from trunk: missing .exe breaks build 55623 in ds tree ........ This will break the build in oss tree. This must be solved by synchronizing the ds make environment with the oss make environment

--- src/scripts_lgpl/linux/update_version.sh
+++ src/scripts_lgpl/linux/update_version.sh
@@ -41,8 +41,7 @@ fi;
 
 
 # Generate version number source module using version_number.exe
-# TODO: why the .exe?
-$VN_DIR/version_number.exe $SVN_VERSION $3 $1.svn $1.temp
+$VN_DIR/version_number $SVN_VERSION $3 $1.svn $1.temp
 
 if [ -f $1 ]; then
     diff $1 $1.temp > nul
