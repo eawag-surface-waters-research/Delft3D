f009dd8310fc0f9cd314897d74dbc11edd8b5130
zeekant <zeekant>
2018-07-08 10:06:24 +0000

    Visual Studio 2017 not for solutions with combined Fortran and C++. Still investigating.

--- src/prepare_sln.py
+++ src/prepare_sln.py
@@ -581,11 +581,11 @@ def build_gui():
     
     Label(text="Visual Studio Version:", relief=RIDGE, width=20).grid(row=0, column=0)
     
-    Radiobutton(root, text="VS 2017 (not tested yet) ", variable=vs_gui, value=2017).grid(row=1, column=0, sticky=W)
-    Radiobutton(root, text="VS 2015, Update 3        ", variable=vs_gui, value=2015).grid(row=2, column=0, sticky=W)
-    Radiobutton(root, text="VS 2013                  ", variable=vs_gui, value=2013).grid(row=3, column=0, sticky=W)
-    Radiobutton(root, text="VS 2012                  ", variable=vs_gui, value=2012).grid(row=4, column=0, sticky=W)
-    Radiobutton(root, text="VS 2010                  ", variable=vs_gui, value=2010).grid(row=5, column=0, sticky=W)
+    Radiobutton(root, text="VS 2017 (Not for Fortran with C++)", variable=vs_gui, value=2017).grid(row=1, column=0, sticky=W)
+    Radiobutton(root, text="VS 2015, Update 3                 ", variable=vs_gui, value=2015).grid(row=2, column=0, sticky=W)
+    Radiobutton(root, text="VS 2013                           ", variable=vs_gui, value=2013).grid(row=3, column=0, sticky=W)
+    Radiobutton(root, text="VS 2012                           ", variable=vs_gui, value=2012).grid(row=4, column=0, sticky=W)
+    Radiobutton(root, text="VS 2010                           ", variable=vs_gui, value=2010).grid(row=5, column=0, sticky=W)
     # default value
     vs_gui.set(2015)
     
