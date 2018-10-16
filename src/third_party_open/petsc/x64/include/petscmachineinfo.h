e6ecf1d488d0316a35547cbb2b4f71abdb4820ec
Luca Carniato <Luca.Carniato@deltares.nl>
2018-10-16 09:21:29 +0000

    UNST-2060, link PETSc in windows: add petsc for windows, compiled in release x64 version

---
+++ src/third_party_open/petsc/x64/include/petscmachineinfo.h
@@ -0,0 +1,19 @@
+static const char *petscmachineinfo = "\n"
+"-----------------------------------------\n"
+"Libraries compiled on 2018-10-11 14:52:53 on L01288 \n"
+"Machine characteristics: CYGWIN_NT-6.1-2.11.1-0.329-5-3-x86_64-64bit\n"
+"Using PETSc directory: /cygdrive/d/carniato/LUCA/ENGINES/FM/FMSource/unstructPetsc/compilePetsc/petsc-3.10.2\n"
+"Using PETSc arch: arch-mswin-c-opt\n"
+"-----------------------------------------\n";
+static const char *petsccompilerinfo = "\n"
+"Using C compiler: /cygdrive/d/carniato/LUCA/ENGINES/FM/FMSource/unstructPetsc/compilePetsc/petsc-3.10.2/lib/petsc/bin/win32fe/win32fe cl -O2 -MD -wd4996    \n"
+"Using Fortran compiler: /cygdrive/d/carniato/LUCA/ENGINES/FM/FMSource/unstructPetsc/compilePetsc/petsc-3.10.2/lib/petsc/bin/win32fe/win32fe ifort  -MD -O3 -fpp   \n"
+"-----------------------------------------\n";
+static const char *petsccompilerflagsinfo = "\n"
+"Using include paths: -I/cygdrive/d/carniato/LUCA/ENGINES/FM/FMSource/unstructPetsc/compilePetsc/petsc-3.10.2/include -I/cygdrive/d/carniato/LUCA/ENGINES/FM/FMSource/unstructPetsc/compilePetsc/petsc-3.10.2/arch-mswin-c-opt/include -I/cygdrive/d/Apps/MSMpi/include\n"
+"-----------------------------------------\n";
+static const char *petsclinkerinfo = "\n"
+"Using C linker: /cygdrive/d/carniato/LUCA/ENGINES/FM/FMSource/unstructPetsc/compilePetsc/petsc-3.10.2/lib/petsc/bin/win32fe/win32fe cl\n"
+"Using Fortran linker: /cygdrive/d/carniato/LUCA/ENGINES/FM/FMSource/unstructPetsc/compilePetsc/petsc-3.10.2/lib/petsc/bin/win32fe/win32fe ifort\n"
+"Using libraries: -L/cygdrive/d/carniato/LUCA/ENGINES/FM/FMSource/unstructPetsc/compilePetsc/petsc-3.10.2/arch-mswin-c-opt/lib -L/cygdrive/d/carniato/LUCA/ENGINES/FM/FMSource/unstructPetsc/compilePetsc/petsc-3.10.2/arch-mswin-c-opt/lib -lpetsc /cygdrive/c/Program\\ Files\\ \(x86\)/IntelSWTools/compilers_and_libraries/windows/mkl/lib/intel64/mkl_intel_lp64_dll.lib mkl_intel_thread_dll.lib mkl_core_dll.lib libiomp5md.lib /cygdrive/d/Apps/MSMpi/lib/mpi.lib /cygdrive/d/Apps/MSMpi/lib/fmpich2.lib Gdi32.lib User32.lib Advapi32.lib Kernel32.lib Ws2_32.lib\n"
+"-----------------------------------------\n";
