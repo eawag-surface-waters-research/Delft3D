11fda1ccac2c51bb6fece5690050603df3fa80d0
Luca Carniato <Luca.Carniato@deltares.nl>
2018-10-17 12:58:59 +0000

    UNST-2060, link PETSc in windows: remove include

--- src/third_party_open/petsc/include/petsc/private/hashseti.h
+++
@@ -1,8 +0,0 @@
-#if !defined(_PETSC_HASHSETI_H)
-#define _PETSC_HASHSETI_H
-
-#include <petsc/private/hashset.h>
-
-PETSC_HASH_SET(HSetI, PetscInt, PetscHashInt, PetscHashEqual)
-
-#endif /* _PETSC_HASHSETI_H */
