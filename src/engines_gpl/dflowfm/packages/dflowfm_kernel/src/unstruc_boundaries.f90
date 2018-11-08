ceb03e8e67d0f3060b7b03cbe202298d360f3ed1
pijl <pijl>
2018-11-08 08:01:27 +0000

    issue: UNST-1726
    *attempt to repair broken build by rev. 62537 (instead of complete revert)

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/unstruc_boundaries.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/unstruc_boundaries.f90
@@ -1120,40 +1120,40 @@ end subroutine ini_alloc_laterals
 
 
 !> Prepare the 'kclat' mask array for a specific type of lateral.
-subroutine prepare_lateral_mask(kclat, ilattype)
+subroutine prepare_lateral_mask(kc, ilattype)
    use m_flowgeom
    use m_wind
    implicit none
    
-   integer         , intent(inout) :: kclat(:) !< (ndx) The mask array that is to be filled.
+   integer         , intent(inout) :: kc(:) !< (ndx) The mask array that is to be filled.
    integer         , intent(in)    :: ilattype !< Type of the new lateral (one of ILATTP_1D|2D|1D2D)
    
    integer                         :: L, k1, k2
 
-   kclat = 0
+   kc = 0
    select case (ilattype)
    case (ILATTP_1D)       ! in everything 1D
       do L = 1,lnx1D
          !if (abs(prof1D(3,L)) .ne. 1 .and. prof1D(3,L) > 0 ) then ! no pipes pos or neg, others only if pos
-            k1 = ln(1,L) ; kclat(k1) = 1
-            k2 = ln(2,L) ; kclat(k2) = 1
+            k1 = ln(1,L) ; kc(k1) = 1
+            k2 = ln(2,L) ; kc(k2) = 1
          !endif   
       enddo   
    case (ILATTP_2D)       ! in everything 2D
       do L = lnx1D+1,lnxi
-         k1 = ln(1,L) ; kclat(k1) = 1
-         k2 = ln(2,L) ; kclat(k2) = 1
+         k1 = ln(1,L) ; kc(k1) = 1
+         k2 = ln(2,L) ; kc(k2) = 1
       enddo   
    case (ILATTP_ALL)      ! both to everything 2D, and 1D, except to 1D pipes
       do L = 1,lnx1D
          if (abs(prof1D(3,L)) .ne. 1 .and. prof1D(3,L) > 0 ) then ! no pipes pos or neg, others only if pos
-            k1 = ln(1,L) ; kclat(k1) = 1
-            k2 = ln(2,L) ; kclat(k2) = 1
+            k1 = ln(1,L) ; kc(k1) = 1
+            k2 = ln(2,L) ; kc(k2) = 1
          endif   
       enddo   
       do L = lnx1D+1,lnxi
-         k1 = ln(1,L) ; kclat(k1) = 1
-         k2 = ln(2,L) ; kclat(k2) = 1
+         k1 = ln(1,L) ; kc(k1) = 1
+         k2 = ln(2,L) ; kc(k2) = 1
       enddo   
    end select
 end subroutine prepare_lateral_mask
