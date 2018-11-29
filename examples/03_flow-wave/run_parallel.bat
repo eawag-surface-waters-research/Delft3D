b6ecf7fbde4b086bfef5cee665a3ca189d798eb4
Adri Mourits <Adri.Mourits@deltares.nl>
2018-11-29 14:26:57 +0000

    examples/03_flow-wave/run_parallel.bat: Added in comments: instructions for starting smpd

--- examples/03_flow-wave/run_parallel.bat
+++ examples/03_flow-wave/run_parallel.bat
@@ -1,5 +1,16 @@
 @ echo off
 
+    rem When using mpich2 for the first time on a machine:
+    rem Execute "smpd -install" as administrator:
+    rem     "Start" -> "All programs" -> "Accessories", right-click "Command Prompt", "Run as Administrator"
+    rem     In this command box:
+    rem         cd ...\src\bin\x64\share\bin
+    rem         (or when not compiled yet)
+    rem         cd ...\src\third_party_open\mpich2\bin
+    rem         smpd -install
+    rem     When there is an smpd already running on the machine, it must be ended first, using the Microsoft Task Manager
+
+
 call ..\..\src\bin\x64\dflow2d3d\scripts\run_dflow2d3d_parallel_dwaves.bat -w r17.mdw
 
 
