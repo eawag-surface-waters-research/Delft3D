84aa4d1d9038ad1c1c66366d17d9d9c135be4ee5
Arjen Markus <Arjen.Markus@deltares.nl>
2018-12-11 07:54:27 +0000

    Add a test for the extension of the hash tables.

--- test/utils_lgpl/deltares_common/packages/test_deltares_common/src/test_hash_search.f90
+++ test/utils_lgpl/deltares_common/packages/test_deltares_common/src/test_hash_search.f90
@@ -33,6 +33,7 @@ subroutine tests_hash_search
     call test( test_hash_fill,          'Initialise and fill a hash table' )
     call test( test_hash_dealloc,       'Deallocate a hash table' )
     call test( test_hash_search_or_add, 'Store and retrieve strings in a hash table' )
+    call test( test_hash_realloc,       'Automatically extend the arrays in a hash table' )
 end subroutine tests_hash_search
 
 subroutine test_hash_init
@@ -59,13 +60,13 @@ subroutine test_hash_fill
     call hashfill_init( hashlist, count )
     call hashfill( hashlist )
 
-    call assert_true( allocated(hashlist%hashfirst), "The 'first' array in the hashlist is NOT allocated" )
+    call assert_true( allocated(hashlist%hashfirst), "The 'first' array in the hashlist should have been allocated" )
 
     if ( allocated(hashlist%hashfirst) ) then
         call assert_equal( size(hashlist%hashfirst), 1+hashlist%hashcon, "The size of the 'first' array in the hash list is incorrect" )
     endif
 
-    call assert_true( allocated(hashlist%hashnext), "The 'next' array in the hashlist is NOT allocated" )
+    call assert_true( allocated(hashlist%hashnext), "The 'next' array in the hashlist shoudl have been allocated" )
 
     if ( allocated(hashlist%hashfirst) ) then
         call assert_equal( size(hashlist%hashnext), hashlist%id_count, "The size of the 'next' array in the hash list is incorrect" )
@@ -90,8 +91,8 @@ subroutine test_hash_dealloc
 
     call assert_equal( hashlist%id_count, 0, "The number of IDs in the deallocated hash list is incorrect" )
     call assert_equal( hashlist%size, 0, "The size of the deallocated hash list is incorrect" )
-    call assert_false( allocated(hashlist%hashfirst), "The 'first' array has NOT been deallocated" )
-    call assert_false( allocated(hashlist%hashnext), "The 'next' array has NOT been deallocated" )
+    call assert_false( allocated(hashlist%hashfirst), "The 'first' array should have been deallocated" )
+    call assert_false( allocated(hashlist%hashnext), "The 'next' array should have been deallocated" )
 end subroutine test_hash_dealloc
 
 subroutine test_hash_search_or_add
@@ -114,8 +115,37 @@ subroutine test_hash_search_or_add
     call assert_equal( idx1, idxn, "The retrieved ID is different from the original ID" )
 
     idxn = hashsearch( hashlist, 'FOO' )
-    call assert_equal( idx1, idxn, "The retrieval is case-sensitive" )
+    call assert_equal( idx1, idxn, "The retrieval should be case-insensitive" )
 end subroutine test_hash_search_or_add
+
+subroutine test_hash_realloc
+    type(t_hashlist) :: hashlist
+    integer          :: idx1, idx2, idx3, idx4, idxn
+    integer          :: count
+
+    !
+    ! Check that the hash list is correctly extended - by forcing a reallocation
+    !
+    count = 1
+    call hashfill_init( hashlist, count )
+
+    idx1 = hashsearch_or_add( hashlist, 'foo' )
+    idx2 = hashsearch_or_add( hashlist, 'bar' )
+    idx3 = hashsearch_or_add( hashlist, 'baz' )
+    idx4 = hashsearch_or_add( hashlist, 'qiz' )
+
+    idxn = hashsearch( hashlist, 'foo' )
+    call assert_false( idxn == -1, "The stored key 'foo' should have been retrieved" )
+    if ( idxn /= -1 ) then
+        call assert_equal( idx1, idxn, "The retrieved ID for 'foo' is different from the original ID" )
+    endif
+
+    idxn = hashsearch( hashlist, 'qiz' )
+    call assert_false( idxn == -1, "The stored key 'qiz' should have been retrieved" )
+    if ( idxn /= -1 ) then
+        call assert_equal( idx4, idxn, "The retrieved ID for 'qiz' is different from the original ID" )
+    endif
+end subroutine test_hash_realloc
 
 end module test_hash_search
 
