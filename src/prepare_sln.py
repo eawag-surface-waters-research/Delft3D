31aae221c3f65f163b035c7ddb4c7a4648ec652f
Adri Mourits <Adri.Mourits@deltares.nl>
2019-02-20 08:58:18 +0000

    prepare_sln.py: removed: text in GUI behind IFORT19: (not tested yet)

--- src/prepare_sln.py
+++ src/prepare_sln.py
@@ -674,7 +674,7 @@ def build_gui():
     
     if chooseIfort == 1:
         Label(text="IFORT Version:", relief=RIDGE, width=20).grid(row=0, column=2)
-        Radiobutton(root, text="IFORT19: (not tested yet)                      ", variable=ifort_gui, value=19).grid(row=1, column=2, sticky=W)
+        Radiobutton(root, text="IFORT19: Intel Parallel Studio XE 2019 Update 1", variable=ifort_gui, value=19).grid(row=1, column=2, sticky=W)
         Radiobutton(root, text="IFORT18: Intel Parallel Studio XE 2018 Update 4", variable=ifort_gui, value=18).grid(row=2, column=2, sticky=W)
         Radiobutton(root, text="IFORT17: (Not Recommended)                     ", variable=ifort_gui, value=17).grid(row=3, column=2, sticky=W)
         Radiobutton(root, text="IFORT16: Intel Parallel Studio XE 2016 Update 4", variable=ifort_gui, value=16).grid(row=4, column=2, sticky=W)
