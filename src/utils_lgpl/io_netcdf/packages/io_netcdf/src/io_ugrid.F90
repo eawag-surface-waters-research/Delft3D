bd183915e3cf1db6ed9fe12dd5e59c45d595da3c
Luca Carniato <Luca.Carniato@deltares.nl>
2019-04-12 09:46:56 +0000

    UNST-2457, io_netcdf, reading UGRID formatted 1D net file: network_geometry should use "node_count" instead of "part_node_count": if part_node_count points to a valid variable use that variable, otherwise try node_count. Correct implementation

--- src/utils_lgpl/io_netcdf/packages/io_netcdf/src/io_ugrid.F90
+++ src/utils_lgpl/io_netcdf/packages/io_netcdf/src/io_ugrid.F90
@@ -1828,11 +1828,13 @@ function ug_init_network_topology(ncid, varid, netids) result(ierr)
    ierr = att_to_varid(ncid, varid, 'branch_lengths'        , netids%varids(ntid_1dbranchlengths))
    !get the number of geometric points for each branch
    ! TODO: UNST-2391
-   
-   ! for backward compatibility: part_node_count was used rather than node_count. If there get the varid of the attribute
-   ierr = att_to_varid(ncid, netids%varids(ntid_1dgeometry), 'part_node_count', netids%varids(ntid_1dgeopointsperbranch))   
-   ! If it is not there, then inquire node_count
-   if ( ierr.ne.0 ) then
+
+   ierr = nf90_inquire_attribute(ncid, netids%varids(ntid_1dgeometry), 'part_node_count')
+   if ( ierr.eq.0 ) then
+      ! for backward compatibility: part_node_count was used rather than node_count. If there get the varid of the attribute
+      ierr = att_to_varid(ncid, netids%varids(ntid_1dgeometry), 'part_node_count', netids%varids(ntid_1dgeopointsperbranch))   
+   else 
+      ! try to get the node_count
       ierr = att_to_varid(ncid, netids%varids(ntid_1dgeometry), 'node_count', netids%varids(ntid_1dgeopointsperbranch))   
    endif
 
