b8a14d6db8dcc0b272f461396dd11f069ca81b8c
Mohamed Nabi <Mohamed.Nabi@deltares.nl>
2019-04-12 15:47:48 +0000

    issue: UNST-2180
    A fix for handling with keywords GateHeight from old and new formats for general structures

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/unstruc_boundaries.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/unstruc_boundaries.f90
@@ -1700,10 +1700,10 @@ implicit none
 character(len=256)            :: plifile
 integer                       :: i, L, Lf, kb, LL, ierr, k, kbi, n, ifld, k1, k2
 integer                       :: nstr
-character (len=256)           :: fnam, rec, key
+character (len=256)           :: fnam, rec, key, rec_old
 integer, allocatable          :: pumpidx(:), gateidx(:), cdamidx(:), cgenidx(:), dambridx(:) ! temp
 double precision              :: tmpval
-integer                       :: istru, istrtype, itmp, janewformat
+integer                       :: istru, istrtype, itmp, janewformat, jaoldformat
 integer                       :: numg, numd, npum, ngs, numgen, numgs, ilinstr, ndambr
 type(TREE_DATA), pointer      :: str_ptr
 double precision, allocatable :: widths(:)
@@ -1729,6 +1729,7 @@ double precision, allocatable :: xl(:,:), yl(:,:)
 integer                       :: branchIndex   
 double precision              :: chainage
 !! if (jatimespace == 0) goto 888                      ! Just cleanup and close ext file.
+logical                       :: success_old
 
 ngs = 0 ! Local counter for all crossed flow liks by *all* general structures.
 nstr = tree_num_nodes(strs_ptr) ! TODO: minor issue: will count *all* children in structure file.
@@ -2298,24 +2299,37 @@ end do
 
       !! GENERALSTRUCTURE !!
       case ('generalstructure')
-         janewformat = 1
+         janewformat = 0
+         jaoldformat = 0
          do k = 1,numgeneralkeywrd        ! generalstructure keywords
             tmpval = dmiss
-            if (janewformat == 1) then
+            if (k == 25 .and. janewformat == 1) then                ! The 25th keyword in new format is same as the 11th keyword in the old format. But they are linked to different properties.
+               key = generalkeywrd(k)                               ! Then the format for this keyword (GateHeight) is determined by the other keywords in that single structure.
+               call prop_get(str_ptr, '', trim(key), rec, success)
+            else if (k == 11 .and. jaoldformat == 1) then
+               key = generalkeywrd_old(k)
+               call prop_get(str_ptr, '', trim(key), rec, success)
+            else
                key = generalkeywrd(k)
                call prop_get(str_ptr, '', trim(key), rec, success)
                if (.not. success) then
                   key = generalkeywrd_old(k)
                   call prop_get(str_ptr, '', trim(key), rec, success)
-                  if (success) then
-                     janewformat = 0 ! old-style keyword found, from now on, only consider old keywords for this structure (needed for gateheight versus GateHeight, which are *not* the same)
-                  end if
-               end if
-            else
-               key = generalkeywrd_old(k)
-               call prop_get(str_ptr, '', trim(key), rec, success)
+                  jaoldformat = 1
+               else
+                  key = generalkeywrd_old(k)
+                  call prop_get(str_ptr, '', trim(key), rec_old, success_old)
+                  if (.not. success_old) then
+                     janewformat = 1
+                  endif
+                  if (success .and. k == 25) then
+                     rec = rec_old
+                  endif
+               endif
             endif
-            
+            if (janewformat == 1 .and. jaoldformat == 1) then
+               call mess(LEVEL_ERROR, 'Combination of old and new keywords for a general structure is not supported ...' )
+            endif
             if (.not. success .or. len_trim(rec) == 0) then
                ! consider all fields optional for now.
                cycle
