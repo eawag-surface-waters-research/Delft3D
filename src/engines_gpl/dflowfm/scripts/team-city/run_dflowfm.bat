6a831cd58b534f09153a6e2891c760afcf45c981
Adri Mourits <Adri.Mourits@deltares.nl>
2019-07-02 07:14:29 +0000

    DELFT3D-36982: Add scripts for running dflowfm and dfmoutput Added to run_dflowfm.bat: comments about smpd

--- src/engines_gpl/dflowfm/scripts/team-city/run_dflowfm.bat
+++ src/engines_gpl/dflowfm/scripts/team-city/run_dflowfm.bat
@@ -4,6 +4,30 @@ title run_dflowfm
     rem This script runs dflowfm on Windows
     rem Adapt and use it for your own purpose
     rem
+    rem When using mpich2 for the first time on a machine:
+    rem Execute "smpd -install" as administrator:
+    rem     Preparation: Check that your Delft3D installation contains "...\x64\share\bin\smpd.exe". Optionally copy it to a local directory (it will run as a service).
+    rem     "Start" -> "All programs" -> "Accessories", right-click "Command Prompt", "Run as Administrator"
+    rem     In this command box:
+    rem         cd ...\x64\share\bin
+    rem         smpd -install
+    rem     When there is an smpd already running on the machine, it must be ended first, using the Microsoft Task Manager, 
+    rem     or in the command  box: smpd -uninstall
+    rem
+    rem
+    rem This script runs dimr in parallel mode on Windows
+    rem Adapt and use it for your own purpose
+    rem
+    rem Usage example:
+    rem Execute in the working directory:
+    rem path\to\delft3d\installation\x64\dimr\scripts\run_dimr_parallel.bat
+    rem More examples: check run scripts in https://svn.oss.deltares.nl/repos/delft3d/trunk/examples/*
+
+
+
+
+
+
     rem Usage example:
     rem Execute in the working directory:
     rem path\to\delft3d\installation\x64\dflowfm\scripts\run_dflowfm.bat
