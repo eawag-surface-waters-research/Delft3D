53a7c83c2fe7ec69f1dfa920a54cb7438c159e0b
Arjen Markus <Arjen.Markus@deltares.nl>
2019-08-05 09:24:18 +0000

    Add several unit tests for the properties module - see UNST-2833. There is some ambiguity in the implementation, as discussed in the JIRA-issue, but for compatibility we are not going to change it without more attention to the consequences.

--- src/test/utils_lgpl/deltares_common/packages/test_deltares_common/src/test_properties.f90
+++ src/test/utils_lgpl/deltares_common/packages/test_deltares_common/src/test_properties.f90
@@ -32,6 +32,8 @@ subroutine tests_properties
     call test( test_properties_load, 'Simply load a properties file' )
     call test( test_properties_check, 'Checking if the values in a properties file get loaded correctly' )
     call test( test_properties_version, 'Checking if the fileversion get loaded correctly' )
+    call test( test_properties_get_single_values, 'Checking if single values are returned correctly' )
+    call test( test_properties_get_strings, 'Checking if strings are returned correctly' )
 end subroutine tests_properties
 
 subroutine test_properties_load
@@ -178,8 +180,8 @@ subroutine test_properties_version
     type(tree_data), pointer :: tree
     integer                  :: error
     logical                  :: success
-    integer                  :: major 
-    integer                  :: minor 
+    integer                  :: major
+    integer                  :: minor
     !
     ! Check that a non-existing file causes an error
     !
@@ -194,16 +196,119 @@ subroutine test_properties_version
     call prop_get_version_number(tree, major = major, minor = minor, success = success)
     call assert_equal( major, 1, 'Major version')
     call assert_equal( minor, 245, 'Minor version')
-    
+
     call prop_get_version_number(tree, keyin = 'versionNumber', major = major, minor = minor, versionstring = fileVersion, success = success)
     call assert_equal( success, .false., 'Incorrect version string')
     call assert_equal( trim(fileVersion), '3', 'version string')
-    
+
     call prop_get_version_number(tree, 'new', 'version', major = major, minor = minor, versionstring = fileVersion, success = success)
     call assert_equal( major, 5, 'Major version')
     call assert_equal( minor, 1, 'Minor version')
     call assert_equal( trim(fileVersion), '5.001', 'version string')
-    
+
 end subroutine test_properties_version
+
+
+subroutine test_properties_get_single_values
+    character(len=20)        :: one_string
+    integer                  :: one_integer
+    real                     :: one_real
+    type(tree_data), pointer :: tree
+    integer                  :: error
+    logical                  :: success
+    !
+    ! Check that a non-existing file causes an error
+    !
+    ! Note:
+    ! Apparently, the tree variable needs to be associated/allocated beforehand
+    !
+    allocate( tree )
+
+    call prop_inifile( 'test_strings.ini', tree, error )
+
+    !
+    ! Retrieve strings ...
+    !
+    call prop_get( tree, 'single', 'string1', one_string, success )
+    call assert_true( success, "Retrieving the string value should succeed (chapter: single)" )
+    call assert_equal( one_string, "1234", "Single words should be treated correctly (chapter: single)" )
+
+    !
+    ! Note that leading blanks (spaces) are removed
+    !
+    call prop_get( tree, 'single', 'string2', one_string, success )
+    call assert_true( success, "Retrieving the string value should succeed (chapter: single)" )
+    call assert_equal( one_string, "1 2 3 4", "Multiple words should be treated correctly - including blanks (chapter: single)" )
+
+    !
+    ! Retrieve numerical values ...
+    !
+    call prop_get( tree, 'single', 'integer', one_integer, success )
+    call assert_true( success, "Retrieving the integer value should succeed (chapter: single)" )
+    call assert_equal( one_integer, 3, "Correct value must be returned (chapter: single)" )
+
+    call prop_get( tree, 'single', 'real', one_real, success )
+    call assert_true( success, "Retrieving the real value should succeed (chapter: single)" )
+    call assert_comparable( one_real, 3.5, 1.0e-6, "Correct value must be returned (chapter: single)" )
+
+end subroutine test_properties_get_single_values
+
+
+subroutine test_properties_get_strings
+    character(len=20), dimension(10) :: string
+    character(len=20), dimension(10) :: expected
+    type(tree_data), pointer         :: tree
+    integer                          :: i
+    integer                          :: error
+    logical                          :: success
+    !
+    ! Check that a non-existing file causes an error
+    !
+    ! Note:
+    ! Apparently, the tree variable needs to be associated/allocated beforehand
+    !
+    allocate( tree )
+
+    call prop_inifile( 'test_strings.ini', tree, error )
+
+    !
+    ! Retrieve strings - array dimension large enough/limited
+    !                  - default character
+    !
+    expected = [ 'A', 'B', 'C', '' , 'D', 'E', '?', '?', '?', '?' ]
+    string   = '?'
+    call prop_get_strings( tree, 'multiple', 'setOfStrings1', size(string), string, success )
+
+    do i = 1,10
+        call assert_true( success, "Retrieving the string value should succeed (chapter: multiple)" )
+        call assert_equal( string(i), expected(i), "Substring should be parsed correctly (default separator)" )
+    enddo
+
+    expected = [ 'A', 'B', '?', '?', '?', '?', '?', '?', '?', '?' ]
+    string   = '?'
+    call prop_get_strings( tree, 'multiple', 'setOfStrings1', 2, string, success )
+
+    do i = 1,10
+        call assert_true( success, "Retrieving the string value should succeed (chapter: multiple)" )
+        call assert_equal( string(i), expected(i), "Substring should be parsed correctly - maximum 2 (two) substrings" )
+    enddo
+
+    !
+    ! Retrieve strings - non-default character
+    !
+
+    expected = [ 'A                   ', 'B                   ', 'C                   ', '                    ', &
+                 'D     ;          E  ', '?                   ', '?                   ', '?                   ', &
+                 '?                   ', '?                   ']
+                 !12345678901234567890 - all strings need to be the same length! (This has been relaxed in later standards,
+                 ! but let's keep it simple
+    string   = '?'
+    call prop_get_strings( tree, 'multiple', 'setOfStrings2', size(string), string, success, '@' )
+
+    do i = 1,10
+        call assert_true( success, "Retrieving the string value should succeed (chapter: multiple)" )
+        call assert_equal( string(i), expected(i), "Substring should be parsed correctly (non-default separator)" )
+    enddo
+end subroutine test_properties_get_strings
 
 end module test_properties
