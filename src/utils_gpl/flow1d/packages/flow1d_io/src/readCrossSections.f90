8c7ec43c2f314281d00761538be713d14a375cc1
Jan Noort <Jan.Noort@deltares.nl>
2019-08-20 13:20:49 +0000

    UNST-2819: Improvement on calculation of the flow area of a closed rectangle. No Preisman slot is added to the flow area anymore.

--- src/utils_gpl/flow1d/packages/flow1d_io/src/readCrossSections.f90
+++ src/utils_gpl/flow1d/packages/flow1d_io/src/readCrossSections.f90
@@ -388,11 +388,6 @@ module m_readCrossSections
                numlevels = numlevels + 1
                level(numLevels) = height
                width(numLevels) = width(1)
-               if (closed) then
-                  numlevels = numLevels + 1
-                  level(numLevels) = height + 1d-3
-                  width(numLevels) = 1d-3
-               endif
                
             endif
 
