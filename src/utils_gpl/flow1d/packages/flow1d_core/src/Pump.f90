a55df9dc2e1c827d68ccd4bae436b06d9816b02b
Jing Zhao <Jing.Zhao@deltares.nl>
2019-08-30 08:59:21 +0000

    UNST-2587: the output of pump capacity does not include the reduction factor.

--- src/utils_gpl/flow1d/packages/flow1d_core/src/Pump.f90
+++ src/utils_gpl/flow1d/packages/flow1d_core/src/Pump.f90
@@ -216,7 +216,7 @@ contains
          else
            pump%is_active = .true.
            qp = pump%reduction_factor * pump%capacity(pump%actual_stage)
-           pump%current_capacity = qp
+           pump%current_capacity = pump%capacity(pump%actual_stage)
          endif
 
       endif
