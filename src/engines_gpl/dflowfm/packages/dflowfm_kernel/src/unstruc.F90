2a94b0ae1975fd63ebc335208395399d2ffe80be
Arthur van Dam <Arthur.vanDam@deltares.nl>
2019-09-27 09:57:20 +0000

    UNST-3059: bugfix in switchiadvnearlink: do *not* unset iadv=8 to 0 for 1d2d links, because it causes very high flow velocities (discovered in Kockengen 1D2D).

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/unstruc.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/unstruc.F90
@@ -43891,13 +43891,13 @@ end if
 
 do kk  = 1,nd(k1)%lnx
    LL = iabs( nd(k1)%ln(kk) )
-   if ( iadv(LL) .ne. 22) then
+   if ( iadv(LL) .ne. 22 .and. (kcu(LL) == 1 .or. kcu(LL) == 2)) then ! Only for regular 1D or 2D.
         iadv(LL) = iadv1
    endif
 enddo
 do kk  = 1,nd(k2)%lnx
    LL = iabs( nd(k2)%ln(kk) )
-   if ( iadv(LL) .ne. 22) then
+   if ( iadv(LL) .ne. 22 .and. (kcu(LL) == 1 .or. kcu(LL) == 2)) then ! Only for regular 1D or 2D.
         iadv(LL) = iadv2
    endif
 enddo
