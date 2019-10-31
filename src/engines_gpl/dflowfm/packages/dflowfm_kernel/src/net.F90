763c2956a6f2b0367684d916c3b41b883095b358
Herman Kernkamp <Herman.Kernkamp@deltares.nl>
2019-10-31 14:51:00 +0000

    unst-3219 unstrucversion = 1.2.81     ! date  = 20192810                             ! unst-3219 speed up of dry points from *.xyz , gridgeom.f90, net.f90 ipolyfound to be able to analyse in debugger slow init of north sea model

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/net.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/net.F90
@@ -35359,6 +35359,62 @@ subroutine samples_to_cellmask()
    return
 end subroutine samples_to_cellmask
 
+! update cellmask from samples
+subroutine samples_to_cellmask2()
+
+   use network_data
+   use m_samples
+   use m_missing, only: jins, dmiss
+   use geometry_module ! , only: pinpok
+   
+   implicit none
+   
+   integer :: i, in, k, kk, n, nn
+   double precision :: xx(6), yy(6)
+   
+   if ( allocated(cellmask) ) deallocate(cellmask)
+   allocate(cellmask(nump1d2d)) ; cellmask = 0
+  
+   zs(1:ns) = 1
+
+   call increasepol(6*nump, 0)
+   npl = 0   
+
+   do k = 1,nump
+      nn = netcell(k)%N 
+      if (nn .lt.1 ) cycle
+   
+      do n = 1,nn
+         kk = netcell(k)%nod(n)
+         npl = npl + 1
+         xpl(npl) = xk(kk)
+         ypl(npl) = yk(kk)
+         zpl(npl) = 1d0
+      enddo     
+      npl = npl + 1 ; xpl(npl) = dmiss ; ypl(npl) = dmiss ; zpl(npl) = dmiss
+
+   enddo 
+
+   in = -1 
+ 
+   do i=1,NS    !  generate cell mask
+         
+      call dbpinpol(xs(i), ys(i), in, dmiss, 1, NPL, xpl, ypl, zpl) ! ALS JE VOOR VEEL PUNTEN MOET NAGAAN OF ZE IN POLYGON ZITTEN
+  
+      ! call pinpok(xs(i), ys(i), nn, xx, yy, in, jins, dmiss)
+         
+      if ( ipolyfound > 0 ) then
+           cellmask(ipolyfound) = 1
+           exit
+      end if
+
+   end do
+   
+  
+   return
+end subroutine samples_to_cellmask2
+
+
 !> update cellmask from samples
 !> a cell is dry when it is:
 !>   1) inside ANY "1"-polygon (drypnts), OR
@@ -35527,7 +35583,7 @@ subroutine delete_drypoints_from_netgeom(dryptsfilelist, jaconfirm, jinside)
                call oldfil(minp, dryptsfile)
                call savesam()
                call reasam(minp, 0)
-               call samples_to_cellmask()
+               call samples_to_cellmask2()
                call delsam(0)
 
                ierror = 0
