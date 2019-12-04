48b6ab456720e052a7c0d089fdf3dc79ff83b3aa
Edwin Spee <Edwin.Spee@deltares.nl>
2019-12-04 09:23:46 +0000

    DELFT3D-35102 for call to MatCreateMPIAIJWithSplitArrays: integer -> PetscInt

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/solve_petsc.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/solve_petsc.F90
@@ -43,20 +43,20 @@ module m_petsc
 #include <petsc/finclude/petscksp.h>
 
   use petsc
-   integer                                                    :: numrows      ! number of rows in this domain
+   PetscInt                                                   :: numrows      ! number of rows in this domain
    integer                                                    :: numallrows   ! number of rows of whole system
    integer,          dimension(:), allocatable                :: rowtoelem    ! local row to local element list, dim(numrows)
 
 !  CRS matrices for PETSc/MatCreateMPIAIJWithSplitArrays
-   integer                                                    :: numdia       ! number of non-zero entries in diagonal block
+   PetscInt                                                   :: numdia       ! number of non-zero entries in diagonal block
    double precision, dimension(:), allocatable                :: adia         ! non-zero matrix entries, diagonal block
-   integer,          dimension(:), allocatable                :: idia, jdia   ! column indices and row pointers of off-diagonal block
+   PetscInt,         dimension(:), allocatable                :: idia, jdia   ! column indices and row pointers of off-diagonal block
 
    integer                                                    :: numoff       ! number of non-zero entries in off-diagonal block
-   double precision, dimension(:), allocatable                :: aoff         ! non-zero matrix entries, diagonal block
-   integer,          dimension(:), allocatable                :: ioff, joff   ! column indices and row pointers of off-diagonal block
+   PetscScalar     , dimension(:), allocatable                :: aoff         ! non-zero matrix entries, diagonal block
+   PetscInt,         dimension(:), allocatable                :: ioff, joff   ! column indices and row pointers of off-diagonal block
    
-   integer,          dimension(:), allocatable                :: joffsav      ! store of joff
+   PetscInt,         dimension(:), allocatable                :: joffsav      ! store of joff
 
    integer,          dimension(:), allocatable                :: guusidxdia   ! index in ccr or bbr array, >0: ccr, <0: bbr, diagonal block, dim(numdia)
    integer,          dimension(:), allocatable                :: guusidxoff   ! index in ccr or bbr array, >0: ccr, <0: bbr, off-diagonal block, dim(numoff)
