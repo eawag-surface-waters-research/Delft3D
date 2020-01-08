7fac730f07d4b904828c4e709be0994b79252911
Arthur van Dam <Arthur.vanDam@deltares.nl>
2020-01-08 12:44:51 +0000

    UNST-3399: bugfixes for non-RHU 1D models: * Added qnerror message for when a netlink already has two 2D neighbouring   cells in lne(1:2,L) and additionally is wrongly connected to 1D points.   This happened in a model where a 1D net link was connected to a netnode   that was actually a vertex netnode of the 2D grid parts. * bugfix: corrected the lne(:,L) construction in preparecells():   the lne(1:2,L) orientation for 1D links (and 1D2D) should stay the same   as the original kn(1:2,L) orientation. In normal find1dcells() this is   by construction, but when reading a net file with netcells, the reconstruction   in preparecells() needs an orientation check. Was crashing in setprofs1D.   See issue text for details.

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/net.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/net.F90
@@ -35778,6 +35778,7 @@ subroutine preparecells(md_netfile, jaidomain, jaiglobal_s, ierr)
  use dfm_error
  use unstruc_display, only: jareinitialize
  use gridoperations
+ use unstruc_messages
 
  implicit none
  character(len=*), intent(in) :: md_netfile  !< net filename
@@ -35820,9 +35821,20 @@ subroutine preparecells(md_netfile, jaidomain, jaiglobal_s, ierr)
     do i = 1, nv
        L = nod(k)%lin(i)
        if (lne(1, L) .ne. -c .and. lne(2, L) .ne. -c ) then
-           j = lnn(L) + 1
-           lnn(L) = j
-           lne(j, L) = -c
+          if (lnn(L) >= 2) then
+             write (msgbuf, '(a,i0,a,i0,a)') 'Illegal: net link ', L, ' already has 2 2D cells, and is also connected to 1D netnode ', k, '.'
+             call qnerror(trim(msgbuf), '', '')
+          else
+             ! the orientation of lne(1:2,L) should be the same as net link orientation kn(1:2,L). See subroutine is_1d_boundary_candidate().
+             if (k == kn(1,L)) then
+                j = 1
+             else
+                j = 2
+             end if
+
+             lnn(L) = lnn(L)+1 ! Only #j is filled for now, but it will always end up to be 2.
+             lne(j, L) = -c
+          end if
        endif
        if (kn(3, L) == 3  .or. kn(3, L) == 4  .or. kn(3, L) == 5 .or. kn(3, L) == 7) then   ! If 1d link L enters a 2d cell
           k1 = kn(1, L)
@@ -35832,9 +35844,20 @@ subroutine preparecells(md_netfile, jaidomain, jaiglobal_s, ierr)
           nc1 = 0
           call incells(xk(k1), yk(k1), nc1)
           if (nc1 .ne. 0) then
-              j = lnn(L) + 1
-              lnn(L) = j
-              lne(j, L) =nc1
+             if (lnn(L) >= 2) then
+                write (msgbuf, '(a,i0,a,i0,a)') 'Illegal: net link ', L, ' already has 2 2D cells, and is also connected to 1D netnode ', k, '.'
+                call qnerror(trim(msgbuf), '', '')
+             else
+                ! the orientation of lne(1:2,L) should be the same as net link orientation kn(1:2,L).
+                if (k == kn(1,L)) then
+                   j = 2
+                else
+                   j = 1
+                end if
+
+                lne(j, L) = nc1 ! j here is the position of the 2D netcell.
+                lnn(L) = 2 ! Must always end up as 2 here.
+             end if
           endif
        endif
     enddo
