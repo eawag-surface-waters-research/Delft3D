187c6ad4e9656659e3f558df658445d4dc881141
Arjen Markus <Arjen.Markus@deltares.nl>
2020-01-08 06:06:39 +0000

    UNST-3192: Changed --disablecaching to --no-geom-cache as per the JIRA issue.

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/net.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/net.F90
@@ -34268,7 +34268,7 @@ function read_commandline() result(istat)
             if ( iarg_autostart.eq.-1 ) then ! unset
                iarg_autostart = MD_AUTOSTARTSTOP
             end if
-         case ('disablecaching')
+         case ('no-geom-cache')
             iarg_usecaching = 0
          case ('findcells')
             md_findcells = 1
