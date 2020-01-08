acb76705e3de90f68656078248c87c2d1db8f09c
Arthur van Dam <Arthur.vanDam@deltares.nl>
2020-01-08 12:42:04 +0000

    GUI-only bugfix: isoscale2 labels correct when vmin < -100000

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/gui.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/gui.F90
@@ -17453,6 +17453,7 @@ double precision :: value
   implicit none
   integer :: n1
   integer :: n2
+  integer :: n3 ! nr of digits behind decimal dot
   integer :: ndec
   double precision :: value
   character fmt*(*)
@@ -17468,15 +17469,21 @@ double precision :: value
 
   if (n1 .lt. 6 .and. n1 .gt. 0) then
      n2 = min(9,n1 + 3)
-     write (fmt(6:6),'(i1)') 9 - n2
+     n3 = 9 - n2
   else if (n1 .ge. -5 .and. n1 .lt. 0) then
-     write (fmt(6:6),'(i1)') 6
+     n3 = 6
   else if ( n1 .eq. 0) then
-     write (fmt(6:6),'(i1)') 6
+     n3 = 6
   else
      fmt ='(e10.3)'
+     return
   endif
-  IF (NDEC .GT. 0) write (fmt(6:6),'(i1)') NDEC ! -1
+
+  IF (NDEC .GT. 0) then
+     n3 = min(n3, NDEC) ! try ndec, but only if it fits
+  end if
+
+  write (fmt(6:6),'(i1)') n3
   return
   end
 
