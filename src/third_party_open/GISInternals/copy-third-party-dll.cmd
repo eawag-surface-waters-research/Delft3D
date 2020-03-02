83681ec09cb4dccba70ed257039cbd5c8f79d727
Julian Hofer <Julian.Hofer@deltares.nl>
2020-03-02 10:57:55 +0000

    UNST-1414: Adds GISInternals, which contains proj and gdal binaries

---
+++ src/third_party_open/GISInternals/copy-third-party-dll.cmd
@@ -0,0 +1,33 @@
+@echo off
+
+set globalErrorLevel=0
+
+echo copy third party dlls...
+
+rem Usage:
+rem > copy-third-party-dlls.cmd <targetdir>
+
+rem with:
+rem   <targetdir>               : Target directory where the dlls are going to be installed by the script
+
+rem To be used by Visual Studio's post build events for dflowfm-interacter, dflowfm-cli and dflowfm-dll
+
+
+set TargetDir=%1
+set SourceDir=%~dp0\release-1911-x64\bin
+
+
+"xcopy.exe" /D /Y /R "%SourceDir%\xerces-c_3_2.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\expat.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\libpq.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\proj_6_1.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\sqlite3.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\spatialite.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\libmysql.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\geos_c.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\openjp2.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\proj.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\freexl.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\iconv.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\libxml2.dll" "%TargetDir%"\ > nul
+"xcopy.exe" /D /Y /R "%SourceDir%\geos.dll" "%TargetDir%"\ > nul
---
+++ src/third_party_open/GISInternals/release-1911-x64/include/mapserver-version.h
@@ -0,0 +1,11 @@
+#ifndef _MAPSERVER_VERSION_H
+#define _MAPSERVER_VERSION_H
+
+#define MS_VERSION_MAJOR 7
+#define MS_VERSION_MINOR 4
+#define MS_VERSION_REV 3
+#define MS_VERSION "7.4.3"
+#define MS_VERSION_NUM  70403 
+
+#endif
+
