324bc9901f0b650f7ed2e7b091280a4d421be86c
Arjen Markus <Arjen.Markus@deltares.nl>
2020-04-28 14:28:50 +0000

    UNST-3832: Add run_dflowfm_processes.sh to the set of shell scripts - complement of the batch file by that name.

---
+++ src/engines_gpl/dflowfm/scripts/team-city/run_dflowfm_processes.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+ulimit -s unlimited
+
+function print_usage_info {
+    echo "Usage: ${0##*/} [OPTION]... [--] [DFLOWFMOPTIONS]..."
+    echo "Run dflowfm program."
+    echo
+    echo
+    echo "Options for ${0##*/}:"
+    echo "-h, --help"
+    echo "       print this help message and exit"
+    echo
+    echo "-p|--processlibrary <proc_def>"
+    echo "       use an alternative process library file instead of $D3D_HOME/share/delft3d/proc_def"
+    echo
+    echo "-eco|--bloomspecies [<bloom.spe>]"
+    echo "       use BLOOM, optionally using an alternative algea database for the default $D3D_HOME/share/delft3d/bloom.spe"
+    echo
+    echo "--"
+    echo "       All following arguments are passed to D-Flow FM."
+    echo "       (optional)"
+    echo "Options for D-Flow FM, see dflowfm --help"
+
+    exit 1
+}
+
+# Estimates the number of physical cores based on /proc/cpuinfo (not counting hyperthreading)
+# If not found variable $numcores remains empty.
+function estimate_num_cores {
+    corespercpu=$(grep 'cpu cores' /proc/cpuinfo 2>/dev/null | head -1 | sed -e 's/.*:[ ]*\([0-9]*\)/\1/')
+    numcpu=$(grep 'physical id' /proc/cpuinfo 2>/dev/null | sort | uniq | wc -l)
+
+    if [ ! -z "$corespercpu" ] && [ ! -z "$numcpu" ]; then
+        numcores=$(( ${numcpu}*${corespercpu} ))
+    fi
+}
+
+function set_omp_threads {
+    estimate_num_cores
+
+    if [ ! -z "${OMP_NUM_THREADS}" ]; then
+        # Do nothing: someone has already set OMP_NUM_THREADS
+        :
+    #elif [ ! -z "$numcores" ]; then
+        # Use numcores-1 (if multiple cores at all)
+        # Set OMP_NUM_THREADS variable in current process (no need to export)
+    #    OMP_NUM_THREADS=$((numcores > 2 ? numcores-1 : 1))
+    else
+        # Could not determine numcores, leave OMP_NUM_THREADS empty.
+        :
+    fi
+}
+
+#
+# Locations
+#
+scriptdirname=`readlink \-f \$0`
+scriptdir=`dirname $scriptdirname`
+export D3D_HOME=$scriptdir/..
+export LD_LIBRARY_PATH=$D3D_HOME/lib:$LD_LIBRARY_PATH
+
+#
+# Analyse the options
+#
+while [[ $# -ge 1 ]]
+do
+key="$1"
+shift
+case $key in
+    -h|--help)
+    print_usage_info
+    ;;
+    -p|--processlibrary)
+    userprocfile="$1"
+    shift
+    ;;
+    -eco|--bloomspecies)
+    eco=true
+    if [[ $# -ge 1 ]]
+        then
+        userspefile="$1" ## using only -eco would result in using the default spe-file in $D3D_HOME/share/delft3d/
+        shift
+    else
+        userspefile=none
+    fi
+    ;;
+    --)
+    dfmoptions=$*
+    break	# exit loop, all remaining options to dflowfm executable
+    ;;
+    *)
+    dfmoptions="$key $*"
+    break	# exit loop, $key+all remaining options to dflowfm executable
+    ;;
+esac
+done
+
+if [ ! "$userprocfile" == "" ]
+    then
+    procfile=$userprocfile
+else
+    procfile=$D3D_HOME/share/delft3d/proc_def
+fi
+
+if [ ! -f $procfile ]; then
+    if [ ! -f $procfile.dat ]; then
+        echo "ERROR: procfile $procfile does not exist"
+        print_usage_info
+    fi
+fi
+
+spefile=$D3D_HOME/share/delft3d/bloom.spe
+if [ "$eco" == "true" ]
+   then
+   if [ ! -f $userspefile ]; then
+       if [ ! -f $spefile ]; then
+          echo "ERROR: default bloom.spe $spefile does not exist"
+          echo "ERROR: the optional specified bloom.spe $userspefile does not exist either"
+          print_usage_info
+       else
+          echo "Using default bloom.spe"
+       fi
+   else
+       echo "Using specified bloom.spe $userspefile"
+       spefile=$userspefile
+   fi
+fi
+
+
+set_omp_threads
+
+if [ "$eco" == "true" ]
+   then
+       echo $D3D_HOME/bin/dflowfm --nodisplay --autostartstop --processlibrary $procfile --bloomspecies $spefile $dfmoptions
+       echo $D3D_HOME/bin/dflowfm --nodisplay --autostartstop --processlibrary $procfile --bloomspecies $spefile $dfmoptions
+   else
+       echo $D3D_HOME/bin/dflowfm --nodisplay --autostartstop --processlibrary $procfile $dfmoptions
+       echo $D3D_HOME/bin/dflowfm --nodisplay --autostartstop --processlibrary $procfile $dfmoptions
+   fi
+
