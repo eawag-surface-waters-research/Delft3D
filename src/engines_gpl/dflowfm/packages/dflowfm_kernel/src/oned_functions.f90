894d9af27f43999f347d649e9141f777f4884248
Jan Noort <Jan.Noort@deltares.nl>
2020-05-15 12:28:42 +0000

    UNST-3881: bug fix: the status was set to false too often.

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/oned_functions.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/oned_functions.f90
@@ -634,6 +634,9 @@ module m_oned_functions
          else
             vp = vp2
          end if
+      else
+         struct%pump%ss_trigger = .false.
+         struct%pump%ds_trigger = .false.
       endif          
 
       ! Finally, redistribute the requested pump discharge across all flow links.
@@ -642,8 +645,6 @@ module m_oned_functions
          struct%fu = 0d0
          struct%ru = 0d0
          struct%au = 0d0
-         struct%pump%ss_trigger = .false.
-         struct%pump%ds_trigger = .false.
       else
 
          ! Limit the pump discharge in case the volume in the cells at the suction side is limited.
