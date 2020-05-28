4d257743f6302c9ec6ef09ef667c1f17e38166a0
Bert Jagers <bert.jagers@deltares.nl>
2020-05-28 07:20:36 +0000

    DELFT3D-37516: Better error checking when reading a box file ... report if number of values doesn't match expectations.

--- src/tools_lgpl/matlab/quickplot/progsrc/private/boxfile.m
+++ src/tools_lgpl/matlab/quickplot/progsrc/private/boxfile.m
@@ -162,9 +162,18 @@ try
                 if isempty(vals)
                     vals =[];
                 end
-                while length(vals)<nval2read
-                    xvals = fscanf(fid,'%f%*[ ,]',[1 nval2read-length(vals)]);
-                    vals = [vals xvals];
+                nvalread = length(vals);
+                if nvalread < nval2read
+                    vals(1,nval2read) = 0;
+                end
+                while nvalread < nval2read
+                    [xvals,nval] = fscanf(fid,' %f%*[ ,]',[1 nval2read-nvalread]);
+                    if nval==0
+                        str = fgetl(fid);
+                        error('Error while reading "%s".\nExpected to read %i values for BOX MNMN=(%i, %i, %i, %i); only %i values read.\n',str,nval2read,data{i,2},nvalread);
+                    end
+                    vals(nvalread+1:nvalread+nval) = xvals;
+                    nvalread = nvalread + nval;
                 end
                 data{i,3} = reshape(vals,[MNMN(4)-MNMN(2)+1 MNMN(3)-MNMN(1)+1])';
             otherwise
