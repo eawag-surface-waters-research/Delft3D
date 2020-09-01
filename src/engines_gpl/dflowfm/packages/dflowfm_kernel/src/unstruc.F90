96ae83654d7ea4c5cf889c862324f66236398aaf
Michelle Jeuken <Michelle.Jeuken@deltares.nl>
2020-09-01 09:35:16 +0000

    UNST-4315 Subroutines gettau and gettaus schould not be part of wrwaq I moved the subroutines gettau and gettaus from wrwaq.F90 to unstruc.F90, close to gettau2 because they are generally used.

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/unstruc.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/unstruc.F90
@@ -34233,6 +34233,48 @@ endif
 
 end subroutine getustwav
 
+subroutine gettaus(typout)
+
+use m_flowgeom
+use m_flow 
+use m_alloc
+
+implicit none
+integer, intent (in)       ::  typout   !< type of setting, 1: set czs and taus, 2: just set czs:
+
+double precision           ::  taucurc  !< local variable for taucurrent
+double precision           ::  czc      !< local variable for chezy
+integer                    ::  ierr     !< Error code
+integer                    ::  n        !< Counter
+
+if (.not. allocated(czs) ) then  
+    call realloc(czs,  ndxi,  keepExisting = .false., fill = 0d0, stat = ierr)
+else if (size(czs) < ndxi) then
+    call realloc(czs,  ndxi,  keepExisting = .false., fill = 0d0, stat = ierr)
+endif
+if (typout == 1) then
+    if (.not. allocated(taus) ) then  
+        call realloc(taus,  ndxi,  keepExisting = .false., fill = 0d0, stat = ierr)
+    else if (size(taus) < ndxi) then
+        call realloc(taus,  ndxi,  keepExisting = .false., fill = 0d0, stat = ierr)
+    endif
+endif 
+
+do n = 1,ndxi
+   call gettau(n,taucurc,czc)
+   czs(n) = czc
+   if (typout == 1) then
+       taus(n) = taucurc
+   endif     
+enddo
+end subroutine gettaus
+
+subroutine gettau(n,taucurc,czc)
+integer          :: n
+double precision :: taucurc,czc,ustw2
+call gettau2(n,taucurc,czc,ustw2)
+end subroutine gettau
+
 subroutine gettau2(n,taucurc,czc,ustw2)
 use m_flowgeom
 use m_flow
