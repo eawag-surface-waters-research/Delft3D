20452e669ba5c4c894fe6c0bd0c48398c180b1b5
Huib Tanis <Huib.Tanis@deltares.nl>
2020-10-07 07:59:54 +0000

    Issue: TEAMCITY-259: Script call has been modified.

--- examples/run_all_examples.bat
+++ examples/run_all_examples.bat
@@ -6,76 +6,97 @@ set TCL_EXE=..\..\src\third_party_open\tcl\bin\win32\tclkit.exe
 
 echo "Running testcase 01_standard ..."
 cd 01_standard
-call run_flow2d3d.bat >screen.log 2>&1
+call run.bat >screen.log 2>&1
 
 
 echo "Running testcase 01_standard parallel ..."
-call run_flow2d3d_parallel.bat >screen_parallel.log 2>&1
+call run_parallel.bat >screen_parallel.log 2>&1
 cd ..
 
 
 echo "Running testcase 02_domaindecomposition ..."
 cd 02_domaindecomposition
-call run_flow2d3d.bat >screen.log 2>&1
+call run.bat >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 03_flow-wave ..."
 cd 03_flow-wave
-call run_flow2d3d.bat >screen.log 2>&1
+call run.bat >screen.log 2>&1
 
 
 echo "Running testcase 03_flow-wave parallel ..."
-call run_flow2d3d_parallel_wave.bat >screen_parallel.log 2>&1
+call run_parallel.bat >screen_parallel.log 2>&1
 cd ..
 
 
 echo "Running testcase 04_fluidmud ..."
 cd 04_fluidmud
-call run_flow2d3d_flm.bat >screen.log 2>&1
+call run.bat >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 05_mormerge ..."
 cd 05_mormerge\merge
-call run_flow2d3d_wave_mormerge.bat >screen.log 2>&1
+call run.bat >screen.log 2>&1
+
+echo "Running testcase 05_mormerge (alternative( ..."
+call run_mormerge_alternative.bat >screen_mormerge_alternative.log 2>&1
 cd ..\..
 
 
 echo "Running testcase 06_delwaq ..."
 cd 06_delwaq
-call run_delwaq.bat >screen.log 2>&1
+call run.bat >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 07_wave ..."
 cd 07_wave
-call run_wave.bat >screen.log 2>&1
+call run.bat >screen.log 2>&1
+
+echo "Running testcase 07_wave (via dimr) ..."
+call run_wave_via_dimr.bat >screen_wave_via_dimr.log 2>&1
 cd ..
 
 
 echo "Running testcase 08_part-tracer ..."
 cd 08_part-tracer
-call run_part.bat >screen.log 2>&1
+call run.bat >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 09_part-oil ..."
 cd 09_part-oil
-call run_part.bat >screen.log 2>&1
+call run.bat >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 10_delwaq-part-tracer ..."
 cd 10_delwaq-part-tracer
-call run_delwaq_part.bat >screen.log 2>&1
+call run.bat >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 11_standard_netcdf ..."
 cd 11_standard_netcdf
-call run_flow2d3d.bat >screen.log 2>&1
+call run.bat >screen.log 2>&1
 cd ..
+
+echo "Running testcase 12_dflowfm (test_data) e02_f14_c040_westerscheldt"
+cd 12_dflowfm\test_data\e02_f14_c040_westerscheldt
+call run.bat >screen.log 2>&1
+cd ..
+
+echo "Running testcase 12_dflowfm (test_data) e100_f02_c02-FriesianInlet_schematic_FM"
+cd e100_f02_c02-FriesianInlet_schematic_FM
+call run.bat >screen.log 2>&1
+cd ..
+
+echo "Running testcase 12_dflowfm (test_data) e100_f02_c02-FriesianInlet_schematic_FM_wave"
+cd e100_f02_c02-FriesianInlet_schematic_FM_wave
+call run.bat >screen.log 2>&1
+cd ../../..
 
 echo ...finished
 pause
--- examples/run_all_examples.sh
+++ examples/run_all_examples.sh
@@ -1,78 +1,95 @@
 #!/bin/bash
 
+export PATH=$PATH:/usr/lib64/mpich/bin/
 
 echo "Running testcase 01_standard ..."
 cd 01_standard
-./run_flow2d3d.sh >screen.log 2>&1
+./run.sh >screen.log 2>&1
 
 
 echo "Running testcase 01_standard parallel ..."
-./run_flow2d3d_parallel.sh >screen_parallel.log 2>&1
+./run_parallel.sh >screen_parallel.log 2>&1
 cd ..
 
 
 echo "Running testcase 02_domaindecomposition ..."
 cd 02_domaindecomposition
-./run_flow2d3d.sh >screen.log 2>&1
+./run.sh >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 03_flow-wave ..."
 cd 03_flow-wave
-./run_flow2d3d.sh >screen.log 2>&1
+./run.sh >screen.log 2>&1
 
 
 echo "Running testcase 03_flow-wave parallel ..."
-./run_flow2d3d_parallel_wave.sh >screen_parallel.log 2>&1
+./run_parallel.sh >screen_parallel.log 2>&1
 cd ..
 
 
 echo "Running testcase 04_fluidmud ..."
 cd 04_fluidmud
-./run_flow2d3d_flm.sh >screen.log 2>&1
+./run.sh >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 05_mormerge ..."
 cd 05_mormerge/merge
-./run_flow2d3d_wave_mormerge.sh >screen.log 2>&1
+./run.sh >screen.log 2>&1
 cd ../..
 
 
 echo "Running testcase 06_delwaq ..."
 cd 06_delwaq
-./run_delwaq.sh >screen.log 2>&1
+./run.sh >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 07_wave ..."
 cd 07_wave
-./run_wave.sh >screen.log 2>&1
+./run.sh >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 08_part-tracer ..."
 cd 08_part-tracer
-./run_part.sh >screen.log 2>&1
+./run.sh >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 09_part-oil ..."
 cd 09_part-oil
-./run_part.sh >screen.log 2>&1
+./run.sh >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 10_delwaq-part-tracer ..."
 cd 10_delwaq-part-tracer
-./run_delwaq_part.sh >screen.log 2>&1
+./run.sh >screen.log 2>&1
 cd ..
 
 
 echo "Running testcase 11_standard_netcdf ..."
 cd 11_standard_netcdf
-./run_flow2d3d.sh >screen.log 2>&1
+./run.sh >screen.log 2>&1
 cd ..
+
+
+echo "Running testcase 12_dflowfm (test_data) e02_f14_c040_westerscheldt"
+cd 12_dflowfm/test_data/e02_f14_c040_westerscheldt
+./run.sh >screen.log 2>&1
+cd ..
+
+echo "Running testcase 12_dflowfm (test_data) e100_f02_c02-FriesianInlet_schematic_FM"
+cd e100_f02_c02-FriesianInlet_schematic_FM
+./run.sh >screen.log 2>&1
+cd ..
+
+echo "Running testcase 12_dflowfm (test_data) e100_f02_c02-FriesianInlet_schematic_FM_wave"
+cd e100_f02_c02-FriesianInlet_schematic_FM_wave
+./run.sh >screen.log 2>&1
+cd ../../..
 
 
 echo ...finished
