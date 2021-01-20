8d28cf086abaf2f388a7678ba0fe7bfecad42d64
Adri Mourits <Adri.Mourits@deltares.nl>
2021-01-20 11:03:54 +0000

    UNST-4714: Added to dimr_artifacts.py: merge lnx64/bin_* into lnx64/bin and lnx64/lib_* into lnx64/lib : If a target file already exists, do not overwrite Do not treat the lnx64/share directory like this: additions should end up in a separate subdirectory (e.g. lnx64/share/drtc)

--- src/engines_gpl/dimr/scripts/dimr_artifacts.py
+++ src/engines_gpl/dimr/scripts/dimr_artifacts.py
@@ -1,7 +1,7 @@
 '''
 Description: DIMR artifacts organisator
 -----------------------------------------------------
-Copyright (C)  Stichting Deltares, 2018
+Copyright (C)  Stichting Deltares, 2021
 '''
 
 from __future__ import print_function   # if code has to work in python 2 and 3!
@@ -15,58 +15,78 @@ def usage():
 
 def platformArtifacts(platform):
     print("\n" + platform + "...")
-    if str(platform).find("lnx") >= 0:
-        libId = ".so"
-    else:
-        libId = ".dll"
     root=os.getcwd()
     pltdir=os.path.join(root, platform)
     #
+    # Linux: merge bin_* into bin and lib_* into lib
+    if str(platform).find("lnx") >= 0:
+        mergDirs = glob.glob(os.path.join(pltdir,"*_*"))
+        for aMergeDir in mergDirs:
+            splitIndex = aMergeDir.rfind("_")
+            destination = aMergeDir[:splitIndex]
+            if (os.path.isdir(destination)):
+                print("Merging " + aMergeDir + " into " + destination)
+                for (path, dirs, files) in os.walk(aMergeDir):
+                    for aFile in files:
+                        sourceFile = os.path.join(path,aFile)
+                        targetFile = sourceFile.replace(aMergeDir, destination)
+                        if (os.path.isfile(targetFile)):
+                            print("        Skipping already existing " + aFile)
+                        else:
+                            print("        Copy " + aFile)
+                            os.makedirs(os.path.dirname(targetFile), exist_ok=True)
+                            shutil.copyfile(sourceFile, targetFile)
+            else:
+                print("ERROR: Destination is not an existing directory: " + destination)
+            print("Removing " + aMergeDir)
+            shutil.rmtree(aMergeDir)
+
+    #
     # Remove/replace unwanted files
     for (path, dirs, files) in os.walk(pltdir):
-        for afile in files:
-            name, extension = os.path.splitext(afile)
+        for aFile in files:
+            name, extension = os.path.splitext(aFile)
             if extension == ".exp" or extension == ".lib" or extension == ".pdb" or extension == ".msm":
-                print("      To be removed: " + os.path.join(path,afile))
-                os.remove(os.path.join(path,afile))
+                print("      To be removed: " + os.path.join(path,aFile))
+                os.remove(os.path.join(path,aFile))
             # libifcore.so: replace by libifcoremt.so, unless it's inside share
-            if (str(afile).find("libifcore.so")==0 and str(path).find("share")==-1):
-                ifcoremt_files_withpath = glob.glob(os.path.join(path, "libifcoremt*"))
-                print("      Removing: " + str(os.path.join(path,afile)))
-                os.remove(os.path.join(path,afile))
-                for afile in ifcoremt_files_withpath:
-                   newfilename = os.path.join(path, "libifcore" + ntpath.basename(afile)[11:])
-                   print("      Copying: " + afile + " to: " + newfilename)
-                   shutil.copyfile(afile, newfilename)
+            if (str(aFile).find("libifcore.so")==0 and str(path).find("share")==-1):
+                ifcoremtFilesWithPath = glob.glob(os.path.join(path, "libifcoremt*"))
+                print("      Removing: " + str(os.path.join(path,aFile)))
+                os.remove(os.path.join(path,aFile))
+                for aFile in ifcoremtFilesWithPath:
+                   newfilename = os.path.join(path, "libifcore" + ntpath.basename(aFile)[11:])
+                   print("      Copying: " + aFile + " to: " + newfilename)
+                   shutil.copyfile(aFile, newfilename)
     #
     # Remove files that are in the share directory (currently only used on Windows)
     if (os.path.isdir(pltdir) and platform != "lib"):
         os.chdir(pltdir)
-        sharedir=os.path.join(pltdir, "share", "bin")
-        if os.path.isdir(sharedir):
-            print("  Share directory found: " + sharedir)
-            # Collect dll files in shared/bin in parameter "sharefiles"
-            sharefiles_withpath = glob.glob(os.path.join(sharedir, "*"))
-            sharefiles = []
-            for afile in sharefiles_withpath:
-                if str(afile).find(".txt") == -1:
-                    sharefiles.append(ntpath.basename(afile))
-            del sharefiles_withpath[:]
-            print("sharefiles:" + str(sharefiles))
+        shareDir=os.path.join(pltdir, "share", "bin")
+        if os.path.isdir(shareDir):
+            print("  Share directory found: " + shareDir)
+            # Collect dll files in shared/bin in parameter "shareFiles"
+            shareFilesWithPath = glob.glob(os.path.join(shareDir, "*"))
+            shareFiles = []
+            for aFile in shareFilesWithPath:
+                if str(aFile).find(".txt") == -1:
+                    shareFiles.append(ntpath.basename(aFile))
+            del shareFilesWithPath[:]
+            print("shareFiles:" + str(shareFiles))
             for (path, dirs, files) in os.walk(pltdir):
-                if str(path).find(sharedir) == -1:
+                if str(path).find(shareDir) == -1:
                     print("    Checking directory " + path + " ...")
-                    for afile in files:
-#                        if any(afile in asharefile for asharefile in sharefiles):
-                        if afile in sharefiles:
-                            print("      To be removed: " + os.path.join(path,afile))
-                            os.remove(os.path.join(path,afile))
+                    for aFile in files:
+                        # if any(aFile in asharefile for asharefile in shareFiles):
+                        if aFile in shareFiles:
+                            print("      To be removed: " + os.path.join(path,aFile))
+                            os.remove(os.path.join(path,aFile))
                             
         else:
-            print("  Share directory not found")
+            print("  Directory " + shareDir + "not found; nothing to do.")
         os.chdir(root)
     else:
-        print("Directory " + pltdir + " does not exist")
+        print("Directory " + pltdir + " does not exist.")
         return
 # === MAIN ===========================
 if len(sys.argv) != 2:
