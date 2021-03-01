382fb33bfe34f04b597b192c8463b5c2a1e69892
Mohamed Nabi <Mohamed.Nabi@deltares.nl>
2021-03-01 09:10:23 +0000

    UNST-4884 : 1D model does not give correction to the velocity field

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/unstruc.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/unstruc.F90
@@ -7345,6 +7345,10 @@ if (ihorvic > 0 .or. NDRAW(29) == 37) then
     endif
 
  endif
+ 
+   if (kmx == 0 .and. lnx1D > 0) then ! setuc
+      call setuc1D ()
+   endif
 
 end subroutine setucxucyucxuucyu
 
@@ -8947,42 +8951,7 @@ else if (icorio == 10) then                             ! vol2D type weigthings
  endif
 
  if (kmx == 0 .and. lnx1D > 0 ) then ! setuc
-    uc1D  = 0d0
-
-    do n  = ndx2D+1,ndxi
-       nx = nd(n)%lnx
-       if (nx == 2) then
-           do LL = 1,nx
-             L   = nd(n)%ln(LL)
-             La  = iabs(L)
-             if (iabs(kcu(La)) == 1) then
-                i12 = 2 ; if (L < 0) i12 = 1
-                if (LL == 1) then
-                    if (L  > 0) then
-                        ip =  1
-                    else
-                        ip = -1
-                    endif
-                else
-                    if (ip*L > 0) then
-                        ip = -ip
-                    endif
-                endif
-                uc1D(n) = uc1D(n) + wcL(i12,La)*u1(La)*ip
-              endif
-          enddo
-        endif
-    enddo
-
-    do LL = lnxi+1,lnx          ! bnd
-       if (kcu(LL) == -1) then  ! 1D type link
-           n = Ln(1,LL) ; k2 = Ln(2,LL)
-           if (uc1D(k2) .ne. 0) then
-               uc1D(n) = uc1D(k2)
-           endif
-       endif
-    enddo
-
+    call setuc1D ()
     u1Du  = 0d0
     if (pure1D > 0) then
        do L = 1,lnx
@@ -8998,6 +8967,55 @@ else if (icorio == 10) then                             ! vol2D type weigthings
 
 end subroutine setucxucyucxuucyunew
 
+! =================================================================================================
+! =================================================================================================
+   subroutine setuc1D ()
+      use m_flow
+      use m_flowgeom
+      implicit none
+      integer :: L, LL, La, n, nx, ip, i12, k2
+      
+      if (kmx == 0 .and. lnx1D > 0) then ! setuc
+         uc1D  = 0d0
+         do n  = ndx2D+1,ndxi
+            nx = nd(n)%lnx
+            if (nx == 2) then
+               do LL = 1,nx
+                  L   = nd(n)%ln(LL)
+                  La  = iabs(L)
+                  if (iabs(kcu(La)) == 1) then
+                     i12 = 2 ; if (L < 0) i12 = 1
+                     if (LL == 1) then
+                        if (L  > 0) then
+                           ip =  1
+                        else
+                           ip = -1
+                        endif
+                     else
+                        if (ip*L > 0) then
+                           ip = -ip
+                        endif
+                     endif
+                     uc1D(n) = uc1D(n) + wcL(i12,La)*u1(La)*ip
+                  endif
+               enddo
+            endif
+         enddo
+
+         do LL = lnxi+1,lnx          ! bnd
+            if (kcu(LL) == -1) then  ! 1D type link
+               n = Ln(1,LL) ; k2 = Ln(2,LL)
+               if (uc1D(k2) .ne. 0) then
+                  uc1D(n) = uc1D(k2)
+               endif
+            endif
+         enddo
+      endif
+      
+   end subroutine setuc1D
+   
+! =================================================================================================
+! =================================================================================================
 subroutine setucxy1D() ! give ucx,ucy magnitude of uc1D, Pure1D
 
 use m_flowgeom
