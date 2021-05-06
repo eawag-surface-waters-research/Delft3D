ccd64017c44459fea01a0ee9489526c1495ff33c
Adri Mourits <Adri.Mourits@deltares.nl>
2021-05-06 15:12:59 +0000

    UNST-4939: dimr/scripts/dimr_artifacts.py: Remove from DIMRset: removeFiles = [     'd_hydro',     'datsel',     'delpar',     'esm_create',     'esm_delete',     'esm_info',     'FBCTools',     'FBCTools.sh',     'kubint',     'lint',     'nesthd1',     'nesthd2',     'vs',     'waqpbexport',     'waqpbimport',     'dioconfig.ini',     'files.def',     'tabmom',     'rr.exe',     'rr_dll_runner.exe',     'FBCTools.exe',     'FBCTools_OpenMI.dll',     'tclkitsh852.exe',     'run_dflow2d3d.sh',     'run_dflow2d3d_dwaves.sh',     'run_dflow2d3d_fluidmud.sh',     'run_dflow2d3d_parallel.sh',     'run_dflow2d3d_parallel_dwaves.sh',     'rd2d3d.sh',     'submit_dflow2d3d.sh'     ] removeDirs = [     'share/doc',     'share/man'     ]

--- src/engines_gpl/dimr/scripts/dimr_artifacts.py
+++ src/engines_gpl/dimr/scripts/dimr_artifacts.py
@@ -7,6 +7,43 @@ Copyright (C)  Stichting Deltares, 2021
 from __future__ import print_function   # if code has to work in python 2 and 3!
 import os, re, sys, glob, ntpath, shutil
 
+removeFiles = [
+    'd_hydro',
+    'datsel',
+    'delpar',
+    'esm_create',
+    'esm_delete',
+    'esm_info',
+    'FBCTools',
+    'FBCTools.sh',
+    'kubint',
+    'lint',
+    'nesthd1',
+    'nesthd2',
+    'vs',
+    'waqpbexport',
+    'waqpbimport',
+    'dioconfig.ini',
+    'files.def',
+    'tabmom',
+    'rr.exe',
+    'rr_dll_runner.exe',
+    'FBCTools.exe',
+    'FBCTools_OpenMI.dll',
+    'tclkitsh852.exe',
+    'run_dflow2d3d.sh',
+    'run_dflow2d3d_dwaves.sh',
+    'run_dflow2d3d_fluidmud.sh',
+    'run_dflow2d3d_parallel.sh',
+    'run_dflow2d3d_parallel_dwaves.sh',
+    'rd2d3d.sh',
+    'submit_dflow2d3d.sh'
+    ]
+removeDirs = [
+    'share/doc',
+    'share/man'
+    ]
+
 def usage():
     print("Usage:")
     print("  " + sys.argv[0] + " <directory>")
@@ -36,17 +73,17 @@ def platformArtifacts(platform):
                             print("        Copy " + aFile)
                             os.makedirs(os.path.dirname(targetFile), exist_ok=True)
                             shutil.copyfile(sourceFile, targetFile)
-            else:
-                print("ERROR: Destination is not an existing directory: " + destination)
-            print("Removing " + aMergeDir)
-            shutil.rmtree(aMergeDir)
+                print("Removing " + aMergeDir)
+                shutil.rmtree(aMergeDir)
+            # else:
+            #    This is a file. Nothing to do
 
     #
     # Remove/replace unwanted files
     for (path, dirs, files) in os.walk(pltdir):
         for aFile in files:
             name, extension = os.path.splitext(aFile)
-            if extension == ".exp" or extension == ".lib" or extension == ".pdb" or extension == ".msm":
+            if extension == ".exp" or extension == ".lib" or extension == ".pdb" or extension == ".msm" or extension == ".a" or extension == ".la" or extension == ".lai":
                 print("      To be removed: " + os.path.join(path,aFile))
                 os.remove(os.path.join(path,aFile))
             # libifcore.so: replace by libifcoremt.so, unless it's inside share
@@ -58,6 +95,18 @@ def platformArtifacts(platform):
                    newfilename = os.path.join(path, "libifcore" + ntpath.basename(aFile)[11:])
                    print("      Copying: " + aFile + " to: " + newfilename)
                    shutil.copyfile(aFile, newfilename)
+            print("A3M: path name : " + path + "    " + name)
+            if (name+extension in removeFiles):
+                print("      Removing file: " + str(os.path.join(path,aFile)))
+                os.remove(os.path.join(path,aFile))
+        path_ = str(path).replace("\\","/")
+        for aRemoveDir in removeDirs:
+            #print("A3M:aRemoveDir: " + aRemoveDir)
+            #print("A3M:path: " + path)
+            if (str(path_).find(aRemoveDir)>1):
+                print("      Removing directory: " + str(path))
+                shutil.rmtree(path, ignore_errors=True)
+                
     #
     # Remove files that are in the share directory (currently only used on Windows)
     if (os.path.isdir(pltdir) and platform != "lib"):
@@ -83,11 +132,13 @@ def platformArtifacts(platform):
                             os.remove(os.path.join(path,aFile))
                             
         else:
-            print("  Directory " + shareDir + "not found; nothing to do.")
+            print("  Directory '" + shareDir + "' not found; nothing to do.")
         os.chdir(root)
     else:
-        print("Directory " + pltdir + " does not exist.")
+        print("Directory '" + pltdir + "' does not exist. Nothing to do.")
         return
+
+
 # === MAIN ===========================
 if len(sys.argv) != 2:
     print("ERROR: wrong number of arguments")
