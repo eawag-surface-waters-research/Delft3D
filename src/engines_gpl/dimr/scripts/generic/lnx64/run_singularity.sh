006503d15dd99a223e0b6162d05be91d4cba3ff0
Dennis Tang <Dennis.Tang@deltares.nl>
2021-06-10 11:33:55 +0000

    UNST-5184 Adjusted run_singularity.sh script to require a dimr config file without the -m flag: - Parse the remaining arguments after the dimr config file as extra options to the run_dimr.sh

--- src/engines_gpl/dimr/scripts/generic/lnx64/run_singularity.sh
+++ src/engines_gpl/dimr/scripts/generic/lnx64/run_singularity.sh
@@ -1,42 +1,53 @@
 #!/bin/sh
 
 function print_usage_info {
-    echo "Usage: ${0##*/} [OPTION]..."
+    echo "Usage: ${0##*/} dimr_config_file [RUN_DIMR.SH OPTIONS]..."
     echo "Runs the run_dimr.sh command of a Singularity container by wrapping and passing additional arguments."
     echo
     echo "Options:"
     echo "-h, --help"
     echo "       Print this help message and exit"
+    echo
+    echo "Examples:"
+    echo "run_dimr dimr_config_file.xml [RUN_DIMR.SH OPTIONS]"
+    echo "run_dimr [RUN_DIMR.SH OPTIONS]"
+    echo "run_dimr [-h | --help]"
     exit 1
 }
 
 # Variables
 executable=run_dimr.sh
 executable_extraopts=
+dimr_config_file=
 container_libdir=/opt/delft3dfm_latest/lnx64/bin/ # The directory WITHIN the container that contains all the executables
 
 # Main
-while [[ $# -ge 1 ]]
-do
-key="$1"
-shift
+if [[ $# -eq 0 ]]; then
+    print_usage_info
+fi
 
-case $key in
+# Parse the first argument of the script
+if [[ $# -ge 1 ]]; then
+arg=$1
+case $arg in
     -h|--help)
     print_usage_info
     ;;
-    --)
-    echo "-- sign detected, option passed to $executable"
-    executable_extraopts="$executable_extraopts $*"
-    break       # exit loop, stop shifting, all remaining arguments without dashes handled below
-    ;;
-    -*)
-    echo "option ${key} passed to $executable"
-    executable_extraopts="$key $*"
-    break       # exit loop, $key+all remaining options to the executable
+    *)
+    dimr_config_file=$arg
     ;;
 esac
-done
+shift
+
+# Parse the remaining arguments and pass it as additional arguments to the executable as extra options
+executable_extraopts="$*"
+fi
+
+# Check if the dimr_config_file exists
+if [ ! -z "$dimr_config_file" ] && [ ! -f "$dimr_config_file" ]; then
+    echo "ERROR: Dimr config file must be set and existent."
+    exit 2 # Exit code 2 to indicate that no such file is present
+fi
 
 # Retrieve the script directory
 scriptdirname=`readlink \-f \$0`
@@ -59,5 +70,6 @@ shopt -u nullglob
 echo "Executing Singularity container with:"
 echo "Singularity container directory   :   $scriptdir"
 echo "Singularity container name        :   $container_file_path"
+echo "Dimr file                         :   $dimr_config_file"
 echo "Extra executable flags            :   $executable_extraopts"
-singularity exec $container_file_path $container_libdir/$executable $executable_extraopts
+singularity exec $container_file_path $container_libdir/$executable -m $dimr_config_file $executable_extraopts
