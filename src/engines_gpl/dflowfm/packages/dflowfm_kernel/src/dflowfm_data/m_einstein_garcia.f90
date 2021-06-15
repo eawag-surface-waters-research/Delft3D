d1d0cc00ea1465d70264f83cd2aa1576ee41fb78
Joost Bonnet <Joost.Bonnet@deltares.nl>
2021-06-15 13:09:05 +0000

    Reverted: UNST-4107: Removed unused m_einstein_garcia module and einstein_init subroutine

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/m_einstein_garcia.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/m_einstein_garcia.f90
@@ -30,11 +30,7 @@
 ! $Id$
 ! $HeadURL$
 
-double precision function ucrouse(z,z0,h,a,rs)
-use m_einstein_garcia
+module m_einstein_garcia              ! integral polynomial coefficients, (sedimentation engineering garcia table 2.5
 implicit none
-double precision :: z, z0, h, a, rs
-
-ucrouse = log(z/z0) * ( (a/(h-a))*( (h-z)/z) )** rs
-
-end function
+double precision :: c1(5,0:6), c2(5,0:6), d(1:5)
+end module m_einstein_garcia
