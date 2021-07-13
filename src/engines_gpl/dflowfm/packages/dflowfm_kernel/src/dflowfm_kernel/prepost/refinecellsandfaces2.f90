9b3df65ef344cc1986cc2665e5b015f82b3446eb
Herman Kernkamp <Herman.Kernkamp@deltares.nl>
2021-07-13 14:37:37 +0000

    UNST-5035, UNST-5236 unstrucversion = 1.2.129    ! date  = 20210713                             ! UNST-5035, adjustment of courant refinement based on Jelmer findings

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/refinecellsandfaces2.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/refinecellsandfaces2.f90
@@ -710,7 +710,7 @@ subroutine refinecellsandfaces2()
                dmiss, jsferic, jasfer3D, JINS, NPL, xpl, ypl, zpl, errorInfo)
             else if (interpolationtype == 4) then
                landsea = 0
-               call bilinarcinfocheck( xc(1), yc(1), zc(1), landsea ) 
+               !call bilinarcinfocheck( xc(1), yc(1), zc(1), landsea ) 
                zmx = -1d9; zmn = 1d9
                do m = 1,N
                    call bilinarcinfo( x(m), y(m), z(m) )
@@ -718,9 +718,9 @@ subroutine refinecellsandfaces2()
                    zmn = min(zmn, z(m))
                enddo
                if (zmn > 0d0 ) then          ! land 
-                  zc(1) = 9d9                ! no refine
+                  zc(1) = 9d9  ; landsea = 3              ! no refine
                else if (zmx >= 0d0 .and. zmn <= 0d0) then ! land/sea
-                  zc(1)   = 0d9              ! always refine 
+                  zc(1) = 0d9  ; landsea = 1              ! always refine 
                endif
             endif
 
