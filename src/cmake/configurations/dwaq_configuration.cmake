c991fd0b141cb108ef4ab87330f26ad61aa9d20a
Adri Mourits <Adri.Mourits@deltares.nl>
2021-07-22 08:11:02 +0000

    UNST-5183: Build WAQ tools via CMake

--- src/cmake/configurations/dwaq_configuration.cmake
+++ src/cmake/configurations/dwaq_configuration.cmake
@@ -1,15 +1,9 @@
 project(dwaq)
 
-# Specify the modules to be included
-if(NOT TARGET deltares_common) 
-    add_subdirectory(${checkout_src_root}/${deltares_common_module} deltares_common)
-endif()
 
-if(NOT TARGET deltares_common_c)
-    add_subdirectory(${checkout_src_root}/${deltares_common_c_module} deltares_common_c)
-endif()
-
-# Waq
+#
+# WAQ
+#=============
 if(NOT TARGET waq_plugin_wasteload)
     add_subdirectory(${checkout_src_root}/${waq_plugin_wasteload_module} waq_plugin_wasteload)
 endif()
@@ -47,6 +41,68 @@ if(NOT TARGET delwaq2)
 endif()
 
 
+
+#
+# PART
+#=============
+if(NOT TARGET part_data_f)
+    add_subdirectory(${checkout_src_root}/${part_data_f_module} part_data_f)
+endif()
+
+if(NOT TARGET part_utils_f)
+    add_subdirectory(${checkout_src_root}/${part_utils_f_module} part_utils_f)
+endif()
+
+if(NOT TARGET part_io_f)
+    add_subdirectory(${checkout_src_root}/${part_io_f_module} part_io_f)
+endif()
+
+if(NOT TARGET part_kernel_f)
+    add_subdirectory(${checkout_src_root}/${part_kernel_f_module} part_kernel_f)
+endif()
+
+
+
+#
+# WAQ Tools
+#=============
+# Waqpb
+if(NOT TARGET waqpb_export)
+    add_subdirectory(${checkout_src_root}/${waqpb_export_module} waqpb_export)
+endif()
+if(NOT TARGET waqpb_import)
+    add_subdirectory(${checkout_src_root}/${waqpb_import_module} waqpb_import)
+endif()
+if(NOT TARGET waqpb_lib)
+    add_subdirectory(${checkout_src_root}/${waqpb_lib_module} waqpb_lib)
+endif()
+
+# Waq_run_processes
+if(NOT TARGET waq_run_processes)
+    add_subdirectory(${checkout_src_root}/${waq_run_processes_module} waq_run_processes)
+endif()
+if(NOT TARGET waq_run_processes_version_number)
+    add_subdirectory(${checkout_src_root}/${waq_run_processes_version_number_module} waq_run_processes_version_number)
+endif()
+
+# duprol2delwaq
+if(NOT TARGET duprol2delwaq)
+    add_subdirectory(${checkout_src_root}/${duprol2delwaq_module} duprol2delwaq)
+endif()
+
+
+
+#
+# Utils
+#=============
+# Deltares_common
+if(NOT TARGET deltares_common) 
+    add_subdirectory(${checkout_src_root}/${deltares_common_module} deltares_common)
+endif()
+if(NOT TARGET deltares_common_c)
+    add_subdirectory(${checkout_src_root}/${deltares_common_c_module} deltares_common_c)
+endif()
+
 # netcdf
 if(WIN32)
     if(NOT TARGET netcdff)
@@ -68,41 +124,26 @@ endif()
 if(NOT TARGET delftio_shm)
     add_subdirectory(${checkout_src_root}/${delftio_shm_module} delftio_shm)
 endif()
-
 if(NOT TARGET delftio)
     add_subdirectory(${checkout_src_root}/${delftio_module} delftio)
 endif()
 
-# emfsm
+# esmfsm
 if(NOT TARGET esmfsm_version_number)
     add_subdirectory(${checkout_src_root}/${esmfsm_version_number_module} esmfsm_version_number)
 endif()
-
 if(NOT TARGET esmfsm_c)
     add_subdirectory(${checkout_src_root}/${esmfsm_c_module} esmfsm_c)
 endif()
-
 if(NOT TARGET esmfsm)
     add_subdirectory(${checkout_src_root}/${esmfsm_module} esmfsm)
 endif()
 
-# part
-if(NOT TARGET part_data_f)
-    add_subdirectory(${checkout_src_root}/${part_data_f_module} part_data_f)
-endif()
 
-if(NOT TARGET part_utils_f)
-    add_subdirectory(${checkout_src_root}/${part_utils_f_module} part_utils_f)
-endif()
 
-if(NOT TARGET part_io_f)
-    add_subdirectory(${checkout_src_root}/${part_io_f_module} part_io_f)
-endif()
-
-if(NOT TARGET part_kernel_f)
-    add_subdirectory(${checkout_src_root}/${part_kernel_f_module} part_kernel_f)
-endif()
-
+#
+# Linux installation
+#=============
 if(UNIX)
     add_subdirectory(${checkout_src_root}/cmake/install_waq install_waq)
 endif()
