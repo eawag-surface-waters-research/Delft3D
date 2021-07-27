3135d147d7ff125e83fa4d45f541ca2e6ce362eb
Adri Mourits <Adri.Mourits@deltares.nl>
2021-07-27 06:53:00 +0000

    Reverted revision(s) 69494 from trunk: UNST-5183: Second try: Switch to compiling D-WAQ via CMake (Windows and Linux) The waq tools are taken into account, as far as they appear in the installation ........ The WAQ Windows testbench shows 63 additional testcases with different results. See (pinned): https://dpcbuild.deltares.nl/buildConfiguration/DimrTestbench_DWaq_Win64/766957

--- src/cmake/configurations/all_configuration.cmake
+++ src/cmake/configurations/all_configuration.cmake
@@ -4,7 +4,7 @@
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dflowfm_configuration.cmake)
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dwaq_configuration.cmake)
+# Not officially supported yet: include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dwaq_configuration.cmake)
 
 # Not officially supported yet: include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dwaves_configuration.cmake)
 
