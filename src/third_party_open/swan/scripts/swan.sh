7cf540d6c8e02e376f4269033dc0cc0e8440f1a9
Edwin Spee <Edwin.Spee@deltares.nl>
2021-10-11 12:57:44 +0000

    UNST-5302 on linux, swan executable not found during wave run

--- src/third_party_open/swan/scripts/swan.sh
+++ src/third_party_open/swan/scripts/swan.sh
@@ -70,9 +70,9 @@ D3D_HOME=$scriptdir/..
 MACHINE_TYPE=`uname -m`
  
 if [ $mpirun -eq 1 ]; then
-    SWANEXEC=${D3D_HOME}/bin/swan_mpi.exe
+    SWANEXEC=${D3D_HOME}/bin/swan_mpi
 else
-    SWANEXEC=${D3D_HOME}/bin/swan_omp.exe
+    SWANEXEC=${D3D_HOME}/bin/swan_omp
   #
   # swan40.72AB and newer runs parallel using OpenMP, using the total number of cores on the machine by default
   # Two ways to force the number of parallel processes:
