40f134d69d3074825a96ddce3061af484014b6b6
Maarten Klapwijk <maarten.klapwijk@deltares.nl>
2021-10-29 14:13:25 +0000

    Issue: UNST-5200 Addition of lpgl licence header to src files, after moving from DS-tree into OSS-tree

--- src/tools_gpl/agrhyd/packages/agrhyd/src/agr_poi_exp.f
+++ src/tools_gpl/agrhyd/packages/agrhyd/src/agr_poi_exp.f
@@ -1,3 +1,26 @@
+!!  Copyright (C)  Stichting Deltares, 2021-2021.
+!!
+!!  This program is free software: you can redistribute it and/or modify
+!!  it under the terms of the GNU General Public License version 3,
+!!  as published by the Free Software Foundation.
+!!
+!!  This program is distributed in the hope that it will be useful,
+!!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+!!  GNU General Public License for more details.
+!!
+!!  You should have received a copy of the GNU General Public License
+!!  along with this program. If not, see <http://www.gnu.org/licenses/>.
+!!
+!!  contact: delft3d.support@deltares.nl
+!!  Stichting Deltares
+!!  P.O. Box 177
+!!  2600 MH Delft, The Netherlands
+!!
+!!  All indications and logos of, and references to registered trademarks
+!!  of Stichting Deltares remain the property of Stichting Deltares. All
+!!  rights reserved.
+
       subroutine agr_poi_exp( ipnt  , ipnt_b, input_hyd, output_hyd, ipnt_q)
 
       ! function : set agggregation pointer for the pointers, expand case
