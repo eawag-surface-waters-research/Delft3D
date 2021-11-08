e1ad02daabc7ee8e33e3d2516e72e7beb8937d90
Jan Mooiman <Jan.Mooiman@deltares.nl>
2021-11-08 15:05:42 +0000

    issue UNST-4586 User Manual directory moved to https://repos.deltares.nl/repos/ds/trunk/doc/user_manuals/delta-shell/dimr as part of dimr_trm (chapter architecture)

--- src/engines_gpl/dimr/doc/DIMR-TechnicalDesign/buildDoc.bat
+++
@@ -1,6 +0,0 @@
-pdflatex "DIMR-TechnicalDesign.tex"
-bibtex "DIMR-TechnicalDesign"
-pdflatex "DIMR-TechnicalDesign.tex"
-pdflatex "DIMR-TechnicalDesign.tex" > DIMR_TD_Log.txt
-xcopy DIMR_TD_Log.txt "..\BuildLogs" /Y
-xcopy *.pdf .. /Y
