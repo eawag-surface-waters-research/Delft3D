b0d90e52b7914103e1fa8a24cc64d7886d0a1d4c
Jan Mooiman <Jan.Mooiman@deltares.nl>
2021-11-08 15:09:33 +0000

    issue UNST-4586 Empty testreport document removed

--- src/engines_gpl/dimr/doc/DIMR-TestReport/buildDoc.bat
+++
@@ -1,6 +0,0 @@
-pdflatex "DIMR-TestReport.tex"
-bibtex "DIMR-TestReport"
-pdflatex "DIMR-TestReport.tex"
-pdflatex "DIMR-TestReport.tex" > DIMR_TR_Log.txt
-xcopy DIMR_TR_Log.txt "..\BuildLogs" /Y
-xcopy *.pdf .. /Y
