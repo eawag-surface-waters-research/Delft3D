b264d5bbe1ba25fb7c240b87a9956fe4faab627c
Jan Mooiman <Jan.Mooiman@deltares.nl>
2021-11-08 15:08:14 +0000

    issue UNST-4586 Empty testplan document removed

--- src/engines_gpl/dimr/doc/DIMR-TestPlan/buildDoc.bat
+++
@@ -1,6 +0,0 @@
-pdflatex "DIMR-TestPlan.tex"
-bibtex "DIMR-TestPlan"
-pdflatex "DIMR-TestPlan.tex"
-pdflatex "DIMR-TestPlan.tex" > DIMR_TP_Log.txt
-xcopy DIMRL_TP_Log.txt "..\BuildLogs" /Y
-xcopy *.pdf .. /Y
