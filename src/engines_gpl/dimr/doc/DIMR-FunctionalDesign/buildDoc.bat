e99b4ed5714b0db3c4d1d6710038962be042f6d0
Jan Mooiman <Jan.Mooiman@deltares.nl>
2021-11-08 15:06:53 +0000

    issue UNST-4586 Empty funtional design document removed

--- src/engines_gpl/dimr/doc/DIMR-FunctionalDesign/buildDoc.bat
+++
@@ -1,6 +0,0 @@
-pdflatex "DIMR-FunctionalDesign.tex"
-bibtex "DIMR-FunctionalDesign"
-pdflatex "DIMR-FunctionalDesign.tex"
-pdflatex "DIMR-FunctionalDesign.tex" > DIMR_FD_Log.txt
-xcopy DIMR_Log.txt "..\BuildLogs" /Y
-xcopy *.pdf .. /Y
