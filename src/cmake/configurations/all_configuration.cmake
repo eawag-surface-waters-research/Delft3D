9e3efd97d963599d3d789b08114d679abcd65f84
Luca Carniato <Luca.Carniato@deltares.nl>
2021-11-17 09:38:23 +0000

    KISS-29: add tools_configuration to all_configuration.cmake. This modification adds mormerge project

--- src/cmake/configurations/all_configuration.cmake
+++ src/cmake/configurations/all_configuration.cmake
@@ -14,6 +14,6 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dimr_configuration.cmake)
 
 # Not officially supported yet: include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/tests_configuration.cmake)
 
-# Not officially supported yet: include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/include/tools_configuration.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/include/tools_configuration.cmake)
 
 project(all)
