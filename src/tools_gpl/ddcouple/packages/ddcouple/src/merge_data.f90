4b96b07112efcde2a58a86d8ccb5d5b3ac06ce63
Maarten Klapwijk <maarten.klapwijk@deltares.nl>
2022-01-05 13:18:36 +0000

    UNST-5669 Add gpl license headers to fortran files
    Former-commit-id: af5d7f3ea126d8f7a9bf3687035863e1e2c01990

--- src/tools_gpl/ddcouple/packages/ddcouple/src/merge_data.f90
+++ src/tools_gpl/ddcouple/packages/ddcouple/src/merge_data.f90
@@ -1,8 +1,29 @@
+!!  Copyright (C)  Stichting Deltares, 2021-2022.
+!!
+!!  This program is free software: you can redistribute it and/or modify
+!!  it under the terms of the GNU General Public License version 3,
+!!  as published by the Free Software Foundation.
+!!
+!!  This program is distributed in the hope that it will be useful,
+!!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+!!  GNU General Public License for more details.
+!!
+!!  You should have received a copy of the GNU General Public License
+!!  along with this program. If not, see <http://www.gnu.org/licenses/>.
+!!
+!!  contact: delft3d.support@deltares.nl
+!!  Stichting Deltares
+!!  P.O. Box 177
+!!  2600 MH Delft, The Netherlands
+!!
+!!  All indications and logos of, and references to registered trademarks
+!!  of Stichting Deltares remain the property of Stichting Deltares. All
+!!  rights reserved.
+
       subroutine merge_data(data_1, data_2)
 
       ! function : merge data_2 into data_1 not fully implemeted but ok for the discharges in ddcouple
-
-      ! (c) Deltares
 
       ! global declarations
 
