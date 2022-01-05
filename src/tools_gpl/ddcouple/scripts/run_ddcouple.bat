6377f5606870306beb66ac25a7290286d7c82ca2
Maarten Klapwijk <maarten.klapwijk@deltares.nl>
2022-01-05 13:54:16 +0000

    UNST-5526 Add ddcouple run script. First version
    Former-commit-id: f3b727faa8ddacc31fdaaef4ffac35c4524c217e

---
+++ src/tools_gpl/ddcouple/scripts/run_ddcouple.bat
@@ -0,0 +1,69 @@
+@ echo off
+title run_ddcouple
+    rem
+    rem This script runs Ddcouple on Windows
+    rem Adapt and use it for your own purpose
+    rem
+setlocal enabledelayedexpansion
+
+    rem
+    rem Set the hyd/ddb file
+    rem
+set argfile= 
+if [%1] EQU [] (
+    goto usage
+) else (
+    if [%1] EQU [--help] (
+        goto usage
+    ) else (
+        set argfile=%1
+    )
+)
+echo Configfile:%argfile%
+if not exist %argfile% (
+    if not exist %argfile%.inp (
+        echo ERROR: input hyd/ddb "%argfile%" does not exist
+        goto usage
+    )
+)
+
+set workdir=%CD%
+rem set argfile=%workdir%\%argfile%
+echo Working directory: %workdir%
+    rem
+    rem Set the directories containing the binaries
+    rem
+set D3D_HOME=%~dp0..\..\..
+
+rem Remove "\dwaq\scripts\..\..\.." from D3D_HOME
+set D3DT=%D3D_HOME:~0,-22%
+rem last directory will be the architecture directory
+for %%f in ("%D3DT%") do set ARCH=%%~nxf
+
+set waqdir=%D3D_HOME%\%ARCH%\dwaq\bin
+
+set sharedir=%D3D_HOME%\%ARCH%\share\bin
+set PATH=%waqdir%;%sharedir%
+
+    rem
+    rem No adaptions needed below
+    rem
+
+    rem Run
+set PATH=%waqdir%;%sharedir%;%~dp0
+
+echo executing in this window: "%waqdir%\ddcouple.exe" "%argfile%"
+"%waqdir%\ddcouple.exe" "%argfile%"
+
+
+
+goto end
+
+:usage
+echo Usage:
+echo run_ddcouple.bat [--help] input.ddb
+echo     --help             : (Optional) show this usage
+echo     input.ddb          : (Mandatory) Ddcouple input file.
+:end
+    rem To prevent the DOS box from disappearing immediately: remove the rem on the following line
+rem pause
