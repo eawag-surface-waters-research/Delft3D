a902c88df6b983f50306209745037f2612a3db86
Jan Noort <Jan.Noort@deltares.nl>
2022-02-10 11:19:44 +0000

    UNST-5541: Bug fix in output through gate. In case of a fully opened gate, the discharge through gate was set to 0. Former-commit-id: f392d09699d3701ddd8f72cb34e444a8722721c2

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/unstruc_structures.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_data/unstruc_structures.f90
@@ -688,7 +688,7 @@ double precision function get_discharge_through_gate_opening(genstr, L0, s1m1, s
    double precision  :: u1L, dsL, gatefraction
    
    dsL = s1m2 - s1m1 
-   gatefraction = genstr%gateclosedfractiononlink(L0)
+   gatefraction = 1d0-genstr%gateclosedfractiononlink(L0)
    
    if (gatefraction > gatefrac_eps) then
       u1L = genstr%ru(3,L0) - genstr%fu(3,L0)*dsL
