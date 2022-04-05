a20f9b936e8c9061186207ae56b736337b82a510
Maarten Klapwijk <maarten.klapwijk@deltares.nl>
2022-04-05 09:40:01 +0000

    UNST-5927 Update informative message about .d09 file to be more informative
    Former-commit-id: 2c78e70c8b78a0db39c3da52d73da624615a59f2

--- src/engines_gpl/waq/packages/waq_process/src/bloom/blinpu.f
+++ src/engines_gpl/waq/packages/waq_process/src/bloom/blinpu.f
@@ -72,8 +72,8 @@
       write(outdbg,'(1x,a/)') trim(idstr)
       write(outdbg, '(a/)') ' BLOOM debug file'
 
-      write(outdbg, '(a)')  ' From now on, BLOOM will not read the bloominp.D09 file any more. Some'
-      write(outdbg, '(a)')  ' of the options that were available trough this file are now accessible'
+      write(outdbg, '(a)')  ' Since August 2019, revision 64400, BLOOM will not read the bloominp.d09 file any more.'
+      write(outdbg, '(a)')  ' Some of the options that were available trough this file are now accessible'
       write(outdbg, '(a/)') ' through parameters settings. Consult the manual for details.'
 
 !  Determine nuspec and nuecog
