c4f63c29ea5f0f48af0483a7ddcf1ba27cec277d
Maarten Klapwijk <maarten.klapwijk@deltares.nl>
2022-06-07 16:07:51 +0000

    UNST-6157 Combine 2 dhaggr routines into one Move m_aggreration_types.F90 from agrhyd to waq_utils_f
    Former-commit-id: ffa311e1f627c42dce4694e099763f6cc8bbac94

--- src/engines_gpl/waq/packages/waq_utils_f/src/waq_utils/dhaggr.f
+++ src/engines_gpl/waq/packages/waq_utils_f/src/waq_utils/dhaggr.f
@@ -28,8 +28,6 @@
 !
 !     Deltares
 !
-!     Created             : June 1998 by Jan van Beek
-!
 !     Function            : Aggregates value to coarser grid
 !
 !     Subroutines called  : GETMLU, Get unit number report file
@@ -44,13 +42,17 @@
 !     NOSEG2  INTEGER  1          INPUT   Number of segments on coarser grid
 !     IPGRID  INTEGER  NOSEG1     INPUT   Grid pointers to coarser grid
 !     IAGTYP  INTEGER  1          INPUT   Aggregation type
-!                                         1 = Accumulation
-!                                         2 = Average
-!                                         3 = Average weighted with WEIGHT
+!                                         IAGTYP_ACCUM = Accumulation
+!                                         IAGTYP_AVG   = Average
+!                                         IAGTYP_WAVG  = Average weighted with WEIGHT
+!                                         IAGTYP_MIN   = Using minimum value
+!                                         IAGTYP_ACSGN = Using signed accumulation (for combining flows in opposite directions)
 !     ARRINP  REAL     NOSEG1     INPUT   Array to be aggregated
 !     WEIGHT  REAL     NOSEG1     INPUT   Weigth in averaging
 !     ARRHLP  REAL     NOSEG2     LOCAL   Local help array
 !     ARROUT  REAL     NOSEG2     OUTPUT  Aggregated array
+      
+      use m_aggregation_types
 !
 !     Declaration of arguments
 !
@@ -68,14 +70,42 @@
 !     LUREP   INTEGER  1          LOCAL   Unit number report file
 !
       INTEGER        ISEG1 , ISEG2 , LUREP
+      real                     :: vmin          ! minimum in weight variable
+      logical                  :: lfound        ! command line option found
+      integer                  :: idummy        !
+      character                :: cdummy        !
+      integer                  :: ierr2         !
+      integer                  :: lunrep        ! report file
+      logical                  :: lfirst = .true.
+      real, parameter          :: rmiss = -999.
+
+      save           lfirst, vmin
+
+
+      if ( lfirst ) then
+         lfirst = .false.
+         call getcom('-vmin',2,lfound,idummy,vmin,cdummy,ierr2)
+         if ( lfound ) then
+            call getmlu(lunrep)
+            if ( ierr2 .ne. 0 ) then
+               write(*,*) 'error commandline option -vmin value could not be interpreted'
+               write(lunrep,*) 'error commandline option -vmin value could not be interpreted'
+               call srstop(1)
+            endif
+            write(*,*) ' commandline option -vmin ',vmin
+            write(lunrep,*) ' commandline option -vmin ',vmin
+         else
+            vmin = 0.0
+         endif
+      endif
 !
 !     Zero accumulation arrays
 !
-      IF ( IAGTYP .EQ. 1 ) THEN
+      IF ( IAGTYP .EQ. IAGTYP_ACCUM  .OR. IAGTYP .EQ. IAGTYP_ACSGN ) THEN
          DO ISEG2 = 1 , NOSEG2
             ARROUT(ISYSO,ISEG2) = 0.0
          ENDDO
-      ELSEIF ( IAGTYP .EQ. 2 .OR. IAGTYP .EQ. 3 ) THEN
+      ELSEIF ( IAGTYP .EQ. IAGTYP_AVG    .OR. IAGTYP .EQ. IAGTYP_WAVG ) THEN
          DO ISEG2 = 1 , NOSEG2
             ARROUT(ISYSO,ISEG2) = 0.0
             ARRHLP(ISYSH,ISEG2) = 0.0
@@ -84,15 +114,15 @@
 !
 !     Accumulate
 !
-      IF ( IAGTYP .EQ. 1 ) THEN
+      IF ( IAGTYP .EQ. IAGTYP_ACCUM ) THEN
          DO ISEG1 = 1 , NOSEG1
-            ISEG2 = IPGRID(ISEG1)
+            ISEG2 = ABS(IPGRID(ISEG1))
             IF ( ISEG2 .GT. 0 ) THEN
                ARROUT(ISYSO,ISEG2) = ARROUT(ISYSO,ISEG2) +
      +                               ARRINP(ISYSI,ISEG1)
             ENDIF
          ENDDO
-      ELSEIF ( IAGTYP .EQ. 2 ) THEN
+      ELSEIF ( IAGTYP .EQ. IAGTYP_AVG ) THEN
          DO ISEG1 = 1 , NOSEG1
             ISEG2 = IPGRID(ISEG1)
             IF ( ISEG2 .GT. 0 ) THEN
@@ -101,7 +131,7 @@
                ARRHLP(ISYSH,ISEG2) = ARRHLP(ISYSH,ISEG2) + 1.0
             ENDIF
          ENDDO
-      ELSEIF ( IAGTYP .EQ. 3 ) THEN
+      ELSEIF ( IAGTYP .EQ. IAGTYP_WAVG ) THEN
          DO ISEG1 = 1 , NOSEG1
             ISEG2 = IPGRID(ISEG1)
             IF ( ISEG2 .GT. 0 ) THEN
@@ -112,6 +142,33 @@
      +                               WEIGHT(ISYSW,ISEG1)
             ENDIF
          ENDDO
+      ELSEIF ( IAGTYP .EQ. IAGTYP_MIN ) THEN
+         DO ISEG1 = 1 , NOSEG1
+            ISEG2 = IPGRID(ISEG1)
+            IF ( ISEG2 .GT. 0 ) THEN
+               IF ( WEIGHT(ISYSW,ISEG1) .GT. VMIN ) THEN
+                  IF ( ARRINP(ISYSI,ISEG1) .NE. RMISS ) THEN
+                     IF ( ARROUT(ISYSO,ISEG2) .NE. RMISS ) THEN
+                        ARROUT(ISYSO,ISEG2) = MIN(ARROUT(ISYSO,ISEG2),ARRINP(ISYSI,ISEG1))
+                     ELSE
+                        ARROUT(ISYSO,ISEG2) = ARRINP(ISYSI,ISEG1)
+                     ENDIF
+                  ENDIF
+               ENDIF
+            ENDIF
+         ENDDO
+      ELSEIF ( IAGTYP .EQ. IAGTYP_ACSGN ) THEN
+         ! modified version of IAGTYP == IAGTYP_ACCUM : deduct values when pointer is negative
+         DO ISEG1 = 1 , NOSEG1
+            ISEG2 = ABS(IPGRID(ISEG1))
+            IF ( IPGRID(ISEG1) .GT. 0 ) THEN
+               ARROUT(ISYSO,ISEG2) = ARROUT(ISYSO,ISEG2) +
+     +                               ARRINP(ISYSI,ISEG1)
+            ELSEIF ( IPGRID(ISEG1) .LT. 0 ) THEN
+               ARROUT(ISYSO,ISEG2) = ARROUT(ISYSO,ISEG2) -
+     +                               ARRINP(ISYSI,ISEG1)
+            ENDIF
+         ENDDO
       ELSE
          CALL GETMLU(LUREP)
          WRITE(LUREP,2000) IAGTYP
@@ -120,7 +177,7 @@
 !
 !     Average
 !
-      IF ( IAGTYP .EQ. 2 .OR. IAGTYP .EQ. 3 ) THEN
+      IF ( IAGTYP .EQ. IAGTYP_AVG .OR. IAGTYP .EQ. IAGTYP_WAVG ) THEN
          DO ISEG2 = 1 , NOSEG2
             IF ( ABS(ARRHLP(ISYSH,ISEG2)) .GT. 1.E-20 ) THEN
                ARROUT(ISYSO,ISEG2) = ARROUT(ISYSO,ISEG2) /
