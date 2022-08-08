046ba5b451defa12e43cd3a20f72fae3b128ba62
Michelle Jeuken <Michelle.Jeuken@deltares.nl>
2022-08-08 13:51:15 +0000

    UNST-6303 string_to_char_array in iso_c_utils exceeds array bounds with stings of 1024 (or more) characters when adding a C_NULL_CHAR
    - By maximizing the location of C_NULL_CHAR to MAXSTRINGLEN this is error is avoided.
    
    I ran into this issue with a model that didn't had projection defined. The string was apparently not initialised and did not contain an end of string character, even after trimming.
    
    Former-commit-id: 8e6f9fa08bbe073a7286486b2a8e78f434f48a3d

--- src/utils_lgpl/deltares_common/packages/deltares_common/src/iso_c_utils.F90
+++ src/utils_lgpl/deltares_common/packages/deltares_common/src/iso_c_utils.F90
@@ -65,7 +65,7 @@ contains
     do i = 1, len(string)
        char_array(i) = string(i:i)
     enddo
-    char_array(len(string)+1) = C_NULL_CHAR
+    char_array(min(MAXSTRINGLEN, len(string)+1)) = C_NULL_CHAR
   end function string_to_char_array
 
 end module iso_c_utils
