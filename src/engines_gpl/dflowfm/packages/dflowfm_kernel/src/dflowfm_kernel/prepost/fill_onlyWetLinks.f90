d25c84fbadc561449cf284b730a444d468937ed4
Jing Zhao <Jing.Zhao@deltares.nl>
2022-08-11 09:38:02 +0000

    UNST-6064: add the newly created subroutine fill_onlyWetLinks.f90. Former-commit-id: eb6c0d5e24f520d6964e8821e3dae9f9955a0e1a

---
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/fill_onlyWetLinks.f90
@@ -0,0 +1,53 @@
+!----- AGPL --------------------------------------------------------------------
+!                                                                               
+!  Copyright (C)  Stichting Deltares, 2017-2022.                                
+!                                                                               
+!  This file is part of Delft3D (D-Flow Flexible Mesh component).               
+!                                                                               
+!  Delft3D is free software: you can redistribute it and/or modify              
+!  it under the terms of the GNU Affero General Public License as               
+!  published by the Free Software Foundation version 3.                         
+!                                                                               
+!  Delft3D  is distributed in the hope that it will be useful,                  
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of               
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                
+!  GNU Affero General Public License for more details.                          
+!                                                                               
+!  You should have received a copy of the GNU Affero General Public License     
+!  along with Delft3D.  If not, see <http://www.gnu.org/licenses/>.             
+!                                                                               
+!  contact: delft3d.support@deltares.nl                                         
+!  Stichting Deltares                                                           
+!  P.O. Box 177                                                                 
+!  2600 MH Delft, The Netherlands                                               
+!                                                                               
+!  All indications and logos of, and references to, "Delft3D",                  
+!  "D-Flow Flexible Mesh" and "Deltares" are registered trademarks of Stichting 
+!  Deltares, and remain the property of Stichting Deltares. All rights reserved.
+!                                                                               
+!-------------------------------------------------------------------------------
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+!> Fills array onlyWetLinks, which contains indices of flowlinks that are wet.
+subroutine fill_onlyWetLinks()
+   use m_alloc
+   use m_flowgeom, only: lnx, wetLinkCount, onlyWetLinks
+   use m_flow, only: hu
+   use m_flowparameters, only: epshu
+   implicit none
+
+   integer :: L
+
+   call realloc(onlyWetLinks, lnx, keepExisting = .false., fill=0)
+   wetLinkCount = 0
+   do L = 1, lnx
+      if (hu(L) > epshu) then
+        wetLinkCount = wetLinkCount +1
+        onlyWetLinks(wetLinkCount) = L
+      endif
+   enddo
+   call realloc(onlyWetLinks, wetLinkCount, keepExisting = .true.)
+end subroutine fill_onlyWetLinks
