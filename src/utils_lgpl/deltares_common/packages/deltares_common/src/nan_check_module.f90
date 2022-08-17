82d5821aec933b01bf059009aaed30d3f6eab88e
Bert Jagers <bert.jagers@deltares.nl>
2022-08-17 18:38:17 +0000

    DELFT3D-37698: * Optional lower bound arguments added to nan_check routines to initially allow the continued use of the old interface (without lower bounds) while enabling the transition to calls that include the lower bounds. Former-commit-id: 44bc5b2eab6735b6d964acf908263d2bd57fbfda

--- src/utils_lgpl/deltares_common/packages/deltares_common/src/nan_check_module.f90
+++ src/utils_lgpl/deltares_common/packages/deltares_common/src/nan_check_module.f90
@@ -47,7 +47,7 @@ end interface
 
 contains
 
-logical function nan_check_1D_sp(field, fieldnam, lundia) result (retval)
+logical function nan_check_1D_sp(field, fieldnam, lundia, ilb) result (retval)
 !!--description-----------------------------------------------------------------
 !
 !    Function: - Checks a 1D single precision array on NaN's.
@@ -56,22 +56,29 @@ logical function nan_check_1D_sp(field, fieldnam, lundia) result (retval)
     !
     ! Arguments
     !
-    real(kind=4), dimension(:), intent(in) :: field
-    character*(*), intent(in)              :: fieldnam
-    integer, intent(in)                    :: lundia
+    real(kind=4), dimension(:)               , intent(in)    :: field
+    character*(*)                            , intent(in)    :: fieldnam
+    integer                                  , intent(in)    :: lundia
+    integer, optional                        , intent(in)    :: ilb
     !
     ! Local variables
     !
     integer                                :: i
+    integer                                :: i0
     character*(256)                        :: message
 !
 !! executable statements -------------------------------------------------------
 !
     retval = .true.
+    if (present(ilb)) then
+       i0 = ilb - 1
+    else
+       i0 = 0
+    endif
     do i = lbound(field,1), ubound(field,1)
        if (isnan(field(i))) then
           write(message,*) 'NaN found in ', fieldnam, &
-               ' at (i) = (', i, ')'
+               ' at (i) = (', i0+i, ')'
           write(lundia,*) '*** ERROR', trim(message)
           retval = .false.
           return
@@ -79,7 +86,7 @@ logical function nan_check_1D_sp(field, fieldnam, lundia) result (retval)
     enddo
 end function nan_check_1D_sp
 
-logical function nan_check_2D_sp(field, fieldnam, lundia) result (retval)
+logical function nan_check_2D_sp(field, fieldnam, lundia, nlb, mlb) result (retval)
 !!--description-----------------------------------------------------------------
 !
 !    Function: - Checks a 2D single precision array on NaN's.
@@ -88,23 +95,36 @@ logical function nan_check_2D_sp(field, fieldnam, lundia) result (retval)
     !
     ! Arguments
     !
-    real(kind=4), dimension(:,:), intent(in) :: field
-    character*(*), intent(in)                :: fieldnam
-    integer, intent(in)                      :: lundia
+    real(kind=4), dimension(:,:)             , intent(in)    :: field
+    character*(*)                            , intent(in)    :: fieldnam
+    integer                                  , intent(in)    :: lundia
+    integer, optional                        , intent(in)    :: nlb
+    integer, optional                        , intent(in)    :: mlb
     !
     ! Local variables
     !
     integer                                  :: m, n
+    integer                                  :: m0, n0
     character*(256)                          :: message
 !
 !! executable statements -------------------------------------------------------
 !
     retval = .true.
+    if (present(nlb)) then
+       n0 = nlb-1
+    else
+       n0 = 0
+    endif
+    if (present(mlb)) then
+       m0 = mlb-1
+    else
+       m0 = 0
+    endif
     do m = lbound(field,2), ubound(field,2)
        do n = lbound(field,1), ubound(field,1)
           if (isnan(field(n,m))) then
              write(message,*) 'NaN found in ', fieldnam, &
-              ' at (n,m) = (', n, ',' , m, ')'
+              ' at (n,m) = (', n0+n, ',' , m0+m, ')'
              write(lundia,*) '*** ERROR', trim(message)
              retval = .false.
              return
@@ -113,7 +133,7 @@ logical function nan_check_2D_sp(field, fieldnam, lundia) result (retval)
     enddo
 end function nan_check_2D_sp
 
-logical function nan_check_3D_sp(field, fieldnam, lundia) result (retval)
+logical function nan_check_3D_sp(field, fieldnam, lundia, nlb, mlb, klb) result (retval)
 !!--description-----------------------------------------------------------------
 !
 !    Function: - Checks a 3D single precision array on NaN's.
@@ -122,24 +142,43 @@ logical function nan_check_3D_sp(field, fieldnam, lundia) result (retval)
     !
     ! Arguments
     !
-    real(kind=4), dimension(:,:,:), intent(in) :: field
-    character*(*), intent(in)                  :: fieldnam
-    integer, intent(in)                        :: lundia
+    real(kind=4), dimension(:,:,:)           , intent(in)    :: field
+    character*(*)                            , intent(in)    :: fieldnam
+    integer                                  , intent(in)    :: lundia
+    integer, optional                        , intent(in)    :: nlb
+    integer, optional                        , intent(in)    :: mlb
+    integer, optional                        , intent(in)    :: klb
     !
     ! Local variables
     !
     integer                                    :: k, m, n
+    integer                                    :: k0, m0, n0
     character*(256)                            :: message
 !
 !! executable statements -------------------------------------------------------
 !
     retval = .true.
+    if (present(nlb)) then
+       n0 = nlb-1
+    else
+       n0 = 0
+    endif
+    if (present(mlb)) then
+       m0 = mlb-1
+    else
+       m0 = 0
+    endif
+    if (present(klb)) then
+       k0 = klb-1
+    else
+       k0 = 0
+    endif
     do k = lbound(field,3), ubound(field,3)
        do m = lbound(field,2), ubound(field,2)
           do n = lbound(field,1), ubound(field,1)
             if (isnan(field(n,m,k))) then
               write(message,*) 'NaN found in ', fieldnam, &
-                  ' at (n,m,k) = (', n, ',', m, ',', k, ')'
+                  ' at (n,m,k) = (', n0+n, ',', m0+m, ',', k0+k, ')'
               write(lundia,*) '*** ERROR', trim(message)
               retval = .false.
               return
@@ -149,7 +188,7 @@ logical function nan_check_3D_sp(field, fieldnam, lundia) result (retval)
      enddo
     end function nan_check_3D_sp
 
-logical function nan_check_4D_sp(field, fieldnam, lundia) result (retval)
+logical function nan_check_4D_sp(field, fieldnam, lundia, nlb, mlb, klb, llb) result (retval)
 !!--description-----------------------------------------------------------------
 !
 !    Function: - Checks a 4D single precision array on NaN's.
@@ -158,25 +197,50 @@ logical function nan_check_4D_sp(field, fieldnam, lundia) result (retval)
     !
     ! Arguments
     !
-    real(kind=4), dimension(:,:,:,:), intent(in) :: field
-    character*(*), intent(in)                    :: fieldnam
-    integer, intent(in)                          :: lundia
+    real(kind=4), dimension(:,:,:,:)         , intent(in)    :: field
+    character*(*)                            , intent(in)    :: fieldnam
+    integer                                  , intent(in)    :: lundia
+    integer, optional                        , intent(in)    :: nlb
+    integer, optional                        , intent(in)    :: mlb
+    integer, optional                        , intent(in)    :: klb
+    integer, optional                        , intent(in)    :: llb
     !
     ! Local variables
     !
     integer                                      :: k, l, m, n
+    integer                                      :: k0, l0, m0, n0
     character*(256)                              :: message
 !
 !! executable statements -------------------------------------------------------
 !
     retval = .true.
+    if (present(nlb)) then
+       n0 = nlb-1
+    else
+       n0 = 0
+    endif
+    if (present(mlb)) then
+       m0 = mlb-1
+    else
+       m0 = 0
+    endif
+    if (present(klb)) then
+       k0 = klb-1
+    else
+       k0 = 0
+    endif
+    if (present(llb)) then
+       l0 = llb-1
+    else
+       l0 = 0
+    endif
     do l = lbound(field,4), ubound(field,4)
        do k = lbound(field,3), ubound(field,3)
           do m = lbound(field,2), ubound(field,2)
              do n = lbound(field,1), ubound(field,1)
                 if (isnan(field(n,m,k,l))) then
                    write(message,*) 'NaN found in ', fieldnam, &
-                      ' at (n,m,k,l) = (', n, ',', m, ',', k, ',', l, ')'
+                      ' at (n,m,k,l) = (', n0+n, ',', m0+m, ',', k0+k, ',', l0+l, ')'
                    write(lundia,*) '*** ERROR', trim(message)
                    retval = .false.
                    return
@@ -187,7 +251,7 @@ logical function nan_check_4D_sp(field, fieldnam, lundia) result (retval)
     enddo
     end function nan_check_4D_sp
 
-logical function nan_check_1D_dp(field, fieldnam, lundia) result (retval)
+logical function nan_check_1D_dp(field, fieldnam, lundia, ilb) result (retval)
 !!--description-----------------------------------------------------------------
 !
 !    Function: - Checks a 1D double precision array on NaN's.
@@ -196,22 +260,29 @@ logical function nan_check_1D_dp(field, fieldnam, lundia) result (retval)
     !
     ! Arguments
     !
-    real(kind=8), dimension(:), intent(in) :: field
-    character*(*), intent(in)              :: fieldnam
-    integer, intent(in)                    :: lundia
+    real(kind=8), dimension(:)               , intent(in)    :: field
+    character*(*)                            , intent(in)    :: fieldnam
+    integer                                  , intent(in)    :: lundia
+    integer, optional                        , intent(in)    :: ilb
     !
     ! Local variables
     !
     integer                                :: i
+    integer                                :: i0
     character*(256)                        :: message
 !
 !! executable statements -------------------------------------------------------
 !
     retval = .true.
+    if (present(ilb)) then
+       i0 = ilb-1
+    else
+       i0 = 0
+    endif
     do i = lbound(field,1), ubound(field,1)
        if (isnan(field(i))) then
           write(message,*) 'NaN found in ', fieldnam, &
-               ' at (i) = (', i, ')'
+               ' at (i) = (', i0+i, ')'
           write(lundia,*) '*** ERROR', trim(message)
           retval = .false.
           return
@@ -219,7 +290,7 @@ logical function nan_check_1D_dp(field, fieldnam, lundia) result (retval)
     enddo
 end function nan_check_1D_dp
 
-logical function nan_check_2D_dp(field, fieldnam, lundia) result (retval)
+logical function nan_check_2D_dp(field, fieldnam, lundia, nlb, mlb) result (retval)
 !!--description-----------------------------------------------------------------
 !
 !    Function: - Checks a 2D double precision array on NaN's.
@@ -228,23 +299,36 @@ logical function nan_check_2D_dp(field, fieldnam, lundia) result (retval)
     !
     ! Arguments
     !
-    real(kind=8), dimension(:,:), intent(in) :: field
-    character*(*), intent(in)                :: fieldnam
-    integer, intent(in)                      :: lundia
+    real(kind=8), dimension(:,:)             , intent(in)    :: field
+    character*(*)                            , intent(in)    :: fieldnam
+    integer                                  , intent(in)    :: lundia
+    integer, optional                        , intent(in)    :: nlb
+    integer, optional                        , intent(in)    :: mlb
     !
     ! Local variables
     !
     integer                                  :: m, n
+    integer                                  :: m0, n0
     character*(256)                          :: message
 !
 !! executable statements -------------------------------------------------------
 !
     retval = .true.
+    if (present(nlb)) then
+       n0 = nlb-1
+    else
+       n0 = 0
+    endif
+    if (present(mlb)) then
+       m0 = mlb-1
+    else
+       m0 = 0
+    endif
     do m = lbound(field,2), ubound(field,2)
        do n = lbound(field,1), ubound(field,1)
           if (isnan(field(n,m))) then
              write(message,*) 'NaN found in ', fieldnam, &
-              ' at (n,m) = (', n, ',' , m, ')'
+              ' at (n,m) = (', n0+n, ',' , m0+m, ')'
              write(lundia,*) '*** ERROR', trim(message)
              retval = .false.
              return
@@ -253,7 +337,7 @@ logical function nan_check_2D_dp(field, fieldnam, lundia) result (retval)
     enddo
 end function nan_check_2D_dp
 
-logical function nan_check_3D_dp(field, fieldnam, lundia) result (retval)
+logical function nan_check_3D_dp(field, fieldnam, lundia, nlb, mlb, klb) result (retval)
 !!--description-----------------------------------------------------------------
 !
 !    Function: - Checks a 3D double precision array on NaN's.
@@ -262,24 +346,43 @@ logical function nan_check_3D_dp(field, fieldnam, lundia) result (retval)
     !
     ! Arguments
     !
-    real(kind=8), dimension(:,:,:), intent(in) :: field
-    character*(*), intent(in)                  :: fieldnam
-    integer, intent(in)                        :: lundia
+    real(kind=8), dimension(:,:,:)           , intent(in)    :: field
+    character*(*)                            , intent(in)    :: fieldnam
+    integer                                  , intent(in)    :: lundia
+    integer, optional                        , intent(in)    :: nlb
+    integer, optional                        , intent(in)    :: mlb
+    integer, optional                        , intent(in)    :: klb
     !
     ! Local variables
     !
     integer                                    :: k, m, n
+    integer                                    :: k0, m0, n0
     character*(256)                            :: message
 !
 !! executable statements -------------------------------------------------------
 !
     retval = .true.
+    if (present(nlb)) then
+       n0 = nlb-1
+    else
+       n0 = 0
+    endif
+    if (present(mlb)) then
+       m0 = mlb-1
+    else
+       m0 = 0
+    endif
+    if (present(klb)) then
+       k0 = klb-1
+    else
+       k0 = 0
+    endif
     do k = lbound(field,3), ubound(field,3)
        do m = lbound(field,2), ubound(field,2)
           do n = lbound(field,1), ubound(field,1)
             if (isnan(field(n,m,k))) then
               write(message,*) 'NaN found in ', fieldnam, &
-                  ' at (n,m,k) = (', n, ',', m, ',', k, ')'
+                  ' at (n,m,k) = (', n0+n, ',', m0+m, ',', k0+k, ')'
               write(lundia,*) '*** ERROR', trim(message)
               retval = .false.
               return
@@ -289,7 +392,7 @@ logical function nan_check_3D_dp(field, fieldnam, lundia) result (retval)
      enddo
     end function nan_check_3D_dp
 
-logical function nan_check_4D_dp(field, fieldnam, lundia) result (retval)
+logical function nan_check_4D_dp(field, fieldnam, lundia, nlb, mlb, klb, llb) result (retval)
 !!--description-----------------------------------------------------------------
 !
 !    Function: - Checks a 4D double precision array on NaN's.
@@ -298,25 +401,50 @@ logical function nan_check_4D_dp(field, fieldnam, lundia) result (retval)
     !
     ! Arguments
     !
-    real(kind=8), dimension(:,:,:,:), intent(in) :: field
-    character*(*), intent(in)                    :: fieldnam
-    integer, intent(in)                          :: lundia
+    real(kind=8), dimension(:,:,:,:)         , intent(in)    :: field
+    character*(*)                            , intent(in)    :: fieldnam
+    integer                                  , intent(in)    :: lundia
+    integer, optional                        , intent(in)    :: nlb
+    integer, optional                        , intent(in)    :: mlb
+    integer, optional                        , intent(in)    :: klb
+    integer, optional                        , intent(in)    :: llb
     !
     ! Local variables
     !
     integer                                      :: k, l, m, n
+    integer                                      :: k0, l0, m0, n0
     character*(256)                              :: message
 !
 !! executable statements -------------------------------------------------------
 !
     retval = .true.
+    if (present(nlb)) then
+       n0 = nlb-1
+    else
+       n0 = 0
+    endif
+    if (present(mlb)) then
+       m0 = mlb-1
+    else
+       m0 = 0
+    endif
+    if (present(klb)) then
+       k0 = klb-1
+    else
+       k0 = 0
+    endif
+    if (present(llb)) then
+       l0 = llb-1
+    else
+       l0 = 0
+    endif
     do l = lbound(field,4), ubound(field,4)
        do k = lbound(field,3), ubound(field,3)
           do m = lbound(field,2), ubound(field,2)
              do n = lbound(field,1), ubound(field,1)
                 if (isnan(field(n,m,k,l))) then
                    write(message,*) 'NaN found in ', fieldnam, &
-                      ' at (n,m,k,l) = (', n, ',', m, ',', k, ',', l, ')'
+                      ' at (n,m,k,l) = (', n0+n, ',', m0+m, ',', k0+k, ',', l0+l, ')'
                    write(lundia,*) '*** ERROR', trim(message)
                    retval = .false.
                    return
