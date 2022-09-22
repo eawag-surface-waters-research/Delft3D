ac97601d636f7f643a685d7343f5b1cd3d8ab78e
Frank Platzek <Frank.Platzek@deltares.nl>
2022-09-22 06:51:41 +0000

    Fix paths to DIMR scripts in D-Flow FM examples run scripts (Windows) Add comment about different paths (Release y/n) for different ways of building
    Former-commit-id: e3b1c1cce7ae1b3dd75d3d4005287cb643319fce

--- examples/12_dflowfm/test_data/e100_f02_c02-FriesianInlet_schematic_FM/run.bat
+++ examples/12_dflowfm/test_data/e100_f02_c02-FriesianInlet_schematic_FM/run.bat
@@ -7,9 +7,14 @@ rem build.bat all
 rem See README.md there for more information
 
 set build_configuration=build_all
-set script_path=..\..\..\..\%build_configuration%\x64\dimr\scripts
-call %script_path%\run_dimr.bat
 
+rem The following path should match the folder where the dimr scripts have been placed after your compilation step
+rem In case you built using the build.bat (configure AND build step), then the scripts will be placed in the following folder:
+set script_path=..\..\..\..\%build_configuration%\x64\dimr\scripts
+rem In case you only configured using the build.bat and built yourself using Visual Studio, then the scripts will be placed in the following folder:
+rem set script_path=..\..\..\..\%build_configuration%\x64\Release\dimr\scripts
+
+call %script_path%\run_dimr.bat
 
     rem To prevent the DOS box from disappearing immediately: remove the rem on the following line
 rem pause
