bb94509202bd55aa5d9e4ce4832cb66a38523192
Jan Noort <Jan.Noort@deltares.nl>
2022-10-11 10:55:38 +0000

    UNST-6428: run_dfm_volume_tool added to subversion Former-commit-id: 8584205ca108cb217db9f87409b03c8a0ce7dee4

---
+++ src/engines_gpl/dflowfm/scripts/team-city/run_dfm_volume_tool.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+ulimit -s unlimited
+
+function print_usage_info {
+    echo "Usage: ${0##*/} [OPTION]... [--] [dfm_volume_toolOPTIONS]..."
+    echo "Run dfm_volume_tool program."
+    echo
+    echo
+    echo "Options for ${0##*/}:"
+    echo "-h, --help"
+    echo "       print this help message and exit"
+    echo
+    echo "--"
+    echo "       All following arguments are passed to dfm_volume_tool."
+    echo "       (optional)"
+    echo "Options for dfm_volume_tool, see dfm_volume_tool --help"
+
+    exit 1
+}
+
+while [[ $# -ge 1 ]]
+do
+key="$1"
+shift
+case $key in
+    -h|--help)
+    print_usage_info
+    ;;
+    --)
+    dfm_volume_tooloptions=$*
+    shift $#
+    break       # exit loop, all remaining options to dflowfm executable
+    ;;
+esac
+done
+
+scriptdirname=`readlink \-f \$0`
+scriptdir=`dirname $scriptdirname`
+export D3D_HOME=$scriptdir/.. 
+export LD_LIBRARY_PATH=$D3D_HOME/lib:$LD_LIBRARY_PATH
+
+##$SCRIPT_DIR/../dflowfm "$@"
+echo $D3D_HOME/bin/dfm_volume_tool --verbose $dfm_volume_tooloptions "$@"
+#echo $dfm_volume_tooloptions
+$D3D_HOME/bin/dfm_volume_tool --verbose $dfm_volume_tooloptions "$@"
+
