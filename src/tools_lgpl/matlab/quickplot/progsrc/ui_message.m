f2d4ad264d5aad456cd0fbbd8b32565c7eed9b5b
Bert Jagers <bert.jagers@deltares.nl>
2022-11-03 12:46:31 +0000

    resolve listboxtop error when cleaning the list. add 'reset' option to recover if similar issue occurs.
    Former-commit-id: 8f8704864b8e9e9f1fe951efc253e649b10e7a19

--- src/tools_lgpl/matlab/quickplot/progsrc/ui_message.m
+++ src/tools_lgpl/matlab/quickplot/progsrc/ui_message.m
@@ -115,6 +115,10 @@ if nargin==1 && isequal(Cmd,'resize')
 end
 
 fig=findobj(allchild(0),'tag','UI_MESSAGE window');
+if isequal(Cmd,'reset')
+    delete(fig)
+    fig = [];
+end
 
 if isempty(UD)
     if ~isempty(fig) && ishandle(fig)
@@ -308,7 +312,7 @@ else
         case 'clear'
             errors={};
             MessageOffset=[];
-            set(findobj(fig,'tag','errorlist'),'string',errors);
+            set(findobj(fig,'tag','errorlist'),'string',errors,'listboxtop',1);
         case 'clipboard'
             i=sort(get(findobj(fig,'tag','errorlist'),'value')); % always copy lines from top to bottom
             clipboard('copy',sprintf('%s\n',errors{i}))
