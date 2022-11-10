09386f4caaf9ea39cbfaa78e4baa2c18434bd677
Herman Kernkamp <Herman.Kernkamp@deltares.nl>
2022-11-10 13:43:24 +0000

    No issue nr, removed superfluous line
    Former-commit-id: 1bd662d566083c49060a1ef69177f28d62a5e4c7

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/getustbcfuhi.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/getustbcfuhi.f90
@@ -177,7 +177,7 @@
                sphi = -csw*snu(LL)+snw*csu(LL)
                abscos = abs(cphi*uu + sphi*vv) / umod
                call getsoulsbywci(modind, z00, ustc2, ustw2, fw, cdrag, umod, abscos, taubpuLL, taubxuLL)
-               ustbLL = sqrt(umod*taubpuLL)
+               ! ustbLL = sqrt(umod*taubpuLL)
             else if (modind == 9) then                            ! wave-current interaction van Rijn (2004)
                call getvanrijnwci(LL, umod, u2dh, taubpuLL, z0urouL)
                taubxuLL = rhoL*(ustc2+ustw2)                      ! depth-averaged, see taubot
