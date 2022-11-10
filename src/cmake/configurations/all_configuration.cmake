5eda75c935b014297bd78bf665cbe95128687f17
Jan Mooiman <Jan.Mooiman@deltares.nl>
2022-11-10 09:53:06 +0000

    Detach reference to flow2d3d and d_hydro
    Former-commit-id: c6c271b2e5651ab54d24db5643d51d16f61e1d28

--- src/cmake/configurations/all_configuration.cmake
+++ src/cmake/configurations/all_configuration.cmake
@@ -16,9 +16,9 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dwaves_configuration.cmake)
 
 # include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/flow1d2d_configuration.cmake)
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/flow2d3d_configuration.cmake)
+# include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/flow2d3d_configuration.cmake)
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/d_hydro_configuration.cmake)
+# include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/d_hydro_configuration.cmake)
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dimr_configuration.cmake)
 
--- src/cmake/configurations/d_hydro_configuration.cmake
+++ src/cmake/configurations/d_hydro_configuration.cmake
@@ -3,11 +3,6 @@ project(d_hydro)
 # D_HYDRO specific components
 add_subdirectory(${checkout_src_root}/${d_hydro_module} d_hydro)
 
-if(UNIX)
-  # install
-  add_subdirectory(${checkout_src_root}/${install_dimr_module} install_dimr)
-endif()
-
 # Specify the modules to be included
 if(NOT TARGET deltares_common)
   add_subdirectory(${checkout_src_root}/${deltares_common_module} deltares_common)
@@ -24,7 +19,6 @@ if(UNIX)
   endif()
 endif(UNIX)
 
-# DIMR specific components
 # D-Hydro lib
 if(NOT TARGET d_hydro_lib)
     add_subdirectory(${checkout_src_root}/${d_hydro_lib_module} d_hydro_lib)
@@ -32,5 +26,5 @@ endif()
 
 if(UNIX)
   # install
-  add_subdirectory(${checkout_src_root}/${install_dimr_module} install_d_hydro)
+  add_subdirectory(${checkout_src_root}/${install_d_hydro_module} install_d_hydro)
 endif()
