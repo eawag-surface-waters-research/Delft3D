e69b3d3f2a76ff715ee13b834853f736b07b6d53
Arthur van Dam <Arthur.vanDam@deltares.nl>
2022-11-28 13:42:42 +0000

    UNST-6171: numk/numl check in geominit must return warning, not a blocking error. Former-commit-id: 28913e310b817ba29ba75838e6e6547308548e55

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/flow_geominit.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/flow_geominit.f90
@@ -120,7 +120,7 @@
  COMMON /DRAWTHIS/ ndraw(50)
 
  if (numk <= 2 .or. numl <= 1 ) then
-    call mess(LEVEL_ERROR,'A valid network requires at least 3 computational grid points (net nodes) and at least 2 netlinks for 1D or 3 netlinks for 2D.')
+    call mess(LEVEL_WARN,'A valid network requires at least 3 computational grid points (net nodes) and at least 2 netlinks for 1D or 3 netlinks for 2D.')
     return               ! only do this for sufficient network
  endif
 
