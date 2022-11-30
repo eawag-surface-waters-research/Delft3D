48596dd09d6a11816f37cee179bb0d9efdaefd37
Bert Jagers <bert.jagers@deltares.nl>
2022-11-30 07:53:40 +0000

    add option to label structure locations
    Former-commit-id: f5ee0d3f1dc12bfc9c448a7e1917c8d63c80b82a

--- src/tools_lgpl/matlab/quickplot/progsrc/private/d3d_simfil.m
+++ src/tools_lgpl/matlab/quickplot/progsrc/private/d3d_simfil.m
@@ -556,6 +556,7 @@ switch FI.FileType(9:end)
                             Ans.XY{j} = landboundary('read',relpath(path_str,pli_files{j}));
                         end
                     end
+                    Ans.Val = inifile('hcgetstringi',FI.Structure,IndexChapter,'id','');
                 else
                     Ans = [];
                 end
@@ -1060,7 +1061,7 @@ switch FI.FileType
                             Out(ifld).Geom = 'POLYL';
                             Out(ifld).Coords = 'xy';
                             Out(ifld).DimFlag(ST_) = 3;
-                            Out(ifld).NVal = 0;
+                            Out(ifld).NVal = 4;
                         end
                         if ismember('dambreak',utypes)
                             ifld = ifld+1;
