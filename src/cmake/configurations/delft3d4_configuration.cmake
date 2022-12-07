92c8859775a382509fa901991282da669e75bfeb
Jan Mooiman <Jan.Mooiman@deltares.nl>
2022-12-07 13:02:38 +0000

    issue Delft3D-37750, Delft3D4 configuration added
    Former-commit-id: 4165fd1f52b5005dd78350cc9385786e37faa300

---
+++ src/cmake/configurations/delft3d4_configuration.cmake
@@ -0,0 +1,17 @@
+# project(delft3d4)
+
+# Specify the modules to be included
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dwaq_configuration.cmake)
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dwaves_configuration.cmake)
+
+# include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/rtc_configuration.cmake)
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/flow2d3d_configuration.cmake)
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/d_hydro_configuration.cmake)
+
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dimr_configuration.cmake)
+
+project(delft3d4)
