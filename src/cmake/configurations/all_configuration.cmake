8465cfb208d703bd04d69358133730ec47bc4efc
Jan Mooiman <Jan.Mooiman@deltares.nl>
2022-12-22 12:35:28 +0000

    issue Delft3D-37750, Update CMakeLists.txt files for DATSEL, KUBINT, LINT, NESTHD1 and NESTHD2
    Former-commit-id: 3f8b60a2a99a701a7e8410f64e3d688b4a244fe6

--- src/cmake/configurations/all_configuration.cmake
+++ src/cmake/configurations/all_configuration.cmake
@@ -3,9 +3,9 @@
 # Specify the modules to be included
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dflowfm_configuration.cmake)
-
+ 
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dwaq_configuration.cmake)
-
+ 
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dwaves_configuration.cmake)
 
 # include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/rr_configuration.cmake)
@@ -16,14 +16,14 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dwaves_configuration.cmake)
 
 # include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/flow1d2d_configuration.cmake)
 
-# include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/flow2d3d_configuration.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/flow2d3d_configuration.cmake)
 
-# include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/d_hydro_configuration.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/d_hydro_configuration.cmake)
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dimr_configuration.cmake)
 
 # Not officially supported yet: include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/tests_configuration.cmake)
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/include/tools_configuration.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/tools_gpl_configuration.cmake)
 
 project(all)
--- src/cmake/modules.cmake
+++ src/cmake/modules.cmake
@@ -1,8 +1,8 @@
 # File that defines all the modules that can be included
-include(modules/engines.cmake)
+# include(modules/engines.cmake)
 include(modules/engines_gpl.cmake)
 include(modules/third_parties.cmake)
+include(modules/tools_gpl.cmake)
 include(modules/utils.cmake)
 include(modules/utils_gpl.cmake)
 include(modules/utils_lgpl.cmake) 
-include(modules/tools_gpl.cmake)
