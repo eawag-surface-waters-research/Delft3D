c80ada21adc178f1681cd5709201f70557ac7080
Adri Mourits <Adri.Mourits@deltares.nl>
2022-12-23 08:24:21 +0000

    Delft3D-37750: Delft3D4-CMake commits partially reverted (doesn't build on Linux): all_configuration.cmake: commented out: flow2d3d, d_hydro Replaced "configurations/tools_gpl_configuration.cmake" by "configurations/include/tools_configuration.cmake" Former-commit-id: 46455ccb009cb3ae1c6406b54f52e9415c5ebb97

--- src/cmake/configurations/all_configuration.cmake
+++ src/cmake/configurations/all_configuration.cmake
@@ -16,14 +16,14 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dwaves_configuration.cmake)
 
 # include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/flow1d2d_configuration.cmake)
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/flow2d3d_configuration.cmake)
+# include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/flow2d3d_configuration.cmake)
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/d_hydro_configuration.cmake)
+# include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/d_hydro_configuration.cmake)
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/dimr_configuration.cmake)
 
 # Not officially supported yet: include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/tests_configuration.cmake)
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/tools_gpl_configuration.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/include/tools_configuration.cmake)
 
 project(all)
