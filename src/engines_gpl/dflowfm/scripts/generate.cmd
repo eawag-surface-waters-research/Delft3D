7c08d8587fb52a57a9abc926a8abd08d28f295f1
Floris Buwalda <Floris.Buwalda@deltares.nl>
2023-02-09 14:58:04 +0000

    UNST-6670 change in generate.py and generate.cmd in response to review comments, text file containing the fortran files to be read is now an input argument.
    Former-commit-id: b130fe5d1ee4cae0827b79a4a5bccc8e651f2c09

--- src/engines_gpl/dflowfm/scripts/generate.cmd
+++ src/engines_gpl/dflowfm/scripts/generate.cmd
@@ -1,4 +1,4 @@
 set SAVEDIR=%CD%
 cd ..\packages\dflowfm_lib\include\
-python ..\..\..\scripts\template\generate.py test.f90 --template-dir=..\..\dflowfm_lib\templates\ --verbose
+python ..\..\..\scripts\template\generate.py --fortranfiles-dir=..\..\..\scripts\fortranfiles.txt --template-dir=..\..\dflowfm_lib\templates\ --verbose
 cd %SAVEDIR%
--- src/engines_gpl/dflowfm/scripts/template/generate.py
+++ src/engines_gpl/dflowfm/scripts/template/generate.py
@@ -4,7 +4,7 @@
 Parse a fortran file and generate BMI functions from templates.
 
 Usage:
-  generate-bmi <fortranfile>... [--template-dir=<template-dir>] [--verbose]
+  generate-bmi [--fortranfiles-dir=<fortranfiles-dir>]... [--template-dir=<template-dir>] [--verbose]
 """
 
 import os
@@ -20,7 +20,7 @@ import mako.template
 import mako.lookup
 
 
-def main(templatedir="templates"):
+def main(fortranfiledirs="fortranfiledirs",templatedir="templates"):
     """read the fortran files, parse them and apply them
     to the templates in templatedir"""
 
@@ -58,36 +58,37 @@ def main(templatedir="templates"):
     variables = []
     numv = 0
     filepath = '..\\..\\..\\scripts\\fortranfiles.txt'
-    fortranfiles = open(filepath,'r')
-    for fortranfile in fortranfiles.read().splitlines():
-        logging.info("Scanning file %s...", fortranfile)
-        with open(fortranfile) as f:
-            linenr = 0
-            for line in f.readlines():
-                linenr = linenr + 1
-                match = variable_re.match(line)
-                if match:
-                    # print '   ', line
-                    variable = match.groupdict()
-                    if variable['dimension'].strip():
-                        variable['rank'] = variable['dimension'].count(',') + 1
-                    else:
-                        variable['rank'] = 0
-                    variable['type'] = FORTRANTYPESMAP[variable['fortrantype']]
-    
-                    try:
-                        variable.update(json.loads(variable["json"]))
-                    except ValueError as e:
-                        logging.exception("Variable's JSON string could not be parsed: %s. File: %s, line %d.", variable, fortranfile, linenr)
-    
-                    # shape overwrites rank
-                    if 'shape' in variable:
-                        variable['rank'] = len(variable['shape'])
-    
-                    #print 'variable: ', variable
-                    variables.append(variable)
-        logging.info("Extracted %d variables.", len(variables)-numv)
-        numv = len(variables)
+    for fdir in fortranfiledirs:
+        fortranfiles = open(fdir,'r')
+        for fortranfile in fortranfiles.read().splitlines():
+            logging.info("Scanning file %s...", fortranfile)
+            with open(fortranfile) as f:
+                linenr = 0
+                for line in f.readlines():
+                    linenr = linenr + 1
+                    match = variable_re.match(line)
+                    if match:
+                        # print '   ', line
+                        variable = match.groupdict()
+                        if variable['dimension'].strip():
+                            variable['rank'] = variable['dimension'].count(',') + 1
+                        else:
+                            variable['rank'] = 0
+                        variable['type'] = FORTRANTYPESMAP[variable['fortrantype']]
+        
+                        try:
+                            variable.update(json.loads(variable["json"]))
+                        except ValueError as e:
+                            logging.exception("Variable's JSON string could not be parsed: %s. File: %s, line %d.", variable, fortranfile, linenr)
+        
+                        # shape overwrites rank
+                        if 'shape' in variable:
+                            variable['rank'] = len(variable['shape'])
+        
+                        #print 'variable: ', variable
+                        variables.append(variable)
+            logging.info("Extracted %d variables.", len(variables)-numv)
+            numv = len(variables)
 
     logging.info("Total: extracted %d variables.", len(variables))
 
@@ -133,4 +134,4 @@ if __name__ == '__main__':
     if arguments['--verbose']:
         logging.basicConfig(level=logging.INFO)
 
-    main(arguments['--template-dir'] or 'templates')
+    main(arguments['--fortranfiles-dir'] or 'fortranfiledirs',arguments['--template-dir'] or 'templates')
