19d3486b9e787e762cea1aa254ad513c22891760
Hidde Elzinga <Hidde.Elzinga@Deltares.nl>
2023-03-03 15:00:11 +0100

    TOGIT-27 Replaced version number system for waq

---
+++ src/version_includes/version_definition.h
@@ -0,0 +1,9 @@
+/* include dynamicly generated checkout information
+like branch and revision*/
+#include "checkout_info.h"
+
+/*default values for deltares*/
+#define COMPANY_NAME "Deltares\0"
+#define COMPANY_URL "http://www.deltares.nl\0"
+#define LEGAL_COPYRIGHT "Copyright \xA9 2023"
+#define LEGAL_TRADEMARKS "Copyright \xA9 Deltares"
