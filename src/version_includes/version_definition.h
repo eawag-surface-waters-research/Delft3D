0f77027607ebc187d8e507fbf9f1f1af48ea1529
Hidde Elzinga <Hidde.Elzinga@Deltares.nl>
2023-03-10 14:37:29 +0100

    Small cleanup of version_definition.h

--- src/version_includes/version_definition.h
+++ src/version_includes/version_definition.h
@@ -3,7 +3,7 @@ like branch and revision*/
 #include "checkout_info.h"
 
 /*default values for deltares*/
-#define COMPANY_NAME "Deltares\0"
-#define COMPANY_URL "http://www.deltares.nl\0"
+#define COMPANY_NAME "Deltares"
+#define COMPANY_URL "https://www.deltares.nl"
 #define LEGAL_COPYRIGHT "Copyright \xA9 2023"
 #define LEGAL_TRADEMARKS "Copyright \xA9 Deltares"
