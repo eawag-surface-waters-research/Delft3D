5ae52be181688bcdd38236f92fe0745e363eed98
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-03-14 01:27:05 +0100

    Remove intel set vars call. Do it outside of cmake setup now

--- cb_ninja.bat
+++ cb_ninja.bat
@@ -1,7 +1,4 @@
 @echo off
-cmd.exe /K ""C:\Program Files (x86)\Intel\oneAPI\setvars.bat" && pwsh"
-
-
 set configuration=dwaq
 set generator="Ninja"
 set build_dir=build_%configuration%
@@ -10,5 +7,4 @@ set install_dir=install_dwaq_ninja
 
 rmdir /s /q %build_dir%
 mkdir %build_dir%
-rem cmake .\src\cmake -G "Visual Studio 16 2019" -A x64 -B "build_all" -D CONFIGURATION_TYPE="all" -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icx -DCMAKE_Fortran_COMPILER=ifx
 cmake .\src\cmake -G %generator% -B %build_dir% -D CONFIGURATION_TYPE=%configuration% -D CMAKE_BUILD_TYPE=%build_type% -DCMAKE_INSTALL_PREFIX=%install_dir% -DCMAKE_Fortran_COMPILER=ifx
