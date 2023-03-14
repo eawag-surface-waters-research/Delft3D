cc8cc010a7d4aec15f913bb850ffa1dbee597ed9
Adri Mourits <adri.mourits@deltares.nl>
2023-03-14 16:21:13 +0100

    TOGIT-33: first version, will not compile
    D-Waves version numbering, copied from D-WAQ

---
+++ src/engines_gpl/wave/version/wave_version.h
@@ -0,0 +1,15 @@
+#include "version_definition.h"
+
+/*Definition of the D-Waves version number*/
+#define MAJOR 3
+#define MINOR 08
+#define REVISION 03
+
+#define MAJOR_STR "3"
+#define MINOR_STR "08"
+#define REVISION_STR "03"
+
+#define VERSION_FULL MAJOR_STR "." MINOR_STR "." REVISION_STR "-" BUILD_NR
+
+/*Product definition*/
+#define PRODUCT_NAME "D-Waves"
