2c4417870beb408ebeaec52bfb6b8bca72c1b4fd
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-03-15 09:02:21 +0100

    Switch chosen compiler to ifort from ifx

--- cb_ninja.bat
+++ cb_ninja.bat
@@ -7,4 +7,4 @@ set install_dir=install_dwaq_ninja
 
 rmdir /s /q %build_dir%
 mkdir %build_dir%
-cmake .\src\cmake -G %generator% -B %build_dir% -D CONFIGURATION_TYPE=%configuration% -D CMAKE_BUILD_TYPE=%build_type% -DCMAKE_INSTALL_PREFIX=%install_dir% -DCMAKE_Fortran_COMPILER=ifx
+cmake .\src\cmake -G %generator% -B %build_dir% -D CONFIGURATION_TYPE=%configuration% -D CMAKE_BUILD_TYPE=%build_type% -DCMAKE_INSTALL_PREFIX=%install_dir% -DCMAKE_Fortran_COMPILER=ifort
