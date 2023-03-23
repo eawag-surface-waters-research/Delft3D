ccad2f8a32ae6ab3e8b50bbd419150edfe221f29
buwalda <floris.buwalda@deltares.nl>
2023-03-23 14:03:52 +0100

    Put return statement back to its intended location

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/m_set_HO_advec_velocity.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/m_set_HO_advec_velocity.f90
@@ -120,8 +120,6 @@
    ku2  = abs(klnup(5,LL))
    ku   = abs(kku)
 
-   if (kku == 0 .or. (kku > 0 .and. ku2 == 0)) return
-   
    if (kmx /= 0) then !different ku for 3D
       kkua = ku
       ku  = kbot(kkua) + kmxn(kkua) - ( Lb + kmxL(LL) - L)
@@ -133,6 +131,8 @@
          if (ku2 < kbot(kkub) .or. ku2 > ktop(kkub)) return
       endif
    endif
+   
+   if (kku == 0 .or. (kku > 0 .and. ku2 == 0)) return
 
    call setHOAvelocity(ucxu, ucyu, L, LL, Lb, k, kd, is, n12, ib, half, sl1, sl2, sl3, kku, ku2, ku)
 
@@ -163,8 +163,6 @@
    ku2  = abs(klnup(2,LL))
    ku   = abs(kku)
 
-   if (kku == 0 .or. (kku > 0 .and. ku2 == 0)) return
-   
    if (kmx /= 0) then !different ku for 3D
       kkua = ku
       ku  = kbot(kkua) + kmxn(kkua) - ( Lb + kmxL(LL) - L)
@@ -176,7 +174,9 @@
          if (ku2 < kbot(kkub) .or. ku2 > ktop(kkub)) return
       endif
    endif
-
+   
+   if (kku == 0 .or. (kku > 0 .and. ku2 == 0)) return
+   
    call setHOAvelocity(ucxu, ucyu, L, LL, Lb, k, kd, is, n12, ib, half, sl1, sl2, sl3, kku, ku2, ku)
 
    end subroutine
@@ -206,8 +206,6 @@
    ku2  = abs(klnup(5,LL))
    ku   = abs(kku)
 
-   if (kku == 0 .or. (kku > 0 .and. ku2 == 0)) return
-   
    if (kmx /= 0) then !different ku for 3D
       kkua = ku
       ku  = kbot(kkua) + kmxn(kkua) - ( Lb + kmxL(LL) - L)
@@ -219,7 +217,9 @@
          if (ku2 < kbot(kkub) .or. ku2 > ktop(kkub)) return
       endif
    endif
-
+   
+   if (kku == 0 .or. (kku > 0 .and. ku2 == 0)) return
+   
    call setHOAvelocity_jasfer3D(ucxu, ucyu, L, LL, Lb, k, kd, is, n12, ib, half, sl1, sl2, sl3, kku, ku2, ku)
 
    end subroutine
@@ -249,8 +249,6 @@
    kku  =     klnup(1,LL)
    ku2  = abs(klnup(2,LL))
    ku   = abs(kku)
-
-   if (kku == 0 .or. (kku > 0 .and. ku2 == 0)) return
    
    if (kmx /= 0) then !different ku for 3D
       kkua = ku
@@ -263,6 +261,8 @@
          if (ku2 < kbot(kkub) .or. ku2 > ktop(kkub)) return
       endif
    endif
+   
+   if (kku == 0 .or. (kku > 0 .and. ku2 == 0)) return
 
    call setHOAvelocity_jasfer3D(ucxu, ucyu, L, LL, Lb, k, kd, is, n12, ib, half, sl1, sl2, sl3, kku, ku2, ku)
 
