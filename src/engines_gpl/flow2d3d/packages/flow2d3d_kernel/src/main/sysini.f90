530fe875e958125cfa748c284e633c1cab3f49dd
Adri Mourits <adri.mourits@deltares.nl>
2023-04-12 16:23:42 +0200

    Get compiled:
    globaldata.f90, gdp_alloc.f90, sysini.f90: Added: uniqueid

--- src/engines_gpl/flow2d3d/packages/flow2d3d_kernel/src/main/sysini.f90
+++ src/engines_gpl/flow2d3d/packages/flow2d3d_kernel/src/main/sysini.f90
@@ -211,6 +211,7 @@ subroutine sysini(error     ,runid     ,filmrs    ,prgnm     , &
     write (lundia, '(2a)')  '***           built from : ', trim(txthlp)
     write (lundia, '(a)')   '***'
     write (lundia, '(2a)')  '***           runid      : ', trim(runid)
+    write (lundia, '(2a)')  '***           uniqueid   : ', trim(gdp%uniqueid)
     write (lundia, '(4a)')  '***           date,time  : ', date, ',', rundat(11:19)
     write (lundia, '(a)')   '***'
     write (lundia, '(80a1)') ('*', n = 1, 80)
