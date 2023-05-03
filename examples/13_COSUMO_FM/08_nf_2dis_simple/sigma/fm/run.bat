e5c43e3e730abb7bee9537664642cf8f795c4409
Adri Mourits <adri.mourits@deltares.nl>
2023-05-03 17:40:18 +0200

    example 13: Prepared to also run on Linux (COSUMOsettings.xml is updated) and cleaned up

--- examples/13_COSUMO_FM/08_nf_2dis_simple/sigma/fm/run.bat
+++
@@ -1,7 +0,0 @@
-@ echo off
-
-call c:\checkouts\oss_bins\2.14.07.69056\x64\dimr\scripts\run_dimr.bat
-
-
-    rem To prevent the DOS box from disappearing immediately: remove the rem on the following line
-rem pause
