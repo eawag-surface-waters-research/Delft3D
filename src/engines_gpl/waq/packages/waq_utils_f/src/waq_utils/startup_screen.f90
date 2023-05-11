af368e57964d1e11318a4243bce9af9bedce5a17
Hidde Elzinga <Hidde.Elzinga@deltares.nl>
2023-05-11 15:20:12 +0200

    Added module for startup_screen

--- src/engines_gpl/waq/packages/waq_utils_f/src/waq_utils/startup_screen.f90
+++ src/engines_gpl/waq/packages/waq_utils_f/src/waq_utils/startup_screen.f90
@@ -20,6 +20,12 @@
 !!  All indications and logos of, and references to registered trademarks
 !!  of Stichting Deltares remain the property of Stichting Deltares. All
 !!  rights reserved.
+module m_startup_screen
+
+implicit none
+
+contains
+
 
       subroutine startup_screen(lunrep)
 
@@ -80,3 +86,4 @@
       if ( timon ) call timstop( ithndl )
 
       end subroutine startup_screen
+end module m_startup_screen
