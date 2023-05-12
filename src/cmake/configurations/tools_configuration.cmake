9c8699a893719a0af7993ed056f5d147ac95c82a
Ralph Peelen <ralph.peelen@deltares.nl>
2023-05-12 08:29:19 +0000

    chore(fm): Add option to tools CMake config to exclude FM parts, easier building of io_netcdf.
    Refs: UNST-6923, oss/delft3d!157

--- src/cmake/configurations/tools_configuration.cmake
+++ src/cmake/configurations/tools_configuration.cmake
@@ -1,3 +1,4 @@
+set(NO_FM_TOOLS True)
 include(${CMAKE_CURRENT_SOURCE_DIR}/configurations/include/tools_configuration.cmake)
 
 # Project name must be at the end of the configuration: it might get a name when including other configurations and needs to overwrite that
