95adbe71bba25faf18be0c5749785f28971a00d1
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-06-07 15:30:57 +0200

    Add conversion script

---
+++ src/convert_vfproj_to_use_ifx.py
@@ -0,0 +1,30 @@
+# Script to edit vfproj files to use intel IFX compiler instead of IFORT
+# Currently, cmake + visual studio (2022) does not automatically allow changing fortran compiler
+
+# Author: Maarten Klapwijk
+# Date 07-06-2023
+
+import sys
+import re
+from pathlib import Path
+
+build_dir = sys.argv[1]
+cmake_build_type = sys.argv[2]
+
+folder_path = Path(build_dir).absolute()
+
+ifort_string = 'Name="'+cmake_build_type+'|x64"'
+ifx_string = 'Name="'+cmake_build_type+'|x64" UseCompiler="ifxCompiler"'
+
+ 
+def convert_vfproj_files(file_name):
+    with open(file_name, 'r') as file :
+        filedata = file.read()
+        filedata = re.sub(re.escape(ifort_string), ifx_string, filedata)
+
+        with open(file_name, 'w') as file:
+            file.write(filedata)
+            file.close()
+         
+for file_name in folder_path.rglob("*.vfproj"):
+    convert_vfproj_files(file_name)
