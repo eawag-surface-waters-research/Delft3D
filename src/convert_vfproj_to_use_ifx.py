81d4df63086a440cd01c475a2cd584f324a3e618
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-06-09 15:27:08 +0200

    Change conversion script to use regex. Remove argument. Add minor documentation

--- src/convert_vfproj_to_use_ifx.py
+++ src/convert_vfproj_to_use_ifx.py
@@ -2,25 +2,28 @@
 # Currently, cmake + visual studio (2022) does not automatically allow changing fortran compiler
 
 # Author: Maarten Klapwijk
-# Date 07-06-2023
+# Date 09-06-2023
 
+# Arguments of the script: 
+#   - ifx/ifort: changes to ifx compiler/does nothing
+#   - build_directory
+
+ 
 import sys
 import re
 from pathlib import Path
 
 build_dir = sys.argv[2]
-cmake_build_type = sys.argv[3]
 
 folder_path = Path(build_dir).absolute()
 
-ifort_string = 'Name="'+cmake_build_type+'|x64"'
-ifx_string = 'Name="'+cmake_build_type+'|x64" UseCompiler="ifxCompiler"'
+regex = r"(?P<config_tag><Configuration[\w\s]+Name=\".*\|x64\")"
+subst = "\\g<config_tag> UseCompiler=\"ifxCompiler\""
 
- 
 def convert_vfproj_files(file_name):
     with open(file_name, 'r') as file :
         filedata = file.read()
-        filedata = re.sub(re.escape(ifort_string), ifx_string, filedata)
+        filedata = re.sub(regex, subst, filedata, 0, re.MULTILINE)
 
         with open(file_name, 'w') as file:
             file.write(filedata)
@@ -30,3 +33,8 @@ if sys.argv[1] == "ifx":
     print("Converting *.vfproj files to use IFX instead of IFORT")
     for file_name in folder_path.rglob("*.vfproj"):
         convert_vfproj_files(file_name)
+    print("Using IFX")
+elif sys.argv[1] == "ifort":
+    print("Using IFORT")
+else:
+    print("Argument given to convert_vfproj_to_use_ifx.py script not recognised. Exiting")
